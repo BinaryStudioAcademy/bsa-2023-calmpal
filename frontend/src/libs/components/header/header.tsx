f15a4023fb29bf3faa8836b833666ffb812401d0
OSCAR SANTIAGO VELEZ MUÑOZ <90299049+ossavemu@users.noreply.github.com>
2023-08-19 18:28:16 -0500

    cp-32: + add header logo

--- frontend/src/libs/components/header/header.tsx
+++ frontend/src/libs/components/header/header.tsx
@@ -1,6 +1,6 @@
 import '#assets/css/header.scss';
 
-import reactLogo from '#assets/img/react.svg';
+import headerLogo from '#assets/img/header-logo.svg';
 import { AppRoute } from '#libs/enums/app-route.enum.js';
 
 import { Link } from '../components.js';
@@ -9,7 +9,7 @@ const Header: React.FC = () => {
   return (
     <header className="navbar">
       <nav className="navbar-content">
-        <img src={reactLogo} className="App-logo" width="30" alt="logo" />
+        <img src={headerLogo} className="App-logo" width="40" alt="logo" />
 
         <Link className="nav-link" to={AppRoute.SIGN_IN}>
           Sing In
