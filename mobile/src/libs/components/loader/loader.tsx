5d64b2a538789617639cd28971dd3d6519de4fbd
v.matveeva <vika030718@gmail.com>
2023-08-20 13:44:40 -0400

    cp-26: * fix  enum name and  structure

--- mobile/src/libs/components/loader/loader.tsx
+++ mobile/src/libs/components/loader/loader.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { ActivityIndicator } from 'react-native';
 
-import { AppColors } from '#libs/enums/ui/app-colors';
+import { AppColor } from '#libs/enums/enums';
 
 const Loader: React.FC = () => {
-  return <ActivityIndicator size="large" color={AppColors.BLUE_100} />;
+  return <ActivityIndicator size="large" color={AppColor.BLUE_100} />;
 };
 
 export { Loader };
--- mobile/src/libs/enums/ui/app-color.enum.ts
+++ mobile/src/libs/enums/ui/app-color.enum.ts
@@ -1,4 +1,4 @@
-const AppColors = {
+const AppColor = {
   BLUE_100: '#4E80FF',
   BLUE_200: '#507CEB',
   PINK_100: '#E1A7D5',
@@ -7,6 +7,6 @@ const AppColors = {
   GRAY_200: '#CCDDF3',
   GRAY_300: '#ACB4D0',
   GRAY_400: '#302E36',
-};
+} as const;
 
-export { AppColors };
+export { AppColor };
