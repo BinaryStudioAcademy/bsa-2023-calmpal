c52d73755fbcbb734568f653337a83c38e842531
iamAlinaaa <alinaaevns1806@gmail.com>
2023-08-20 22:18:35 +0300

    cp-23: + added styles for button and input components, changes on input field related to different types of input state

--- mobile/src/libs/components/button/styles.ts
+++ mobile/src/libs/components/button/styles.ts
@@ -1,13 +1,27 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  button: {
-    padding: 10,
-    backgroundColor: 'blue',
+  btn: {
+    backgroundColor: '#302E36',
     alignItems: 'center',
+    justifyContent: 'center',
+    height: 44,
+    borderRadius: 8,
+    gap: 10,
   },
   label: {
     color: 'white',
+    fontWeight: '600',
+    fontSize: 16,
+  },
+  btnDisabled: {
+    backgroundColor: '#312F377F',
+  },
+  labelDisabled: {
+    color: '#FFFFFF7F',
+  },
+  btnPressed: {
+    backgroundColor: '#1D1C21FF',
   },
 });
 
