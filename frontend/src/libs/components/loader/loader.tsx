c4c1fd09be97e22d731b4b8d2c19c6f8f4f80b11
OSCAR SANTIAGO VELEZ MUÑOZ <90299049+ossavemu@users.noreply.github.com>
2023-08-20 06:57:53 -0500

    cp-20: * fixing css imports and html semantic

--- frontend/src/libs/components/loader/loader.tsx
+++ frontend/src/libs/components/loader/loader.tsx
@@ -4,10 +4,10 @@ const Loader: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.ellipsis}>
-        <div className={styles.child}></div>
-        <div className={styles.child}></div>
-        <div className={styles.child}></div>
-        <div className={styles.child}></div>
+        <div className={styles.child} />
+        <div className={styles.child} />
+        <div className={styles.child} />
+        <div className={styles.child} />
       </div>
     </div>
   );
