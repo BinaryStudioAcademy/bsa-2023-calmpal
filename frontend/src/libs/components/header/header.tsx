03fd20d26f634ae1a3ae51ab55cbf49c513fa3cb
OSCAR SANTIAGO VELEZ MUÑOZ <90299049+ossavemu@users.noreply.github.com>
2023-08-19 23:15:49 -0500

    cp-32: + completing comments

--- frontend/src/libs/components/header/header.tsx
+++ frontend/src/libs/components/header/header.tsx
@@ -1,17 +1,17 @@
-import '#assets/css/header.scss';
-
 import headerLogo from '#assets/img/header-logo.svg';
 import { AppRoute } from '#libs/enums/app-route.enum.js';
 
 import { Link } from '../components.js';
+import styles from './header.module.scss';
 
 const Header: React.FC = () => {
   return (
-    <header className="navbar">
-      <nav className="navbar-content">
-        <img src={headerLogo} className="App-logo" width="40" alt="logo" />
-
-        <Link className="nav-link" to={AppRoute.SIGN_IN}>
+    <header className={styles.header}>
+      <Link to={AppRoute.ROOT}>
+        <img src={headerLogo} width="40" alt="logo" />
+      </Link>
+      <nav className={styles.nav}>
+        <Link className={styles.link} to={AppRoute.SIGN_IN}>
           Sing In
         </Link>
       </nav>
--- frontend/src/libs/components/link/link.tsx
+++ frontend/src/libs/components/link/link.tsx
@@ -3,18 +3,22 @@ import { NavLink } from 'react-router-dom';
 import { type AppRoute } from '#libs/enums/enums.js';
 import { type ValueOf } from '#libs/types/types.js';
 
+import styles from './link.module.scss';
+
 type Properties = {
   to: ValueOf<typeof AppRoute>;
   children: React.ReactNode;
   className?: string;
 };
 
+const stylePatcher = (newStyle: string): string => `${styles.link} ${newStyle}`;
+
 const Link: React.FC<Properties> = ({
   children,
   to,
-  className,
+  className = '',
 }: Properties) => (
-  <NavLink className={className ?? ''} to={to}>
+  <NavLink className={stylePatcher(className)} to={to}>
     {children}
   </NavLink>
 );
