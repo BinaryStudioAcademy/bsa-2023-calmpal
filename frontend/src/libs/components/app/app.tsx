b7117a0bbba88eacfcac65c8c89e3bb86879cb06
OSCAR SANTIAGO VELEZ MUÑOZ <90299049+ossavemu@users.noreply.github.com>
2023-08-19 22:15:37 -0500

    cp-20: * fixed styles and app.tsx

--- frontend/src/libs/components/app/app.tsx
+++ frontend/src/libs/components/app/app.tsx
@@ -1,5 +1,5 @@
 import reactLogo from '#assets/img/react.svg';
-import { Link, Loader, RouterOutlet } from '#libs/components/components.js';
+import { Link, RouterOutlet } from '#libs/components/components.js';
 import { AppRoute } from '#libs/enums/enums.js';
 import {
   useAppDispatch,
@@ -27,7 +27,6 @@ const App: React.FC = () => {
 
   return (
     <>
-      <Loader />
       <img src={reactLogo} className="App-logo" width="30" alt="logo" />
 
       <ul className="App-navigation-list">
