1beaf4ea4c5bba01fa099facd00c00dfcf64a3a1
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-08-21 11:42:29 +0300

    cp-18: * protected route component by adding useSelector

--- frontend/src/libs/components/protected-route/protected-route.tsx
+++ frontend/src/libs/components/protected-route/protected-route.tsx
@@ -1,4 +1,5 @@
 import { type ComponentType } from 'react';
+import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
 import { AppRoute } from '#libs/enums/enums.js';
@@ -7,15 +8,14 @@ type Properties = {
   component: ComponentType;
 };
 
-const mockUserData = (isUserExist: boolean): boolean => {
-  return isUserExist;
-};
-
 const ProtectedRoute: React.FC<Properties> = ({
   component: Component,
   ...rest
 }) => {
-  const hasUser = mockUserData(false);
+  const { user } = useSelector(() => ({
+    user: null,
+  }));
+  const hasUser = Boolean(user);
 
   return hasUser ? <Component {...rest} /> : <Navigate to={AppRoute.SIGN_IN} />;
 };
