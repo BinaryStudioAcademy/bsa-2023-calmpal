2b2cc662b25cdec39706db2ab2823c3aff6b1808
HIMDanny <danilbliz0201@gmail.com>
2023-08-22 15:06:58 +0300

    cp-17: + jwt service

--- backend/src/packages/auth/auth.ts
+++ backend/src/packages/auth/auth.ts
@@ -1,13 +1,20 @@
+import { config } from '#libs/packages/config/config.js';
 import { logger } from '#libs/packages/logger/logger.js';
 import { userService } from '#packages/users/users.js';
 
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
+import { JWTService } from './jwt.service.js';
 
+const jwtService = new JWTService({
+  secret: config.ENV.JWT.SECRET_KEY,
+  alg: config.ENV.JWT.ALG,
+});
 const authService = new AuthService(userService);
 const authController = new AuthController(logger, authService);
 
-export { authController, authService };
+export { authController, authService, jwtService };
 export { type AuthService } from './auth.service.js';
+export { type JWTService } from './jwt.service.js';
 export { AuthApiPath } from './libs/enums/enums.js';
-export { verifyToken } from './libs/helpers/helpers.js';
+export { type AuthTokenPayload } from './libs/types/types.js';
