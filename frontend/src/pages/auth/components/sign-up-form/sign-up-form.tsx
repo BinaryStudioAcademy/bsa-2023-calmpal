fac7118aa897696bfdd1ba744e9a660f4f4f55ad
evgen <evgenkitan@gmail.com>
2023-08-22 13:58:09 +0300

    cp-15: + field for name in sign-up form

--- frontend/src/pages/auth/components/sign-up-form/sign-up-form.tsx
+++ frontend/src/pages/auth/components/sign-up-form/sign-up-form.tsx
@@ -1,17 +1,19 @@
-import { Button, Input } from '#libs/components/components.js';
+import { Button, Input, Link } from '#libs/components/components.js';
+import { AppRoute } from '#libs/enums/app-route.enum.js';
 import { useAppForm, useCallback } from '#libs/hooks/hooks.js';
 import {
   type UserSignUpRequestDto,
   userSignUpValidationSchema,
 } from '#packages/users/users.js';
 
+import styles from '../../styles.module.scss';
 import { DEFAULT_SIGN_UP_PAYLOAD } from './libs/constants.js';
 
 type Properties = {
   onSubmit: (payload: UserSignUpRequestDto) => void;
 };
 
-const SignUpForm: React.FC<Properties> = ({ onSubmit }) => {
+const SignUpForm: React.FC<Properties> = ({ onSubmit }: Properties) => {
   const { control, errors, handleSubmit } = useAppForm<UserSignUpRequestDto>({
     defaultValues: DEFAULT_SIGN_UP_PAYLOAD,
     validationSchema: userSignUpValidationSchema,
@@ -26,13 +28,23 @@ const SignUpForm: React.FC<Properties> = ({ onSubmit }) => {
 
   return (
     <>
-      <h3>Sign Up</h3>
-      <form onSubmit={handleFormSubmit}>
+      <form onSubmit={handleFormSubmit} className={styles['authForm']}>
+        <h1>Create a free account to save your preferences</h1>
         <p>
           <Input
             type="text"
-            label="Email"
-            placeholder="Enter your email"
+            label="Name"
+            placeholder="Please write your name"
+            name="fullName"
+            control={control}
+            errors={errors}
+          />
+        </p>
+        <p>
+          <Input
+            type="text"
+            label="E-mail"
+            placeholder="name@gmail.com"
             name="email"
             control={control}
             errors={errors}
@@ -42,13 +54,18 @@ const SignUpForm: React.FC<Properties> = ({ onSubmit }) => {
           <Input
             type="text"
             label="Password"
-            placeholder="Enter your password"
             name="password"
             control={control}
             errors={errors}
           />
         </p>
-        <Button type="submit" label="Sign up" />
+        <Button type="submit" label="Sign up" className={styles['btn']} />
+        <span className={styles['authFormLink']}>
+          Already have an account? Go to
+          <Link to={AppRoute.SIGN_IN}>
+            <p>Sign in</p>
+          </Link>
+        </span>
       </form>
     </>
   );
