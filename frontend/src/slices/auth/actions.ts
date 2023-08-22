403faea3c63d46ac2e56d5afcad3dee1db2b311b
myducck <dmitro.kucherenko@gmail.com>
2023-08-22 14:01:57 +0300

    cp-13: * sign in action

--- frontend/src/slices/auth/actions.ts
+++ frontend/src/slices/auth/actions.ts
@@ -2,6 +2,8 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { type AsyncThunkConfig } from '#libs/types/types.js';
 import {
+  type UserDetailsDto,
+  type UserSignInRequestDto,
   type UserSignUpRequestDto,
   type UserSignUpResponseDto,
 } from '#packages/users/users.js';
@@ -18,4 +20,21 @@ const signUp = createAsyncThunk<
   return authApi.signUp(registerPayload);
 });
 
-export { signUp };
+const signIn = createAsyncThunk<
+  UserDetailsDto,
+  UserSignInRequestDto,
+  AsyncThunkConfig
+>(`${sliceName}/sign-in`, async (loginPayload, { rejectWithValue, extra }) => {
+  const { authApi, storage } = extra;
+
+  try {
+    const userData = await authApi.signIn(loginPayload);
+    await storage.set('token', userData.token);
+
+    return userData.user;
+  } catch (error) {
+    return rejectWithValue(error as Error);
+  }
+});
+
+export { signIn, signUp };
