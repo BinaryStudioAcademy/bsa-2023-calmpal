39db5ee5d7fc33ad29c35d4aaecddb750c11968e
myducck <dmitro.kucherenko@gmail.com>
2023-08-22 14:04:48 +0300

    cp-13: * auth and sign in component

--- frontend/src/pages/auth/auth.tsx
+++ frontend/src/pages/auth/auth.tsx
@@ -1,25 +1,26 @@
+import clsx from 'clsx';
+
 import { AppRoute } from '#libs/enums/enums.js';
+import { useAppDispatch, useCallback, useLocation } from '#libs/hooks/hooks.js';
 import {
-  useAppDispatch,
-  useAppSelector,
-  useCallback,
-  useLocation,
-} from '#libs/hooks/hooks.js';
-import { type UserSignUpRequestDto } from '#packages/users/users.js';
+  type UserSignInRequestDto,
+  type UserSignUpRequestDto,
+} from '#packages/users/users.js';
 import { actions as authActions } from '#slices/auth/auth.js';
 
 import { SignInForm, SignUpForm } from './components/components.js';
+import styles from './styles.module.scss';
 
 const Auth: React.FC = () => {
   const dispatch = useAppDispatch();
-  const { dataStatus } = useAppSelector(({ auth }) => ({
-    dataStatus: auth.dataStatus,
-  }));
   const { pathname } = useLocation();
 
-  const handleSignInSubmit = useCallback((): void => {
-    // handle sign in
-  }, []);
+  const handleSignInSubmit = useCallback(
+    (payload: UserSignInRequestDto): void => {
+      void dispatch(authActions.signIn(payload));
+    },
+    [dispatch],
+  );
 
   const handleSignUpSubmit = useCallback(
     (payload: UserSignUpRequestDto): void => {
@@ -42,10 +43,27 @@ const Auth: React.FC = () => {
   };
 
   return (
-    <>
-      state: {dataStatus}
-      {getScreen(pathname)}
-    </>
+    <div className={styles['auth']}>
+      <div className={styles['logo']}>Calmpal</div>
+
+      <div className={styles['authName']}>
+        <p>Welcome to</p>
+        <p>Calmpal</p>
+      </div>
+      <div className={styles['overlay']}>{getScreen(pathname)}</div>
+
+      <span className={clsx(styles['shadow'], styles['shadowTopRight'])} />
+      <span className={clsx(styles['shadow'], styles['shadowBottomRight'])} />
+      <span className={clsx(styles['shadow'], styles['shadowBottomLeft'])} />
+      <span
+        className={clsx(styles['shadow'], styles['shadowCornerBottomLeft'])}
+      />
+      <span className={clsx(styles['shadow'], styles['shadowCenter'])} />
+      <span className={clsx(styles['bubble'], styles['bubbleBottom'])} />
+      <span className={clsx(styles['bubble'], styles['bubbleRight'])} />
+      <span className={clsx(styles['bubble'], styles['bubbleCenter'])} />
+      <span className={clsx(styles['bubble'], styles['bubbleTop'])} />
+    </div>
   );
 };
 
--- frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
+++ frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
@@ -1,16 +1,61 @@
-import { Button } from '#libs/components/components.js';
+import { Button, Input, Link } from '#libs/components/components.js';
+import { AppRoute } from '#libs/enums/app-route.enum.js';
+import { useAppForm, useCallback } from '#libs/hooks/hooks.js';
+import {
+  type UserSignInRequestDto,
+  userSignInValidationSchema,
+} from '#packages/users/users.js';
+
+import styles from '../../styles.module.scss';
+import { DEFAULT_SIGN_IN_PAYLOAD } from './libs/constants.js';
 
 type Properties = {
-  onSubmit: () => void;
+  onSubmit: (payload: UserSignInRequestDto) => void;
 };
 
-const SignInForm: React.FC<Properties> = () => (
-  <>
-    <h1>Sign In</h1>
-    <form>
-      <Button label="Sign in" />
-    </form>
-  </>
-);
+const SignInForm: React.FC<Properties> = ({ onSubmit }: Properties) => {
+  const { control, errors, handleSubmit } = useAppForm<UserSignInRequestDto>({
+    defaultValues: DEFAULT_SIGN_IN_PAYLOAD,
+    validationSchema: userSignInValidationSchema,
+  });
+
+  const handleFormSubmit = useCallback(
+    (event_: React.BaseSyntheticEvent): void => {
+      void handleSubmit(onSubmit)(event_);
+    },
+    [handleSubmit, onSubmit],
+  );
+
+  return (
+    <>
+      <form className={styles['authForm']} onSubmit={handleFormSubmit}>
+        <h1>Sign In to your account</h1>
+
+        <Input
+          type="text"
+          label="Email"
+          placeholder="name@gmail.com"
+          name="email"
+          control={control}
+          errors={errors}
+        />
+        <Input
+          type="text"
+          label="Password"
+          placeholder=""
+          name="password"
+          control={control}
+          errors={errors}
+        />
+        <Button type="submit" label="Sign in" className={styles['btn']} />
+
+        <span className={styles['authFormLink']}>
+          Don&apos;t have an account? Go to
+          <Link to={AppRoute.SIGN_UP}>Sign up</Link>
+        </span>
+      </form>
+    </>
+  );
+};
 
 export { SignInForm };
