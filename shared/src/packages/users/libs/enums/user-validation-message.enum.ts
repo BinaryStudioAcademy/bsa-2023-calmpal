1688026fd3999bd456fbd25266d41297870c8fa8
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-08-22 15:09:27 +0300

    cp-14: * fixed lint error

--- shared/src/packages/users/libs/enums/user-validation-message.enum.ts
+++ shared/src/packages/users/libs/enums/user-validation-message.enum.ts
@@ -2,7 +2,6 @@ const UserValidationMessage = {
   EMAIL_REQUIRED: 'Email is required',
   EMAIL_WRONG: 'Email is wrong',
   NAME_REQUIRED: 'Name is required'
-  
 } as const;
 
 export { UserValidationMessage };
