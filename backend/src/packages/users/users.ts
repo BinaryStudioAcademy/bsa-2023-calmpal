aa0d6c8dff67b8bc84dc0de8263f0034885292c0
Vladyslav Dobychin <vladyslavdobychin@gmail.com>
2023-08-22 11:37:36 +0300

    cp-16: * replace a new instance of config with an already existing one

--- backend/src/packages/users/users.ts
+++ backend/src/packages/users/users.ts
@@ -1,4 +1,4 @@
-import { BaseConfig } from '#libs/packages/config/base-config.package.js';
+import { config } from '#libs/packages/config/config.js';
 import { logger } from '#libs/packages/logger/logger.js';
 import { JWTService } from '#packages/auth/jwt.service.js';
 
@@ -8,7 +8,6 @@ import { UserRepository } from './user.repository.js';
 import { UserService } from './user.service.js';
 
 const userRepository = new UserRepository(UserModel);
-const config = new BaseConfig(logger);
 const jwtService = new JWTService(config.ENV.AUTH.JWT_SECRET);
 const userService = new UserService(userRepository, jwtService);
 const userController = new UserController(logger, userService);
