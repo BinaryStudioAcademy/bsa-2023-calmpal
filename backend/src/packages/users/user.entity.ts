2cab13923f68b3e8f3874b3a45cb945905600f92
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-08-22 11:56:08 +0300

    cp-14: + createdAt and updatedAt field for user entity

--- backend/src/packages/users/user.entity.ts
+++ backend/src/packages/users/user.entity.ts
@@ -3,6 +3,10 @@ import { type Entity } from '#libs/types/types.js';
 class UserEntity implements Entity {
   private id: number | null;
 
+  private createdAt: string  | null;
+
+  private updatedAt: string | null;
+
   private email: string;
 
   private fullName: string;
@@ -17,18 +21,24 @@ class UserEntity implements Entity {
     fullName,
     passwordHash,
     passwordSalt,
+    createdAt,
+    updatedAt
   }: {
     id: number | null;
     email: string;
     fullName: string;
     passwordHash: string;
     passwordSalt: string;
+    createdAt: string | null;
+    updatedAt: string | null;
   }) {
     this.id = id;
     this.email = email;
     this.fullName = fullName;
     this.passwordHash = passwordHash;
     this.passwordSalt = passwordSalt;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
   }
 
   public static initialize({
@@ -37,12 +47,16 @@ class UserEntity implements Entity {
     fullName,
     passwordHash,
     passwordSalt,
+    createdAt,
+    updatedAt
   }: {
     id: number;
     email: string;
     fullName: string,
     passwordHash: string;
     passwordSalt: string;
+    createdAt: string;
+    updatedAt: string;
   }): UserEntity {
     return new UserEntity({
       id,
@@ -50,6 +64,8 @@ class UserEntity implements Entity {
       fullName,
       passwordHash,
       passwordSalt,
+      createdAt,
+      updatedAt
     });
   }
 
@@ -70,6 +86,8 @@ class UserEntity implements Entity {
       fullName,
       passwordHash,
       passwordSalt,
+      createdAt: null,
+      updatedAt: null
     });
   }
 
@@ -77,11 +95,15 @@ class UserEntity implements Entity {
     id: number;
     email: string;
     fullName: string;
+    createdAt: string;
+    updatedAt: string;
   } {
     return {
       id: this.id as number,
       email: this.email,
-      fullName: this.fullName
+      fullName: this.fullName,
+      createdAt: this.createdAt as string,
+      updatedAt: this.updatedAt as string
     };
   }
 
