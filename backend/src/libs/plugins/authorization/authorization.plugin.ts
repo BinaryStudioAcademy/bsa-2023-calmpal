1ed7b409661ec4b1819280e9763752dc585dde62
HIMDanny <danilbliz0201@gmail.com>
2023-08-22 21:09:54 +0300

    cp-17: - auth error status

--- backend/src/libs/plugins/authorization/authorization.plugin.ts
+++ backend/src/libs/plugins/authorization/authorization.plugin.ts
@@ -3,7 +3,7 @@ import fp from 'fastify-plugin';
 import { ExceptionMessage } from '#libs/enums/enums.js';
 import { AuthError } from '#libs/exceptions/exceptions.js';
 import { ControllerHook } from '#libs/packages/controller/controller.js';
-import { HTTPCode, type HTTPMethod } from '#libs/packages/http/http.js';
+import { type HTTPMethod } from '#libs/packages/http/http.js';
 import { checkIsWhiteRoute } from '#libs/packages/server-application/server-application.js';
 import { type AuthTokenPayload, type JWTService } from '#packages/auth/auth.js';
 import { type UserService } from '#packages/users/users.js';
@@ -33,7 +33,6 @@ const authorization = fp<Options>((fastify, { services }, done) => {
     if (!token) {
       throw new AuthError({
         message: ExceptionMessage.UNAUTHORIZED_USER,
-        status: HTTPCode.UNAUTHORIZED,
       });
     }
 
@@ -44,7 +43,6 @@ const authorization = fp<Options>((fastify, { services }, done) => {
     if (!authorizedUser) {
       throw new AuthError({
         message: ExceptionMessage.INVALID_TOKEN,
-        status: HTTPCode.UNAUTHORIZED,
       });
     }
 
