42be60be27549c203cf34d62b0c9e006b6f187af
evgen <evgenkitan@gmail.com>
2023-08-22 15:55:51 +0300

    cp-15: * button and input fixes

--- frontend/src/libs/components/app/app.tsx
+++ frontend/src/libs/components/app/app.tsx
@@ -1,8 +1,4 @@
-// import reactLogo from '#assets/img/react.svg';
-import {
-  // Header, Link,
-  RouterOutlet,
-} from '#libs/components/components.js';
+import { RouterOutlet } from '#libs/components/components.js';
 import { AppRoute } from '#libs/enums/enums.js';
 import { useAppDispatch, useEffect, useLocation } from '#libs/hooks/hooks.js';
 import { actions as userActions } from '#slices/users/users.js';
@@ -21,22 +17,6 @@ const App: React.FC = () => {
 
   return (
     <>
-      {/* <Header />
-      <img src={reactLogo} className="App-logo" width="30" alt="logo" />
-
-      <ul className="App-navigation-list">
-        <li>
-          <Link to={AppRoute.ROOT}>Root</Link>
-        </li>
-        <li>
-          <Link to={AppRoute.SIGN_IN}>Sign in</Link>
-        </li>
-        <li>
-          <Link to={AppRoute.SIGN_UP}>Sign up</Link>
-        </li>
-      </ul>
-      <p>Current path: {pathname}</p> */}
-
       <div>
         <RouterOutlet />
       </div>
