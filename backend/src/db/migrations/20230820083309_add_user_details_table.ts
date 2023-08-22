776ae1611d5dbb9924627ab3799ad6a402c407b0
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-08-22 17:54:29 +0300

    cp-14: * set up ColumnName object as const

--- backend/src/db/migrations/20230820083309_add_user_details_table.ts
+++ backend/src/db/migrations/20230820083309_add_user_details_table.ts
@@ -12,7 +12,7 @@ const ColumnName = {
   SURVEY: 'survey',
   CREATED_AT: 'created_at',
   UPDATED_AT: 'updated_at',
-};
+} as const;
 
 function up(knex: Knex): Promise<void> {
   return knex.schema.createTable(TABLE_NAME, (table) => {
