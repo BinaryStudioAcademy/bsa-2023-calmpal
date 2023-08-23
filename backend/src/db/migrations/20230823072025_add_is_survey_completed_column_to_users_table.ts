a5332102b2dcb5fff2d7b71eae9168a9886da18b
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-08-23 10:30:17 +0300

    cp-50: + new migration file for adding is_survey_completed column in users table

---
+++ backend/src/db/migrations/20230823072025_add_is_survey_completed_column_to_users_table.ts
@@ -0,0 +1,21 @@
+import { type Knex } from 'knex';
+
+const TABLE_NAME = 'users';
+
+const ColumnName = {
+  IS_SURVEY_COMPLETED: 'is_survey_completed',
+};
+
+function up(knex: Knex): Promise<void> {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.boolean(ColumnName.IS_SURVEY_COMPLETED).defaultTo(false);
+  });
+}
+
+function down(knex: Knex): Promise<void> {
+  return knex.schema.table(TABLE_NAME, (table) => {
+    table.dropColumn(ColumnName.IS_SURVEY_COMPLETED);
+  });
+}
+
+export { down, up };
