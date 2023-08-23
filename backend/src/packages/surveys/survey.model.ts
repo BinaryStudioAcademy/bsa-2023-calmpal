d670e536e53d156b4e6edf6c49e9c833df280a92
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-08-23 13:29:00 +0300

    cp-50: + survey model

---
+++ backend/src/packages/surveys/survey.model.ts
@@ -0,0 +1,30 @@
+import { Model, type RelationMappings } from 'objection';
+
+import {
+  AbstractModel,
+  DatabaseTableName,
+} from '#libs/packages/database/database.js';
+import { UserModel } from '#packages/users/user.model.js';
+
+class SurveyModel extends AbstractModel {
+  public preferences!: string[];
+
+  public static override get tableName(): string {
+    return DatabaseTableName.SURVEYS;
+  }
+
+  public static get relationMappings(): RelationMappings {
+    return {
+      users: {
+        relation: Model.HasOneRelation,
+        modelClass: UserModel,
+        join: {
+          from: `${DatabaseTableName.SURVEYS}.user_id`,
+          to: `${DatabaseTableName.USERS}.id`,
+        },
+      },
+    };
+  }
+}
+
+export { SurveyModel };
