5563071a8c338afc4e55ca060c138a71d4a4e7f3
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-08-23 15:48:56 +0300

    cp-50: + survey service

---
+++ backend/src/packages/surveys/survey.service.ts
@@ -0,0 +1,52 @@
+import { type Service } from '#libs/types/types.js';
+
+import { type SurveyGetAllResponseDto } from './libs/types/types.js';
+import { SurveyEntity } from './survey.entity.js';
+import { type SurveyRepository } from './survey.repository.js';
+import {
+  type SurveyGetAllItemsResponseDto,
+  type SurveyRequestDto,
+} from './surveys.js';
+
+class SurveyService implements Service {
+  private surveyRepository: SurveyRepository;
+
+  public constructor(surveyRepository: SurveyRepository) {
+    this.surveyRepository = surveyRepository;
+  }
+
+  public find(): ReturnType<Service['find']> {
+    return Promise.resolve(null);
+  }
+
+  public async findAll(): Promise<SurveyGetAllResponseDto> {
+    const items = await this.surveyRepository.findAll();
+
+    return {
+      items: items.map((item) => item.toObject()),
+    };
+  }
+
+  public async create(
+    payload: SurveyRequestDto,
+  ): Promise<SurveyGetAllItemsResponseDto> {
+    const item = await this.surveyRepository.create(
+      SurveyEntity.initializeNew({
+        userId: payload.userId,
+        preferences: payload.preferences,
+      }),
+    );
+
+    return item.toObject();
+  }
+
+  public update(): ReturnType<Service['update']> {
+    return Promise.resolve(null);
+  }
+
+  public delete(): ReturnType<Service['delete']> {
+    return Promise.resolve(true);
+  }
+}
+
+export { SurveyService };
