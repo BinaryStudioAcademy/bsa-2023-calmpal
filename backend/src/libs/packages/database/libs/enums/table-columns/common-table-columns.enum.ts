7c76793906445312e8632293228017749979f19b
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-08-23 18:25:52 +0300

    cp-14: * made all enums singular

--- backend/src/libs/packages/database/libs/enums/table-columns/common-table-columns.enum.ts
+++ backend/src/libs/packages/database/libs/enums/table-columns/common-table-columns.enum.ts
@@ -1,7 +1,7 @@
-const CommonTableColumns = {
+const CommonTableColumn = {
   ID: 'id',
   CREATED_AT: 'created_at',
   UPDATED_AT: 'updated_at',
 } as const;
 
-export { CommonTableColumns };
+export { CommonTableColumn };
--- backend/src/libs/packages/database/libs/enums/table-columns/users-table-columns.enum.ts
+++ backend/src/libs/packages/database/libs/enums/table-columns/users-table-columns.enum.ts
@@ -1,7 +1,7 @@
-const UsersTableColumns = {
+const UsersTableColumn = {
   EMAIL: 'email',
   PASSWORD_HASH: 'password_hash',
   PASSWORD_SALT: 'password_salt',
 } as const;
 
-export { UsersTableColumns };
+export { UsersTableColumn };
