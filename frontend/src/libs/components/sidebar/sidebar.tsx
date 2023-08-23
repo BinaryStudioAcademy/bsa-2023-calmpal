c4b3cee62c1edb20a99739d498632788247d4083
Анастасія Лисенко <anastasiialysenko0@gmail.com>
2023-08-23 21:27:23 +0300

    cp-42: + sidebar

---
+++ frontend/src/libs/components/sidebar/sidebar.tsx
@@ -0,0 +1,71 @@
+import { useLocation } from 'react-router-dom';
+
+import home from '#assets/img/home.svg';
+import logo from '#assets/img/logo.svg';
+import { Link } from '#libs/components/components.js';
+import { AppRoute } from '#libs/enums/enums.js';
+
+import styles from './styles.module.scss';
+
+const Sidebar: React.FC = () => {
+  const iconSelected = 'icon-selected';
+  const iconStyles = 'icon-container';
+  const { pathname } = useLocation();
+
+  return (
+    <div className={styles['sidebar']}>
+      <nav className={styles['nav']}>
+        <Link to={AppRoute.ROOT}>
+          <div className={styles['image']}>
+            <img src={logo} alt="logo" />
+          </div>
+        </Link>
+      </nav>
+      <nav className={styles['nav']}>
+        <div className={styles['icons-container']}>
+          <button
+            className={`${
+              pathname === AppRoute.ROOT
+                ? styles[iconSelected]
+                : styles[iconStyles]
+            }`}
+          >
+            <Link to={AppRoute.ROOT}>
+              <div>
+                <img src={home} alt="home" className={styles['icon']} />
+              </div>
+            </Link>
+          </button>
+          <button
+            className={`${
+              pathname === AppRoute.SIGN_IN
+                ? styles[iconSelected]
+                : styles[iconStyles]
+            }`}
+          >
+            <Link to={AppRoute.SIGN_IN}>
+              <div>
+                <img src={home} alt="home" className={styles['icon']} />
+              </div>
+            </Link>
+          </button>
+          <button
+            className={`${
+              pathname === AppRoute.SIGN_UP
+                ? styles[iconSelected]
+                : styles[iconStyles]
+            }`}
+          >
+            <Link to={AppRoute.SIGN_UP}>
+              <div>
+                <img src={home} alt="home" className={styles['icon']} />
+              </div>
+            </Link>
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export { Sidebar };
