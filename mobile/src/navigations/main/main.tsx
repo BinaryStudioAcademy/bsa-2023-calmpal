729d26e55b8a8cb627a39fdda5273caa5e6739b8
v.matveeva <vika030718@gmail.com>
2023-08-23 16:27:28 -0400

    cp-38: * path to the icon

--- mobile/src/navigations/main/main.tsx
+++ mobile/src/navigations/main/main.tsx
@@ -4,11 +4,11 @@ import {
 } from '@react-navigation/bottom-tabs';
 import React from 'react';
 
+import HomeIcon from '#assets/img/icons/home.svg';
 import { AppColor, MainScreenName } from '#libs/enums/enums';
 import { type TabNavigationParameterList } from '#libs/types/types';
 import { Home } from '#screens/main/home';
 
-import HomeIcon from '../../assets/img/icons/home.svg';
 import { styles } from './styles';
 
 const BottomTab = createBottomTabNavigator<TabNavigationParameterList>();
