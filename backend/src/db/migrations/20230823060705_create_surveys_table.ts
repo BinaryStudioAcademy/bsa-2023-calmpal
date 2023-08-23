fba6b03ffbda09c0594a429d0785e4f9ecc7ad42
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-08-23 10:29:12 +0300

    cp-50: + new migration file for creating surveys table

---
+++ backend/src/db/migrations/20230823060705_create_surveys_table.ts
@@ -0,0 +1,46 @@
+import { type Knex } from 'knex';
+
+const TABLE_NAME = 'surveys';
+const FOREIGN_TABLE_NAME = 'users';
+
+const ColumnName = {
+  ID: 'id',
+  USER_ID: 'user_id',
+  PREFERENCES: 'preferences',
+  CREATED_AT: 'created_at',
+  UPDATED_AT: 'updated_at',
+};
+
+const RelationRule = {
+  CASCADE: 'CASCADE',
+  SET_NULL: 'SET NULL',
+};
+
+function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.increments(ColumnName.ID).primary();
+    table
+      .integer(ColumnName.USER_ID)
+      .unique()
+      .references(ColumnName.ID)
+      .inTable(FOREIGN_TABLE_NAME)
+      .onUpdate(RelationRule.CASCADE)
+      .onDelete(RelationRule.SET_NULL)
+      .notNullable();
+    table.specificType(ColumnName.PREFERENCES, 'text[]').notNullable();
+    table
+      .dateTime(ColumnName.CREATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .dateTime(ColumnName.UPDATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+  });
+}
+
+function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists(TABLE_NAME);
+}
+
+export { down, up };
