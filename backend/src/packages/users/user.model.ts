595d9171f6c74774749ac63ef4aa0d98a8f62ca9
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-08-23 13:30:12 +0300

    cp-50: + relation mapping to user model

--- backend/src/packages/users/user.model.ts
+++ backend/src/packages/users/user.model.ts
@@ -1,7 +1,10 @@
+import { Model, type RelationMappings } from 'objection';
+
 import {
   AbstractModel,
   DatabaseTableName,
 } from '#libs/packages/database/database.js';
+import { SurveyModel } from '#packages/surveys/survey.model.js';
 
 class UserModel extends AbstractModel {
   public email!: string;
@@ -12,6 +15,19 @@ class UserModel extends AbstractModel {
 
   public static override get tableName(): string {
     return DatabaseTableName.USERS;
+  }
+
+  public static get relationMappings(): RelationMappings {
+    return {
+      surveys: {
+        relation: Model.HasManyRelation,
+        modelClass: SurveyModel,
+        join: {
+          from: `${DatabaseTableName.USERS}.id`,
+          to: `${DatabaseTableName.SURVEYS}.user_id`,
+        },
+      },
+    };
   }
 }
 
