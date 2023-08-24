0eeb37c6701dc66e5b14ebf3c4712f3f6a774a5d
myducck <dmitro.kucherenko@gmail.com>
2023-08-24 17:47:20 +0300

    cp-49: * common button component

--- frontend/src/libs/components/button/button.tsx
+++ frontend/src/libs/components/button/button.tsx
@@ -1,10 +1,27 @@
+import { getValidClassNames } from '#libs/helpers/get-valid-class-names.js';
+
+import styles from './styles.module.scss';
+
 type Properties = {
   label: string;
   type?: 'button' | 'submit';
+  className?: string;
+  onClick?: (event: React.MouseEvent) => void;
 };
 
-const Button: React.FC<Properties> = ({ type = 'button', label }) => (
-  <button type={type}>{label}</button>
+const Button: React.FC<Properties> = ({
+  type = 'button',
+  label,
+  className = '',
+  onClick,
+}) => (
+  <button
+    type={type}
+    className={getValidClassNames(styles['btn'], className)}
+    onClick={onClick}
+  >
+    {label}
+  </button>
 );
 
 export { Button };
