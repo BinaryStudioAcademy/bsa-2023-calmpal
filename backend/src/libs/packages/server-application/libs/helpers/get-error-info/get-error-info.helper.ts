540453de54c684206210e00bde95cc017b21b828
HIMDanny <danilbliz0201@gmail.com>
2023-08-24 14:07:08 +0300

    cp-17: * add helper extension to functions

--- backend/src/libs/packages/server-application/libs/helpers/get-error-info/get-error-info.helper.ts
+++ backend/src/libs/packages/server-application/libs/helpers/get-error-info/get-error-info.helper.ts
@@ -1,10 +1,10 @@
 import { AuthError } from '#libs/exceptions/exceptions.js';
 import { HTTPError } from '#libs/packages/http/http.js';
 
-import { getAuthErrorInfo } from './get-auth-error-info.js';
-import { getDefaultErrorInfo } from './get-default-error-info.js';
-import { getHTTPErrorInfo } from './get-http-error-info.js';
-import { getValidationErrorInfo } from './get-validation-error-info.js';
+import { getAuthErrorInfo } from './get-auth-error-info.helper.js';
+import { getDefaultErrorInfo } from './get-default-error-info.helper.js';
+import { getHTTPErrorInfo } from './get-http-error-info.helper.js';
+import { getValidationErrorInfo } from './get-validation-error-info.helper.js';
 import { type ErrorInfo, type ErrorParameter } from './types/types.js';
 
 const getErrorInfo = (error: ErrorParameter): ErrorInfo => {
