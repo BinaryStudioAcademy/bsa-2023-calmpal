dbe2e74e1e030888bd4113b61b7465f091d98978
Dana Todoriak <todoriak@gmail.com>
2023-08-24 13:44:25 +0300

    cp-22: * label component

--- mobile/src/screens/auth/components/label/styles.ts
+++ mobile/src/screens/auth/components/label/styles.ts
@@ -3,7 +3,11 @@ import { StyleSheet } from 'react-native';
 import { AppColor } from '#libs/enums/enums';
 
 const styles = StyleSheet.create({
-  labelContainer: {
+  labelText: {
+    color: AppColor.WHITE,
+    fontSize: 24,
+    fontWeight: 'bold',
+    letterSpacing: 3,
     backgroundColor: AppColor.BLUE_200,
     borderRadius: 8,
     paddingHorizontal: 12,
@@ -13,12 +17,6 @@ const styles = StyleSheet.create({
     width: 140,
     paddingTop: 14,
     paddingBottom: 14,
-  },
-  labelText: {
-    color: 'white',
-    fontSize: 24,
-    fontWeight: 'bold',
-    letterSpacing: 3,
   },
 });
 
