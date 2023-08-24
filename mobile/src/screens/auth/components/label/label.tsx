00e5214f1b54096dc4d50f2b7eb43c5a971f5519
iamAlinaaa <alinaaevns1806@gmail.com>
2023-08-24 11:07:39 +0300

    cp-23: * label structure

--- mobile/src/screens/auth/components/label/label.tsx
+++ mobile/src/screens/auth/components/label/label.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-import { Text, View } from '#libs/components/components';
+import { Text } from '#libs/components/components';
 
 import { styles } from './styles';
 
 const Label: React.FC = () => {
-  return (
-    <View style={styles.labelContainer}>
-      <Text style={styles.labelText}>CalmPal</Text>
-    </View>
-  );
+  return <Text style={styles.labelText}>CalmPal</Text>;
 };
 
 export { Label };
