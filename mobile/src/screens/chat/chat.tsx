a2cf85c63ece0af7be520fe4f2275c3ee57604c0
v.matveeva <vika030718@gmail.com>
2023-08-25 17:14:49 -0400

    cp-54: + LinearGradient for gradient BG

--- mobile/src/screens/chat/chat.tsx
+++ mobile/src/screens/chat/chat.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { ScrollView } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import { Input, View } from '#libs/components/components';
+import { AppColor } from '#libs/enums/enums';
 import { useAppForm } from '#libs/hooks/hooks';
 
 import { ChatItem, ChatLink } from './components/components';
@@ -16,29 +18,33 @@ const Chat: React.FC = () => {
   });
 
   return (
-    <View style={styles.containerWrapper}>
-      <View style={styles.container}>
-        <Input
-          control={control}
-          errors={errors}
-          name="search"
-          placeholder="Search topic"
-          setSearchQuery={setSearchQuery}
-          style={styles.input}
-        />
-        <ScrollView>
-          {data
-            .filter((item) =>
-              item.title.toLowerCase().includes(searchQuery.toLowerCase()),
-            )
-            .map((item) => (
-              <ChatItem chatItem={item} key={item.id} />
-            ))}
-        </ScrollView>
-
-        <ChatLink />
+    <LinearGradient
+      colors={[AppColor.WHITE, AppColor.BLUE_100]}
+      style={styles.linearGradient}
+    >
+      <View style={styles.containerWrapper}>
+        <View style={styles.container}>
+          <Input
+            control={control}
+            errors={errors}
+            name="search"
+            placeholder="Search topic"
+            setSearchQuery={setSearchQuery}
+            style={styles.input}
+          />
+          <ScrollView contentContainerStyle={styles.list}>
+            {data
+              .filter((item) =>
+                item.title.toLowerCase().includes(searchQuery.toLowerCase()),
+              )
+              .map((item) => (
+                <ChatItem chatItem={item} key={item.id} />
+              ))}
+          </ScrollView>
+          <ChatLink />
+        </View>
       </View>
-    </View>
+    </LinearGradient>
   );
 };
 
--- mobile/src/screens/chat/styles.ts
+++ mobile/src/screens/chat/styles.ts
@@ -3,12 +3,11 @@ import { StyleSheet } from 'react-native';
 import { AppColor } from '#libs/enums/enums';
 
 const styles = StyleSheet.create({
-  containerWrapper: { backgroundColor: AppColor.WHITE, flex: 1 },
+  containerWrapper: { flex: 1 },
   container: {
-    backgroundColor: AppColor.TRANSPARENT_BLUE_100,
     flex: 1,
     paddingBottom: 16,
-    paddingHorizontal: 16,
+    paddingHorizontal: 12, // The Main padding for all screens is 16, but we use 16-4=12 here because we add padding to the Item List to avoid shadow cutting
     paddingTop: 28,
   },
   input: {
@@ -17,8 +16,15 @@ const styles = StyleSheet.create({
     borderWidth: 0,
     fontSize: 14,
     height: 48,
+    marginHorizontal: 4, // To align the input to the borders of the list
     paddingHorizontal: 20,
     paddingVertical: 10,
+  },
+  linearGradient: {
+    flex: 1,
+  },
+  list: {
+    paddingHorizontal: 4, // To avoid problems with cutting a shadow
   },
 });
 
