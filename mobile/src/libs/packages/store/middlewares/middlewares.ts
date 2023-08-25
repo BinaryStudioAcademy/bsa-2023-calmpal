8120a2e53e24123fd089fb51828722de415127a5
BrianSammit <briansammit.cruz@gmail.com>
2023-08-25 12:59:26 -0500

    cp-27: * handleError middleware

--- mobile/src/libs/packages/store/middlewares/middlewares.ts
+++ mobile/src/libs/packages/store/middlewares/middlewares.ts
@@ -8,16 +8,14 @@ import {
 import { NotificationType } from '#libs/enums/notification/notification-type.enum';
 import { notify } from '#slices/notifications/actions';
 
-const handleError: Middleware = (store) => {
+const handleError: Middleware = ({ dispatch }) => {
   return (next: Dispatch) => {
     return (action: AnyAction) => {
       const result = next(action);
 
       if (isRejected(action)) {
-        const { message } = action.error;
-        if (message) {
-          store.dispatch(notify({ type: NotificationType.ERROR, message }));
-        }
+        const message = action.error as string;
+        dispatch(notify({ type: NotificationType.ERROR, message }));
       }
 
       return result;
