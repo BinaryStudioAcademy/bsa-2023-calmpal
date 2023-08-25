046e345a2a6e10a836d415b675202af5aa8cfdbc
v.matveeva <vika030718@gmail.com>
2023-08-25 14:35:57 -0400

    cp-54: + add chat bg-color

--- mobile/src/screens/chat/styles.ts
+++ mobile/src/screens/chat/styles.ts
@@ -3,18 +3,22 @@ import { StyleSheet } from 'react-native';
 import { AppColor } from '#libs/enums/enums';
 
 const styles = StyleSheet.create({
+  containerWrapper: { backgroundColor: AppColor.WHITE, flex: 1 },
   container: {
-    flex: 1,
-    padding: 16,
     backgroundColor: AppColor.TRANSPARENT_BLUE_100,
+    flex: 1,
+    paddingBottom: 16,
+    paddingHorizontal: 16,
+    paddingTop: 28,
   },
   input: {
     backgroundColor: AppColor.BLUE_100,
     borderRadius: 12,
+    borderWidth: 0,
+    fontSize: 14,
     height: 48,
     paddingHorizontal: 20,
     paddingVertical: 10,
-    borderWidth: 0,
   },
 });
 
