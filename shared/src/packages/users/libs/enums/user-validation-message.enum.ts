ee99f17b7f218eab5c7476db5fc4297d045331b0
Dana Todoriak <todoriak@gmail.com>
2023-08-25 11:30:15 +0300

    cp-22: + dots for user validation messages

--- shared/src/packages/users/libs/enums/user-validation-message.enum.ts
+++ shared/src/packages/users/libs/enums/user-validation-message.enum.ts
@@ -1,7 +1,7 @@
 const UserValidationMessage = {
-  EMAIL_REQUIRED: 'Email is required',
-  EMAIL_WRONG: 'Email is wrong',
-  PASSWORD_REQUIRED: 'Password is required',
+  EMAIL_REQUIRED: 'Email is required.',
+  EMAIL_WRONG: 'Email is wrong.',
+  PASSWORD_REQUIRED: 'Password is required.',
 } as const;
 
 export { UserValidationMessage };
