25d795a7198fbeaaebcf2806903838c6ac2314fd
v.matveeva <vika030718@gmail.com>
2023-08-25 14:57:19 -0400

    cp-54: * routing

--- mobile/src/navigations/main/main.tsx
+++ mobile/src/navigations/main/main.tsx
@@ -1,12 +1,16 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { getHeaderTitle } from '@react-navigation/elements';
 import { type RouteProp } from '@react-navigation/native';
 import React from 'react';
+import { type StyleProp, type ViewStyle } from 'react-native';
 
 import ChatIcon from '#assets/img/icons/chat.svg';
 import HomeIcon from '#assets/img/icons/home.svg';
 import { AppColor, MainScreenName } from '#libs/enums/enums';
 import { type TabNavigationParameterList } from '#libs/types/types';
 import { Chat } from '#screens/chat/chat';
+import { ChatHeader } from '#screens/chat/components/components';
+import { HomeHeader } from '#screens/home/components/components';
 import { Home } from '#screens/home/home';
 
 import { styles } from './styles';
@@ -52,8 +56,44 @@ const tabNavigatorOptions = ({
 const Main: React.FC = () => {
   return (
     <BottomTab.Navigator screenOptions={tabNavigatorOptions}>
-      <BottomTab.Screen name={MainScreenName.HOME} component={Home} />
-      <BottomTab.Screen name={MainScreenName.CHAT} component={Chat} />
+      <BottomTab.Screen
+        name={MainScreenName.HOME}
+        component={Home}
+        options={{
+          header: ({ route, options }): React.ReactNode => {
+            const title = getHeaderTitle(options, route.name);
+
+            return (
+              <HomeHeader
+                title={title}
+                headerStyle={options.headerStyle as StyleProp<ViewStyle>}
+                titleStyle={options.headerTitleStyle as StyleProp<ViewStyle>}
+              />
+            );
+          },
+          headerStyle: styles.headerStyle,
+          headerTitleStyle: styles.headerTitleStyle,
+        }}
+      />
+      <BottomTab.Screen
+        name={MainScreenName.CHAT}
+        component={Chat}
+        options={{
+          header: ({ route, options }): React.ReactNode => {
+            const title = getHeaderTitle(options, route.name);
+
+            return (
+              <ChatHeader
+                title={title}
+                headerStyle={options.headerStyle as StyleProp<ViewStyle>}
+                titleStyle={options.headerTitleStyle as StyleProp<ViewStyle>}
+              />
+            );
+          },
+          headerStyle: styles.headerStyle,
+          headerTitleStyle: styles.headerTitleStyle,
+        }}
+      />
     </BottomTab.Navigator>
   );
 };
--- mobile/src/navigations/main/styles.ts
+++ mobile/src/navigations/main/styles.ts
@@ -9,6 +9,20 @@ const styles = StyleSheet.create({
     height: 122,
     shadowColor: AppColor.GRAY_300,
   },
+  headerStyle: {
+    backgroundColor: AppColor.WHITE,
+    borderBottomColor: AppColor.GRAY_100,
+    borderBottomWidth: 1,
+    height: 81,
+    paddingHorizontal: 24,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  headerTitleStyle: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: AppColor.GRAY_500,
+  },
 });
 
 export { styles };
