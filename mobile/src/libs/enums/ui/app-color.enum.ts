1d131297abc0358d7720618514109fbed541b945
v.matveeva <vika030718@gmail.com>
2023-08-25 14:25:11 -0400

    cp-54: + colors

--- mobile/src/libs/enums/ui/app-color.enum.ts
+++ mobile/src/libs/enums/ui/app-color.enum.ts
@@ -5,11 +5,13 @@ const AppColor = {
   GRAY_100: '#EBF3FF',
   GRAY_200: '#CCDDF3',
   GRAY_300: '#ACB4D0',
-  GRAY_400: '#302E36',
+  GRAY_400: '#EDF2F7',
+  GRAY_500: '#302E36',
   PINK_100: '#E1A7D5',
   RED_100: '#D51A52',
   TRANSPARENT_BLUE_100: '#051E4703',
   WHITE: '#ffffff',
+  BLACK: '#000000',
 } as const;
 
 export { AppColor };
