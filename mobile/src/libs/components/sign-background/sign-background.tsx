d58d79d04fc6fb38abafcd174d48ca8c280321b9
iamAlinaaa <alinaaevns1806@gmail.com>
2023-08-25 13:22:29 +0300

    cp-51: * minor changes to naaming

--- mobile/src/libs/components/sign-background/sign-background.tsx
+++ mobile/src/libs/components/sign-background/sign-background.tsx
@@ -1,9 +1,9 @@
 import React, { type ReactNode } from 'react';
 import { Image, type ImageSourcePropType } from 'react-native';
 
-import bottomBubbleImg from '#assets/bottom-bubble.png';
-import backgroundImg from '#assets/sign-background.png';
-import topBubbleImg from '#assets/top-bubble.png';
+import bottomBubbleImage from '#assets/bottom-bubble.png';
+import backgroundImage from '#assets/sign-background.png';
+import topBubbleImage from '#assets/top-bubble.png';
 import { View } from '#libs/components/components';
 
 import { styles } from './styles';
@@ -17,17 +17,17 @@ const SignBackground: React.FC<Properties> = ({ children }) => {
     <View style={styles.wrapper}>
       <View style={styles.backgroundWrapper}>
         <Image
-          source={backgroundImg as ImageSourcePropType}
-          style={styles.backgroundImg}
+          source={backgroundImage as ImageSourcePropType}
+          style={styles.backgroundImage}
         />
       </View>
       <Image
-        source={topBubbleImg as ImageSourcePropType}
-        style={styles.topBubbleImg}
+        source={topBubbleImage as ImageSourcePropType}
+        style={styles.topBubbleImage}
       />
       <Image
-        source={bottomBubbleImg as ImageSourcePropType}
-        style={styles.bottomBubbleImg}
+        source={bottomBubbleImage as ImageSourcePropType}
+        style={styles.bottomBubbleImage}
       />
       {children}
     </View>
