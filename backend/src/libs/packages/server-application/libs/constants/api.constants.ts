d0fd70a143ebf7b67f3d0caf1969058ea323265c
HIMDanny <danilbliz0201@gmail.com>
2023-08-25 21:28:21 +0300

    cp-72: + all unregistered routes as white routes

--- backend/src/libs/packages/server-application/libs/constants/api.constants.ts
+++ backend/src/libs/packages/server-application/libs/constants/api.constants.ts
@@ -1,9 +1,12 @@
 import { APIPath } from '#libs/enums/enums.js';
 import { AuthApiPath } from '#packages/auth/auth.js';
 
+const SERVED_PAGE_PATH = '/*';
+
 const WHITE_ROUTES = [
   { path: `/api/v1${APIPath.AUTH}${AuthApiPath.SIGN_UP}`, methods: ['POST'] },
   { path: `/api/v1${APIPath.AUTH}${AuthApiPath.SIGN_IN}`, methods: ['POST'] },
+  { path: `${SERVED_PAGE_PATH}`, methods: ['GET'] },
 ] as const;
 
-export { WHITE_ROUTES };
+export { SERVED_PAGE_PATH, WHITE_ROUTES };
