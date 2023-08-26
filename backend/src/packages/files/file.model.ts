d1fd51f7b68a79b13dafc1a3ffd5f7f6e0c4fa59
Анастасія Лисенко <anastasiialysenko0@gmail.com>
2023-08-26 18:22:37 +0300

    cp-70: + file model

---
+++ backend/src/packages/files/file.model.ts
@@ -0,0 +1,16 @@
+import {
+  AbstractModel,
+  DatabaseTableName,
+} from '#libs/packages/database/database.js';
+
+class FileModel extends AbstractModel {
+  public url!: string;
+
+  public contentType!: string;
+
+  public static override get tableName(): string {
+    return DatabaseTableName.FILES;
+  }
+}
+
+export { FileModel };
