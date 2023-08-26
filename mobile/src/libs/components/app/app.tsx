4b6dffd4cf3bd6de9e92dec1b9e82e8cccf8bcbf
Dana Todoriak <todoriak@gmail.com>
2023-08-26 21:38:04 +0300

    cp-76: + splash screen

--- mobile/src/libs/components/app/app.tsx
+++ mobile/src/libs/components/app/app.tsx
@@ -3,6 +3,7 @@ import 'fast-text-encoding';
 import { NavigationContainer } from '@react-navigation/native';
 import React, { type FC } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
 import { Provider as StoreProvider } from 'react-redux';
 
 import { store } from '#libs/packages/store/store';
@@ -11,6 +12,8 @@ import { Root as RootNavigation } from '#navigations/navigations';
 import { styles } from './styles';
 
 const App: FC = () => {
+  SplashScreen.hide();
+
   return (
     <StoreProvider store={store.instance}>
       <GestureHandlerRootView style={styles.root}>
