a5ec61bf63a933d6977e2dceef9b062eb29fed6c
myducck <dmitro.kucherenko@gmail.com>
2023-08-27 18:59:07 +0300

    cp-49: * app component

--- frontend/src/libs/components/app/app.tsx
+++ frontend/src/libs/components/app/app.tsx
@@ -7,13 +7,20 @@ import {
   useLocation,
 } from '#libs/hooks/hooks.js';
 import { actions as authActions } from '#slices/auth/auth.js';
+import { actions as surveyActions } from '#slices/survey/survey.js';
 import { actions as userActions } from '#slices/users/users.js';
 
 const App: React.FC = () => {
   const { pathname } = useLocation();
   const dispatch = useAppDispatch();
-  const { authenticatedUserDataStatus } = useAppSelector(({ auth }) => ({
+  const {
+    authenticatedUserId,
+    authenticatedUserDataStatus,
+    surveyPreferencesDataStatus,
+  } = useAppSelector(({ auth, survey }) => ({
+    authenticatedUserId: auth.authenticatedUser?.id,
     authenticatedUserDataStatus: auth.authenticatedUserDataStatus,
+    surveyPreferencesDataStatus: survey.dataStatus,
   }));
 
   const isRoot = pathname === AppRoute.ROOT;
@@ -28,7 +35,18 @@ const App: React.FC = () => {
     void dispatch(authActions.getAuthenticatedUser());
   }, [dispatch]);
 
-  if (authenticatedUserDataStatus === DataStatus.PENDING) {
+  useEffect(() => {
+    if (authenticatedUserId) {
+      void dispatch(
+        surveyActions.getUserSurveyPreferences({ userId: authenticatedUserId }),
+      );
+    }
+  }, [authenticatedUserId]);
+
+  if (
+    authenticatedUserDataStatus === DataStatus.PENDING ||
+    surveyPreferencesDataStatus === DataStatus.PENDING
+  ) {
     return <Loader />;
   }
 
