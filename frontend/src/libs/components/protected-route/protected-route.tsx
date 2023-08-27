aaf387c4c9c3c8c3dd269110b003b0a53d5fa0ef
myducck <dmitro.kucherenko@gmail.com>
2023-08-27 18:58:03 +0300

    cp-49: * adapt survey components to slice

--- frontend/src/libs/components/protected-route/protected-route.tsx
+++ frontend/src/libs/components/protected-route/protected-route.tsx
@@ -2,7 +2,7 @@ import { type ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { AppRoute } from '#libs/enums/enums.js';
-import { useAppSelector } from '#libs/hooks/hooks.js';
+import { useAppSelector, useLocation } from '#libs/hooks/hooks.js';
 import { type ValueOf } from '#libs/types/types.js';
 
 type Properties = {
@@ -14,10 +14,21 @@ const ProtectedRoute: React.FC<Properties> = ({
   children,
   redirectPath = AppRoute.SIGN_IN,
 }) => {
-  const { authenticatedUser } = useAppSelector(({ auth }) => ({
-    authenticatedUser: auth.authenticatedUser,
-  }));
+  const { pathname } = useLocation();
+
+  const { authenticatedUser, surveyPreferences } = useAppSelector(
+    ({ auth, survey }) => ({
+      authenticatedUser: auth.authenticatedUser,
+      surveyPreferences: survey.isSurveyCompleted,
+    }),
+  );
   const hasUser = Boolean(authenticatedUser);
+  const hasNoSurvey =
+    hasUser && !surveyPreferences && pathname !== AppRoute.SURVEY;
+
+  if (hasNoSurvey) {
+    return <Navigate to={AppRoute.SURVEY} />;
+  }
 
   return hasUser ? <>{children}</> : <Navigate to={redirectPath} />;
 };
--- frontend/src/pages/surveys/survey.tsx
+++ frontend/src/pages/surveys/survey.tsx
@@ -1,13 +1,36 @@
+import { Navigate } from 'react-router-dom';
+
 import { Header } from '#libs/components/components.js';
-import { useCallback } from '#libs/hooks/hooks.js';
+import { AppRoute } from '#libs/enums/app-route.enum.js';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useCallback,
+} from '#libs/hooks/hooks.js';
+import { actions as surveyActions } from '#slices/survey/survey.js';
 
 import { PreferencesStep } from './components/components.js';
 import styles from './styles.module.scss';
 
 const Survey: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { userId, surveyPreferences } = useAppSelector(({ auth, survey }) => ({
+    userId: auth.authenticatedUser?.id,
+    surveyPreferences: survey.isSurveyCompleted,
+  }));
+
   const onSubmit = useCallback((options: string[]) => {
-    return options;
+    void dispatch(
+      surveyActions.createUserSurveyPreferences({
+        userId: userId as number,
+        preferences: options,
+      }),
+    );
   }, []);
+
+  if (surveyPreferences) {
+    return <Navigate to={AppRoute.ROOT} />;
+  }
 
   return (
     <div className={styles['container']}>
