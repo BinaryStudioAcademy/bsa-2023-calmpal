3555e2b823b5b0f81e11c62860dea7813c977b93
Анастасія Лисенко <anastasiialysenko0@gmail.com>
2023-08-28 01:06:32 +0300

    cp-70: * shared enums

--- shared/src/libs/enums/exception-message.enum.ts
+++ shared/src/libs/enums/exception-message.enum.ts
@@ -4,6 +4,7 @@ const ExceptionMessage = {
   USER_NOT_FOUND: 'User with these credentials was not found.',
   INCORRECT_CREDENTIALS: 'Incorrect credentials.',
   USER_ALREADY_EXISTS: 'User already exists.',
+  INCORRECT_FILE_TYPE: 'Content type of the file is not in png, jpeg.',
 } as const;
 
 export { ExceptionMessage };
