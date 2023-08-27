e79854ccf0a3a07b685ed59645569880c554199d
myducck <dmitro.kucherenko@gmail.com>
2023-08-27 15:13:47 +0300

    cp-49: * components exports

--- frontend/src/libs/components/components.ts
+++ frontend/src/libs/components/components.ts
@@ -6,5 +6,7 @@ export { Link } from './link/link.js';
 export { Loader } from './loader/loader.js';
 export { ProtectedRoute } from './protected-route/protected-route.js';
 export { RouterProvider } from './router-provider/router-provider.jsx';
+export { SurveyCategory } from './survey-category/survey-category.js';
+export { Textarea } from './textarea/textarea.js';
 export { Provider as StoreProvider } from 'react-redux';
 export { Outlet as RouterOutlet } from 'react-router-dom';
