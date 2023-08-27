aea2e587720c798ca63ac5aa1b5e0d03d49c9b63
myducck <dmitro.kucherenko@gmail.com>
2023-08-27 18:36:42 +0300

    cp-49: + survey dtos

--- shared/src/packages/surveys/libs/types/types.ts
+++ shared/src/packages/surveys/libs/types/types.ts
@@ -1,1 +1,4 @@
+export { type SurveyGetAllItemResponseDto } from './survey-get-all-item-response-dto.types.js';
+export { type SurveyGetAllResponseDto } from './survey-get-all-response-dto.types.js';
 export { type SurveyInputDto } from './survey-input-dto.types.js';
+export { type SurveyRequestDto } from './survey-request-dto.types.js';
--- shared/src/packages/surveys/survey.ts
+++ shared/src/packages/surveys/survey.ts
@@ -2,5 +2,10 @@ export {
   SurveyTextareaOptions,
   SurveyValidationMessage,
 } from './libs/enums/enums.js';
-export { type SurveyInputDto } from './libs/types/types.js';
+export {
+  type SurveyGetAllItemResponseDto,
+  type SurveyGetAllResponseDto,
+  type SurveyInputDto,
+  type SurveyRequestDto,
+} from './libs/types/types.js';
 export { surveyInput as surveyInputValidationSchema } from './libs/validation-schemas/validation-schemas.js';
