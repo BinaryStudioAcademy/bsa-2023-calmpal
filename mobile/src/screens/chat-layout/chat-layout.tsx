8f46d0e207429191a7a359116817c04624539f5b
Dana Todoriak <todoriak@gmail.com>
2023-08-27 14:55:34 +0300

    cp-60: * default values and mocked data to libs/constants.ts

--- mobile/src/screens/chat-layout/chat-layout.tsx
+++ mobile/src/screens/chat-layout/chat-layout.tsx
@@ -10,7 +10,7 @@ import {
 } from '#libs/hooks/hooks';
 
 import { ChatInput, MessageItem } from './components/components';
-import { PREVIOUS_USER } from './libs/constants';
+import { DEFAULT_VALUES, MOCKED_DATA, PREVIOUS_USER } from './libs/constants';
 import { styles } from './styles';
 
 type Message = {
@@ -20,59 +20,11 @@ type Message = {
 };
 
 const ChatLayout: React.FC = () => {
-  const mockedData = [
-    {
-      id: 1,
-      isUser: true,
-      message:
-        'Hi, Doctor. I’ve been feeling really down lately, and I’m not sure why. Can you help me? 😢😭',
-    },
-    {
-      id: 2,
-      isUser: false,
-      message:
-        'Of course! I’m here to support you. 🙂 Can you tell me more about how you’ve been feeling? Any specific symptoms or changes in your daily life?',
-    },
-    {
-      id: 3,
-      isUser: true,
-      message: 'Ok, here’s the symptoms for me',
-    },
-    {
-      id: 4,
-      isUser: true,
-      message: 'I’ll be there in 2 mins ⏰',
-    },
-    {
-      id: 5,
-      isUser: false,
-      message:
-        'I’ve been experiencing persistent sadness, loss of interest in things I used to enjoy. It’s been affecting my work and relationships too!! 💊❌😵',
-    },
-    {
-      id: 6,
-      isUser: false,
-      message: 'I’ll be there in 2 mins ⏰',
-    },
-    {
-      id: 7,
-      isUser: false,
-      message:
-        'I’ve been5435345 experiencing persistent sadness, loss of interest in things I used to enjoy. It’s been affecting my work and relationships too!! 💊❌😵',
-    },
-    {
-      id: 8,
-      isUser: true,
-      message:
-        'I’ve been234234 experiencing persistent sadness, loss of interest in things I used to enjoy. It’s been affecting my work and relationships too!! 💊❌😵',
-    },
-  ];
-
   const { control, handleSubmit, reset } = useAppForm<{ text: string }>({
-    defaultValues: { text: '' },
+    defaultValues: DEFAULT_VALUES,
   });
 
-  const [messages, setMessages] = useState<Message[]>(mockedData);
+  const [messages, setMessages] = useState<Message[]>(MOCKED_DATA);
   const scrollViewReference = useRef<ScrollView | null>(null);
 
   const scrollViewToEnd = (): void => {
--- mobile/src/screens/chat-layout/libs/constants.ts
+++ mobile/src/screens/chat-layout/libs/constants.ts
@@ -1,3 +1,55 @@
 const PREVIOUS_USER = 1;
 
-export { PREVIOUS_USER };
+const DEFAULT_VALUES = {
+  text: '',
+};
+
+const MOCKED_DATA = [
+  {
+    id: 1,
+    isUser: true,
+    message:
+      'Hi, Doctor. I’ve been feeling really down lately, and I’m not sure why. Can you help me? 😢😭',
+  },
+  {
+    id: 2,
+    isUser: false,
+    message:
+      'Of course! I’m here to support you. 🙂 Can you tell me more about how you’ve been feeling? Any specific symptoms or changes in your daily life?',
+  },
+  {
+    id: 3,
+    isUser: true,
+    message: 'Ok, here’s the symptoms for me',
+  },
+  {
+    id: 4,
+    isUser: true,
+    message: 'I’ll be there in 2 mins ⏰',
+  },
+  {
+    id: 5,
+    isUser: false,
+    message:
+      'I’ve been experiencing persistent sadness, loss of interest in things I used to enjoy. It’s been affecting my work and relationships too!! 💊❌😵',
+  },
+  {
+    id: 6,
+    isUser: false,
+    message: 'I’ll be there in 2 mins ⏰',
+  },
+  {
+    id: 7,
+    isUser: false,
+    message:
+      'I’ve been5435345 experiencing persistent sadness, loss of interest in things I used to enjoy. It’s been affecting my work and relationships too!! 💊❌😵',
+  },
+  {
+    id: 8,
+    isUser: true,
+    message:
+      'I’ve been234234 experiencing persistent sadness, loss of interest in things I used to enjoy. It’s been affecting my work and relationships too!! 💊❌😵',
+  },
+];
+
+export { DEFAULT_VALUES, MOCKED_DATA, PREVIOUS_USER };
