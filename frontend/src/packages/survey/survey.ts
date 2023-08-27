db92998796b8dbfa842743fd60ebbf546b698b59
myducck <dmitro.kucherenko@gmail.com>
2023-08-27 18:51:03 +0300

    cp-49: * imports from shared

--- frontend/src/packages/survey/survey.ts
+++ frontend/src/packages/survey/survey.ts
@@ -1,3 +1,8 @@
 export { SurveyTextareaOptions } from './libs/enums/enums.js';
-export { type SurveyInputDto } from './libs/types/types.js';
+export {
+  type SurveyGetAllItemResponseDto,
+  type SurveyGetAllResponseDto,
+  type SurveyInputDto,
+  type SurveyRequestDto,
+} from './libs/types/types.js';
 export { surveyInputValidationSchema } from './libs/validation-shemas/validation-schemas.js';
