16d817bd597c030b86c6f959d2cbbea1acb39c24
myducck <dmitro.kucherenko@gmail.com>
2023-08-28 20:50:33 +0300

    cp-84: * backend user dto imports

--- backend/src/packages/users/libs/types/types.ts
+++ backend/src/packages/users/libs/types/types.ts
@@ -2,7 +2,6 @@ export { type UserCommonQueryResponse } from './user-common-query-response.js';
 export { type UserCreateQueryPayload } from './user-create-query-payload.js';
 export {
   type UserAuthResponseDto,
-  type UserGetAllResponseDto,
   type UserSignUpRequestDto,
   type UserSignUpResponseDto,
 } from 'shared/build/index.js';
