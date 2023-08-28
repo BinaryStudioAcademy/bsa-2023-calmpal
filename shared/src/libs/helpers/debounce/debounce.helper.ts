9c3d274030d2a2c93a4049509cc867d1440a1e7e
v.matveeva <vika030718@gmail.com>
2023-08-28 19:28:31 -0400

    cp-54: * fix debounce

--- shared/src/libs/helpers/debounce/debounce.helper.ts
+++ shared/src/libs/helpers/debounce/debounce.helper.ts
@@ -3,12 +3,24 @@ import { type Dispatch, type SetStateAction } from 'react';
 
 const DEBOUNCE_DELAY = 300;
 
+let debouncedFunction: ReturnType<typeof libraryDebounce> | undefined;
+
 const debounce = (
   setSearchQuery: Dispatch<SetStateAction<string>>,
 ): ((text: string) => void) => {
-  return libraryDebounce((text: string) => {
+  const debounced = libraryDebounce((text: string) => {
     setSearchQuery(text);
   }, DEBOUNCE_DELAY);
+
+  return (text: string): void => {
+    if (debouncedFunction) {
+      debounced.clear();
+      debouncedFunction.clear();
+    }
+
+    debouncedFunction = debounced;
+    debounced(text);
+  };
 };
 
 export { debounce };
