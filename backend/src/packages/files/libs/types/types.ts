7ce24066651d2bc8852deed0055515d06b06ab2d
Анастасія Лисенко <anastasiialysenko0@gmail.com>
2023-08-28 23:20:51 +0300

    cp-70: * changed imported type

--- backend/src/packages/files/libs/types/types.ts
+++ backend/src/packages/files/libs/types/types.ts
@@ -2,7 +2,7 @@ export { type FileCommonQueryResponse } from './file-common-query-response.js';
 export { type FileCreateQueryPayload } from './file-create-query-payload.js';
 export {
   type AWSUploadRequestDto,
-  type FileGetAllResponseDto,
+  type FileGetAllItemResponseDto,
   type FileUploadRequestDto,
   type FileUploadResponseDto,
 } from 'shared/build/index.js';
