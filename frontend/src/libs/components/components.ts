c6d8dd487bcada33c856070a10728fc05c27d7ba
HIMDanny <danilbliz0201@gmail.com>
2023-08-28 20:36:37 +0300

    cp-81: + export switch component

--- frontend/src/libs/components/components.ts
+++ frontend/src/libs/components/components.ts
@@ -6,5 +6,6 @@ export { Link } from './link/link.js';
 export { Loader } from './loader/loader.js';
 export { ProtectedRoute } from './protected-route/protected-route.js';
 export { RouterProvider } from './router-provider/router-provider.jsx';
+export { Switch } from './switch/switch.js';
 export { Provider as StoreProvider } from 'react-redux';
 export { Outlet as RouterOutlet } from 'react-router-dom';
