56a13a60a1f6294ef76d273695c5c51bd363235c
Анастасія Лисенко <anastasiialysenko0@gmail.com>
2023-08-28 22:40:40 +0300

    cp-70: + requested changes

--- backend/src/packages/files/files.ts
+++ backend/src/packages/files/files.ts
@@ -21,8 +21,7 @@ const fileService = new FileService({
 });
 const fileController = new FileController(logger, fileService);
 
-export { awsService, fileController, fileService };
-export { type AWSService } from './aws.service.js';
+export { fileController, fileService };
 export { FileModel } from './file.model.js';
 export { type FileService } from './file.service.js';
 export {
