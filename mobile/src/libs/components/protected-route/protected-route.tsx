a9787d18b476282bb384b6830285edee1ac75daf
Dana Todoriak <todoriak@gmail.com>
2023-08-28 19:46:59 +0300

    cp-25: * user existence check to use Boolean(user)

--- mobile/src/libs/components/protected-route/protected-route.tsx
+++ mobile/src/libs/components/protected-route/protected-route.tsx
@@ -5,15 +5,15 @@ import { RootScreenName } from '#libs/enums/enums';
 import { useAppSelector, useEffect, useNavigation } from '#libs/hooks/hooks';
 import { type RootNavigationParameterList } from '#libs/types/types';
 
-type Proptertis = {
+type Properties = {
   children: ReactNode;
 };
 
-const ProtectedRoute: React.FC<Proptertis> = ({ children }) => {
+const ProtectedRoute: React.FC<Properties> = ({ children }) => {
   const { user } = useAppSelector((store) => store.auth);
   const navigation =
     useNavigation<NativeStackNavigationProp<RootNavigationParameterList>>();
-  const hasUser = Boolean(user?.id);
+  const hasUser = Boolean(user);
 
   useEffect(() => {
     if (!hasUser) {
