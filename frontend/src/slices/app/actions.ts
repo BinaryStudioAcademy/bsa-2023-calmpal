33106029b5923ddb37cf41d39a7cdc9647d85ea3
juanc-escobar <juanc-escobar@hotmail.com>
2023-08-28 12:32:00 -0500

    cp-21: * Destructuring extra arguments

--- frontend/src/slices/app/actions.ts
+++ frontend/src/slices/app/actions.ts
@@ -1,13 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { type NotificationPayload } from '#libs/packages/notification/libs/types/types.js';
-import { type Notification } from '#libs/packages/notification/notification.package.js';
 import { type AsyncThunkConfig } from '#libs/types/types.js';
 
 const notify = createAsyncThunk<unknown, NotificationPayload, AsyncThunkConfig>(
   'app/notify',
   (payload, { extra }) => {
-    const notification: Notification = extra.notification;
+    const { notification } = extra;
     const { type, message } = payload;
 
     notification[type](message);
