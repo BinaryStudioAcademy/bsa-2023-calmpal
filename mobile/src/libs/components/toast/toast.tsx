9c31b3a82432b5be1f3ac32ab0b2d673131dd0bc
BrianSammit <briansammit.cruz@gmail.com>
2023-08-28 11:03:47 -0500

    cp-27: * Type properties

--- mobile/src/libs/components/toast/toast.tsx
+++ mobile/src/libs/components/toast/toast.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { type ValueOf } from 'react-native-gesture-handler/lib/typescript/typeUtils';
+import {
+  type ToastConfig,
+  type ToastConfigParams,
+} from 'react-native-toast-message';
 import LibraryToast, { ErrorToast } from 'react-native-toast-message';
+
+import { type NotificationType } from '#libs/enums/enums';
 
 import { styles } from './styles';
 
 type Properties = {
-  type?: string;
-  text1?: string;
-  text2?: string;
+  type?: ValueOf<typeof NotificationType>;
+  title?: string;
+  message?: string;
 };
 
-const toastConfig = {
-  error: (properties: Properties): React.ReactNode => (
+const toastConfig: ToastConfig = {
+  error: (properties: ToastConfigParams<Properties>): React.ReactNode => (
     <ErrorToast
       {...properties}
       style={styles.errorToast}
