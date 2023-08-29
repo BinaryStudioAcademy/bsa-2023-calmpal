732270e2b6de9c76cb1f8664448b0f17af50f5e9
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-08-29 15:04:40 +0300

    cp-78: * styling and functionality in progress bar

---
+++ frontend/src/libs/components/meditation-player/components/audio-player/progress-bar/progress-bar.tsx
@@ -0,0 +1,49 @@
+import { formatTime } from '#libs/helpers/helpers.js';
+import { useCallback } from '#libs/hooks/hooks.js';
+
+import styles from './styles.module.scss';
+
+type Properties = {
+  progressBarReference: React.RefObject<HTMLInputElement | null>;
+  audioReference: React.RefObject<HTMLAudioElement | null>;
+  duration: number;
+  timeProgress: number;
+};
+
+const ProgressBar: React.FC<Properties> = ({
+  progressBarReference,
+  audioReference,
+  duration,
+  timeProgress,
+}) => {
+  const handleProgressChange: React.ChangeEventHandler<HTMLInputElement> =
+    useCallback(
+      (event) => {
+        if (audioReference.current && progressBarReference.current) {
+          const newTime = Number.parseFloat(event.target.value);
+          if (!Number.isNaN(newTime)) {
+            audioReference.current.currentTime = newTime;
+          }
+        }
+      },
+      [audioReference, progressBarReference],
+    );
+
+  return (
+    <div>
+      <div className={styles['wrapper']}>
+        <span className={styles['time']}>{formatTime(timeProgress)}</span>
+        <span className={styles['time']}>{formatTime(duration)}</span>
+      </div>
+      <input
+        className={styles['progress']}
+        type="range"
+        ref={progressBarReference as React.RefObject<HTMLInputElement>}
+        defaultValue="0"
+        onChange={handleProgressChange}
+      />
+    </div>
+  );
+};
+
+export { ProgressBar };
