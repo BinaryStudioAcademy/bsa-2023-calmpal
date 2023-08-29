83728640b1ba50d74801b3edf6fa5c5e6ec610c2
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-08-29 22:14:05 +0300

    cp-78: * button component and it style

--- frontend/src/libs/components/button/button.tsx
+++ frontend/src/libs/components/button/button.tsx
@@ -1,13 +1,24 @@
+import { type ReactNode } from 'react';
+
 import styles from './styles.module.scss';
 
 type Properties = {
-  label: string;
+  label?: string;
   type?: 'button' | 'submit';
+  children?: ReactNode;
+  style?: 'primary' | 'secondary' | 'rounded' | 'rounded-transparent';
+  onClick?: () => void;
 };
 
-const Button: React.FC<Properties> = ({ type = 'button', label }) => (
-  <button type={type} className={styles['submit']}>
-    {label}
+const Button: React.FC<Properties> = ({
+  type = 'button',
+  label,
+  children,
+  style = 'primary',
+  onClick,
+}) => (
+  <button type={type} className={styles[style]} onClick={onClick}>
+    {label ?? children}
   </button>
 );
 
