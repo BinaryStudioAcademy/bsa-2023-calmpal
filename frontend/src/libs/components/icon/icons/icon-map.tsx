d2eb23335713b229118459cfc4d7e37f41b7dcc0
evgen <evgenkitan@gmail.com>
2023-08-30 13:38:21 +0300

    cp-55: * icon component

--- frontend/src/libs/components/icon/icons/icon-map.tsx
+++ frontend/src/libs/components/icon/icons/icon-map.tsx
@@ -1,17 +1,16 @@
 import { type ValueOf } from '#/libs/types/types.js';
+import { ReactComponent as Arrow } from '#assets/icons/arrow.svg';
+import { ReactComponent as Plus } from '#assets/icons/plus.svg';
+import { ReactComponent as TrashBox } from '#assets/icons/trash-box.svg';
 import { IconNameToIcon } from '#libs/enums/enums.js';
-
-import { ReactComponent as Arrow } from './arrow.svg';
-import { ReactComponent as Plus } from './plus.svg';
-import { ReactComponent as TrashBox } from './trash-box.svg';
 
 const iconNameToPlainSvgMap: Record<
   ValueOf<typeof IconNameToIcon>,
-  JSX.Element
+  React.FunctionComponent<React.SVGProps<SVGSVGElement>>
 > = {
-  [IconNameToIcon.ARROW]: <Arrow />,
-  [IconNameToIcon.PLUS]: <Plus />,
-  [IconNameToIcon.TRASHBOX]: <TrashBox />,
+  [IconNameToIcon.ARROW]: Arrow,
+  [IconNameToIcon.PLUS]: Plus,
+  [IconNameToIcon.TRASHBOX]: TrashBox,
 };
 
 export { iconNameToPlainSvgMap };
