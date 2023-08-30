e72ebb2644c7c85d219e98648a9b9eae530ca851
myducck <dmitro.kucherenko@gmail.com>
2023-08-30 15:05:55 +0300

    cp-86: * app component and styles

--- frontend/src/libs/components/app/app.tsx
+++ frontend/src/libs/components/app/app.tsx
@@ -1,18 +1,13 @@
-import home from '#assets/img/home.svg';
-import { Loader, RouterOutlet, Sidebar } from '#libs/components/components.js';
-import { AppRoute, DataStatus } from '#libs/enums/enums.js';
+import { Loader, RouterOutlet } from '#libs/components/components.js';
+import { DataStatus } from '#libs/enums/enums.js';
 import {
   useAppDispatch,
   useAppSelector,
   useEffect,
-  useLocation,
 } from '#libs/hooks/hooks.js';
 import { actions as authActions } from '#slices/auth/auth.js';
 
-import styles from './styles.module.scss';
-
 const App: React.FC = () => {
-  const { pathname } = useLocation();
   const dispatch = useAppDispatch();
   const { authenticatedUserDataStatus } = useAppSelector(({ auth }) => ({
     authenticatedUserDataStatus: auth.authenticatedUserDataStatus,
@@ -22,23 +17,11 @@ const App: React.FC = () => {
     void dispatch(authActions.getAuthenticatedUser());
   }, [dispatch]);
 
-  const displaySidebar =
-    pathname !== AppRoute.SIGN_IN && pathname !== AppRoute.SIGN_UP;
-
   if (authenticatedUserDataStatus === DataStatus.PENDING) {
     return <Loader />;
   }
 
-  return (
-    <div className={styles['app-container']}>
-      {displaySidebar && (
-        <Sidebar routes={[{ path: AppRoute.ROOT, name: 'home', icon: home }]} />
-      )}
-      <div className={styles['body-container']}>
-        <RouterOutlet />
-      </div>
-    </div>
-  );
+  return <RouterOutlet />;
 };
 
 export { App };
