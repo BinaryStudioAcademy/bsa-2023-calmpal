c92a37b49a4836f54c9df64bcc4b6abded4b5374
myducck <dmitro.kucherenko@gmail.com>
2023-08-30 13:37:37 +0300

    cp-49: * survey validation schema

--- shared/src/packages/surveys/libs/validation-schemas/survey.validation-schema.ts
+++ shared/src/packages/surveys/libs/validation-schemas/survey.validation-schema.ts
@@ -6,17 +6,17 @@ import {
 } from '../enums/enums.js';
 import { type SurveyInputDto } from '../types/types.js';
 
-const minArrayLength = 1;
+const MINIMUM_ARRAY_LENGTH = 1;
 
 const surveyInput = joi.object<SurveyInputDto, true>({
-  options: joi.array().items(joi.string()).min(minArrayLength).messages({
+  options: joi.array().items(joi.string()).min(MINIMUM_ARRAY_LENGTH).messages({
     'array.min': SurveyValidationMessage.OPTION_REQUIRED,
   }),
   textarea: joi.string().when('options', {
     is: joi
       .array()
       .has(joi.string().valid(SurveyTextareaOptions.OTHER))
-      .min(minArrayLength),
+      .min(MINIMUM_ARRAY_LENGTH),
     then: joi.string().trim().required().messages({
       'any.required': SurveyValidationMessage.TEXT_REQUIRED,
       'string.empty': SurveyValidationMessage.TEXT_REQUIRED,
