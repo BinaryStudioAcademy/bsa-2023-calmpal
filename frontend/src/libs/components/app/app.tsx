1a0c6cfe009dfdeb102f4d3b2c63ee99c3ebdbe8
myducck <dmitro.kucherenko@gmail.com>
2023-08-30 23:58:40 +0300

    cp-49: * linter errors

--- frontend/src/libs/components/app/app.tsx
+++ frontend/src/libs/components/app/app.tsx
@@ -4,22 +4,17 @@ import {
   useAppDispatch,
   useAppSelector,
   useEffect,
-  useLocation,
 } from '#libs/hooks/hooks.js';
 import { actions as authActions } from '#slices/auth/auth.js';
 
 const App: React.FC = () => {
-  const { pathname } = useLocation();
   const dispatch = useAppDispatch();
-  const {
-    authenticatedUser,
-    authenticatedUserDataStatus,
-    surveyPreferencesDataStatus,
-  } = useAppSelector(({ auth }) => ({
-    authenticatedUser: auth.authenticatedUser,
-    authenticatedUserDataStatus: auth.authenticatedUserDataStatus,
-    surveyPreferencesDataStatus: auth.surveyPreferencesDataStatus,
-  }));
+  const { authenticatedUserDataStatus, surveyPreferencesDataStatus } =
+    useAppSelector(({ auth }) => ({
+      authenticatedUser: auth.authenticatedUser,
+      authenticatedUserDataStatus: auth.authenticatedUserDataStatus,
+      surveyPreferencesDataStatus: auth.surveyPreferencesDataStatus,
+    }));
 
   useEffect(() => {
     void dispatch(authActions.getAuthenticatedUser());
