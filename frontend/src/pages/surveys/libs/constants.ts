820de62cb1076285ec43841bf2cdcb257b9e4d1c
myducck <dmitro.kucherenko@gmail.com>
2023-08-31 13:18:04 +0300

    cp-49: * survey coponents constants

--- frontend/src/pages/surveys/libs/constants.ts
+++ frontend/src/pages/surveys/libs/constants.ts
@@ -1,12 +1,17 @@
-import { type SurveyInputDto } from '#packages/survey/survey.js';
+import {
+  PREFERENCES_OTHER_CATEGORY,
+  type SurveyInputDto,
+} from '#packages/survey/survey.js';
 
 const TEXTAREA_MAX_LENGTH = 1000;
-
 const TEXTAREA_ROWS_COUNT = 3;
 
+const INVALID_ARRAY_INDEX = -1;
+const SPLICE_COUNT = 1;
+
 const DEFAULT_SURVEY_PAYLOAD: SurveyInputDto = {
-  options: [],
-  textarea: '',
+  preferences: [],
+  other: '',
 };
 
 const PREFERENCES_CATEGORIES: string[] = [
@@ -15,11 +20,14 @@ const PREFERENCES_CATEGORIES: string[] = [
   'Improve sleep quality',
   'Get over with depression',
   'Improve focus',
+  PREFERENCES_OTHER_CATEGORY,
 ];
 
 export {
   DEFAULT_SURVEY_PAYLOAD,
+  INVALID_ARRAY_INDEX,
   PREFERENCES_CATEGORIES,
+  SPLICE_COUNT,
   TEXTAREA_MAX_LENGTH,
   TEXTAREA_ROWS_COUNT,
 };
