42eaf8cb6aa4c1d08c353e5355a67837f9bf157d
Yevhenii Kitan <92929504+Ekit18@users.noreply.github.com>
2023-09-01 00:21:42 +0300

    cp-123: * fix enum import

--- frontend/src/slices/auth/actions.ts
+++ frontend/src/slices/auth/actions.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-import { AppRoute } from '#libs/enums/app-route.enum.js';
+import { AppRoute } from '#libs/enums/enum.js';
 import { storage, StorageKey } from '#libs/packages/storage/storage.js';
 import { type AsyncThunkConfig } from '#libs/types/types.js';
 import {
