c2815eab94c3056e1336f95ad575136706b9903d
BrianSammit <briansammit.cruz@gmail.com>
2023-08-31 15:47:49 -0500

    cp-128: * auth action to check for token when app first load

--- mobile/src/slices/auth/actions.ts
+++ mobile/src/slices/auth/actions.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-import { StorageKey } from '#libs/packages/storage/storage';
+import { storage, StorageKey } from '#libs/packages/storage/storage';
 import { type AsyncThunkConfig } from '#libs/types/types';
 import {
   type UserAuthResponseDto,
@@ -37,13 +37,19 @@ const signIn = createAsyncThunk<
 });
 
 const getAuthenticatedUser = createAsyncThunk<
-  UserAuthResponseDto,
+  UserAuthResponseDto | null,
   undefined,
   AsyncThunkConfig
->(`${sliceName}/get-authenticated-user`, (_, { extra }) => {
+>(`${sliceName}/get-authenticated-user`, async (_, { extra }) => {
   const { authApi } = extra;
 
-  return authApi.getAuthenticatedUser();
+  const hasToken = await storage.has(StorageKey.TOKEN);
+
+  if (hasToken) {
+    return await authApi.getAuthenticatedUser();
+  }
+
+  return null;
 });
 
 export { getAuthenticatedUser, signIn, signUp };
