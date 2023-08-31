1e6a7f599f5ff5f8d5990dd2a9cbbabbd146e47c
iamAlinaaa <alinaaevns1806@gmail.com>
2023-08-31 11:34:42 +0300

    cp-51: - optional chaining

--- mobile/src/screens/survey/survey.tsx
+++ mobile/src/screens/survey/survey.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Link, SignBackground, Text } from '#libs/components/components';
 import { RootScreenName } from '#libs/enums/enums';
 import { useAppDispatch, useAppSelector, useCallback } from '#libs/hooks/hooks';
+import { type UserAuthResponseDto } from '#packages/users/users';
 import { actions as authActions } from '#slices/auth/auth';
 
 import { PreferencesStep } from './components/components';
@@ -11,14 +12,14 @@ import { styles } from './styles';
 const Survey: React.FC = () => {
   const dispatch = useAppDispatch();
   const { userId } = useAppSelector(({ auth }) => ({
-    userId: auth.authenticatedUser?.id,
+    userId: (auth.authenticatedUser as UserAuthResponseDto).id,
   }));
 
   const handleSubmit = useCallback(
     (options: string[]) => {
       void dispatch(
         authActions.createUserSurveyPreferences({
-          userId: userId as number,
+          userId: userId,
           preferences: options,
         }),
       );
