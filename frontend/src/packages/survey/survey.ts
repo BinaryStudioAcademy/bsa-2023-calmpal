84c15159ede3a2162e7855e9d949163f62920dc7
myducck <dmitro.kucherenko@gmail.com>
2023-08-31 13:14:40 +0300

    cp-49: + preferences other category constant

--- frontend/src/packages/survey/survey.ts
+++ frontend/src/packages/survey/survey.ts
@@ -1,4 +1,4 @@
-export { SurveyTextareaOptions } from './libs/enums/enums.js';
+export { PREFERENCES_OTHER_CATEGORY } from './libs/constants/constants.js';
 export { getSurveyCategories } from './libs/helpers/helpers.js';
 export {
   type SurveyGetAllItemResponseDto,
