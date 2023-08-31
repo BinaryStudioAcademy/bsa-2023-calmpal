a09456f473ad79f7171abf3a5378fe8df9ba4d95
iamAlinaaa <alinaaevns1806@gmail.com>
2023-08-31 16:16:46 +0300

    cp-51: - unnecessary logics

--- mobile/src/screens/survey/components/survey-category/survey-category.tsx
+++ mobile/src/screens/survey/components/survey-category/survey-category.tsx
@@ -3,37 +3,7 @@ import React from 'react';
 import { Text, TouchableOpacity, View } from '#libs/components/components';
 import { useCallback, useState } from '#libs/hooks/hooks';
 
-// import {
-//   type FormControllerRenderProps,
-//   type FormFieldValues,
-//   type FormPath,
-// } from '#libs/types/types';
 import { styles } from './styles';
-
-// type Properties<T extends FormFieldValues> = {
-//   field: FormControllerRenderProps<T, FormPath<T>>;
-//   label: string;
-// };
-
-// const SurveyCategory = <T extends FormFieldValues>({
-//   field,
-//   label,
-// }: Properties<T>): JSX.Element => {
-//   const [isSelected, setIsSelected] = useState(false);
-//   const { onChange, value } = field;
-
-//   const handleChange = useCallback(() => {
-//     let options = value as string[];
-//     if (options.includes(label)) {
-//       options = options.filter((option: string) => option !== label);
-//       setIsSelected(false);
-//     } else {
-//       options.push(label);
-//       setIsSelected(true);
-//     }
-
-//     onChange(options);
-//   }, [label, value, onChange]);
 
 type Properties = {
   label: string;
@@ -48,13 +18,11 @@ const SurveyCategory: React.FC<Properties> = ({
   const handleChange = useCallback(() => {
     onChange(label);
     setIsSelected(!isSelected);
-    // console.log(label, onChange);
   }, [label, onChange, isSelected]);
 
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={handleChange}>
-        {/* <TouchableOpacity onPress={handleChange}> */}
         <View
           style={[
             styles.categoryButton,
