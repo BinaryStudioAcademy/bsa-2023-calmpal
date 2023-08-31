b5d1f1b6cb6c5d57c9158271c37833e2c34336e2
myducck <dmitro.kucherenko@gmail.com>
2023-08-31 15:56:21 +0300

    cp-49: * checkbox export

--- frontend/src/libs/components/components.ts
+++ frontend/src/libs/components/components.ts
@@ -1,5 +1,6 @@
 export { App } from './app/app.js';
 export { Button } from './button/button.js';
+export { Checkbox } from './checkbox/checkbox.js';
 export { Header } from './header/header.js';
 export { Input } from './input/input.js';
 export { Link } from './link/link.js';
