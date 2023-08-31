b0e6483d104e8074f51a3cbbd5e5355e1646f842
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-08-31 20:03:34 +0300

    cp-19: + check if token exists inside getAuthenticatedUser action

--- frontend/src/libs/components/app/app.tsx
+++ frontend/src/libs/components/app/app.tsx
@@ -3,10 +3,8 @@ import { DataStatus } from '#libs/enums/enums.js';
 import {
   useAppDispatch,
   useAppSelector,
-  useCallback,
   useEffect,
 } from '#libs/hooks/hooks.js';
-import { storage, StorageKey } from '#libs/packages/storage/storage.js';
 import { actions as authActions } from '#slices/auth/auth.js';
 
 const App: React.FC = () => {
@@ -15,17 +13,9 @@ const App: React.FC = () => {
     authenticatedUserDataStatus: auth.authenticatedUserDataStatus,
   }));
 
-  const handleGetAuthenticatedUser = useCallback(async (): Promise<void> => {
-    const hasToken = await storage.has(StorageKey.TOKEN);
-
-    if (hasToken) {
-      await dispatch(authActions.getAuthenticatedUser());
-    }
-  }, [dispatch]);
-
   useEffect(() => {
-    void handleGetAuthenticatedUser();
-  }, [handleGetAuthenticatedUser]);
+    void dispatch(authActions.getAuthenticatedUser());
+  }, [dispatch]);
 
   if (authenticatedUserDataStatus === DataStatus.PENDING) {
     return <Loader />;
