e1f34f364de8c4cb5f1fbd84c06baf351173d3ed
myducck <dmitro.kucherenko@gmail.com>
2023-08-31 11:30:55 +0300

    cp-49: * no completed survey behaviour

--- frontend/src/libs/components/app/app.tsx
+++ frontend/src/libs/components/app/app.tsx
@@ -1,24 +1,37 @@
-import { Loader, RouterOutlet } from '#libs/components/components.js';
-import { DataStatus } from '#libs/enums/enums.js';
+import { Loader, Navigate, RouterOutlet } from '#libs/components/components.js';
+import { AppRoute, DataStatus } from '#libs/enums/enums.js';
 import {
   useAppDispatch,
   useAppSelector,
   useEffect,
+  useLocation,
 } from '#libs/hooks/hooks.js';
 import { actions as authActions } from '#slices/auth/auth.js';
 
 const App: React.FC = () => {
+  const { pathname } = useLocation();
   const dispatch = useAppDispatch();
-  const { authenticatedUserDataStatus, surveyPreferencesDataStatus } =
-    useAppSelector(({ auth }) => ({
-      authenticatedUser: auth.authenticatedUser,
-      authenticatedUserDataStatus: auth.authenticatedUserDataStatus,
-      surveyPreferencesDataStatus: auth.surveyPreferencesDataStatus,
-    }));
+  const {
+    authenticatedUser,
+    authenticatedUserDataStatus,
+    surveyPreferencesDataStatus,
+  } = useAppSelector(({ auth }) => ({
+    authenticatedUser: auth.authenticatedUser,
+    authenticatedUserDataStatus: auth.authenticatedUserDataStatus,
+    surveyPreferencesDataStatus: auth.surveyPreferencesDataStatus,
+  }));
 
   useEffect(() => {
     void dispatch(authActions.getAuthenticatedUser());
   }, [dispatch]);
+
+  const hasNoSurvey =
+    authenticatedUser &&
+    !authenticatedUser.isSurveyCompleted &&
+    pathname !== AppRoute.SURVEY;
+  if (hasNoSurvey) {
+    return <Navigate to={AppRoute.SURVEY} />;
+  }
 
   if (
     authenticatedUserDataStatus === DataStatus.IDLE ||
