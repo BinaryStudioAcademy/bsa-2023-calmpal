7804b4e2932ea5add213c039362993f585e7ecdf
myducck <dmitro.kucherenko@gmail.com>
2023-08-31 15:55:00 +0300

    cp-49: + checkbox component

---
+++ frontend/src/libs/components/checkbox/checkbox.tsx
@@ -0,0 +1,21 @@
+import styles from './styles.module.scss';
+
+type Properties = {
+  label: string;
+  onChange: () => void;
+};
+
+const Checkbox: React.FC<Properties> = ({ label, onChange }): JSX.Element => {
+  return (
+    <label className={styles['container']}>
+      <input
+        className={styles['checkbox']}
+        type="checkbox"
+        onChange={onChange}
+      />
+      <span className={styles['label']}>{label}</span>
+    </label>
+  );
+};
+
+export { Checkbox };
