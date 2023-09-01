38e76dc644954c4d0a30190e1ec6ae31fcd21363
OSCAR SANTIAGO VELEZ MUÑOZ <90299049+ossavemu@users.noreply.github.com>
2023-08-31 22:57:30 -0500

    cp-65: * fix actions.ts

--- frontend/src/slices/chat/actions.ts
+++ frontend/src/slices/chat/actions.ts
@@ -2,20 +2,9 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { type ChatMessage } from '#libs/types/types.js';
 
-// type AddMessagePayload = {
-//   text: string;
-// };
-
 type NewMessagePayload = {
   message: ChatMessage;
 };
-
-// const addMessage = createAsyncThunk(
-//   'chat/addMessage',
-//   (payload: AddMessagePayload) => {
-//     return payload.text;
-//   },
-// );
 
 const newMessage = createAsyncThunk(
   'chat/newMessage',
