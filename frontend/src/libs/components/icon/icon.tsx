e2e020677c09c3253b850aa2d994911ad0f137de
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-09-01 21:20:54 +0300

    cp-71: - className property from Icon component

--- frontend/src/libs/components/icon/icon.tsx
+++ frontend/src/libs/components/icon/icon.tsx
@@ -1,23 +1,14 @@
 import { iconNameToIcon } from '#libs/maps/icon-name-to-plain-svg.js';
 import { type IconName } from '#libs/types/types.js';
 
-import styles from './styles.module.scss';
-
 type Properties = {
   name: IconName;
-  className?: string;
 };
 
-const Icon: React.FC<Properties> = ({ name, className = '' }) => {
+const Icon: React.FC<Properties> = ({ name }) => {
   const SelectedIcon = iconNameToIcon[name];
 
-  const cssClass = styles[className] ?? '';
-
-  return (
-    <div className={cssClass}>
-      <SelectedIcon />
-    </div>
-  );
+  return <SelectedIcon />;
 };
 
 export { Icon };
