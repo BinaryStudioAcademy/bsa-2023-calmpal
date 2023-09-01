4e1fa80d7240fcf2a5b6d0868e04cd6e21238754
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-09-01 16:10:20 +0300

    cp-71: * moved IconName type from shaved to front end folder

--- frontend/src/libs/types/types.ts
+++ frontend/src/libs/types/types.ts
@@ -5,10 +5,10 @@ export {
   type FormFieldPath,
   type FormFieldValues,
 } from './form/form.type.js';
+export { type IconName } from './icon-name.type.js';
 export { type Route } from './route/route.type.js';
 export { type ValueOf } from './value-of.type.js';
 export {
-  type IconName,
   type ServerErrorDetail,
   type ServerErrorResponse,
   type ValidationSchema,
