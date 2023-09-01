3fff897a814f3700ab222c9250705bd73691d37d
Dana Todoriak <todoriak@gmail.com>
2023-09-01 20:16:17 +0300

    cp-145: * fix code linting error

--- mobile/src/screens/auth/components/label/label.tsx
+++ mobile/src/screens/auth/components/label/label.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { type ImageSourcePropType } from 'react-native';
 
 import LogoImage from '#assets/img/logo.png';
 import { Image } from '#libs/components/components';
@@ -7,9 +6,7 @@ import { Image } from '#libs/components/components';
 import { styles } from './styles';
 
 const Label: React.FC = () => {
-  return (
-    <Image source={LogoImage as ImageSourcePropType} style={styles.label} />
-  );
+  return <Image source={LogoImage} style={styles.label} />;
 };
 
 export { Label };
