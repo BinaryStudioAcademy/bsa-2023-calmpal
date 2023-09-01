bcca7eed63153f10679c442f1e62354e6b0a914b
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-01 15:37:46 +0300

    cp-113: + new styles for profile

--- mobile/src/screens/profile/profile.tsx
+++ mobile/src/screens/profile/profile.tsx
@@ -4,10 +4,11 @@ import NotificationBellIcon from '#assets/img/icons/bell.svg';
 import { View } from '#libs/components/components';
 
 import { ProfileCategory } from './components/components';
+import { styles } from './styles';
 
 const Profile: React.FC = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <ProfileCategory
         iconSourceSvg={<NotificationBellIcon />}
         title="Notifications and Reminders"
---
+++ mobile/src/screens/profile/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+// import { AppColor } from '#libs/enums/enums';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export { styles };
