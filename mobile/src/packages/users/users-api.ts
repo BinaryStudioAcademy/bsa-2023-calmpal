b165f063704a7b9cbad3ea38af820c731a764fce
BrianSammit <briansammit.cruz@gmail.com>
2023-09-01 13:39:44 -0500

    cp-142: * Clean up layout, remove everything related to get all users and remove label at the top

--- mobile/src/packages/users/users-api.ts
+++ mobile/src/packages/users/users-api.ts
@@ -1,10 +1,7 @@
-import { APIPath, ContentType } from '#libs/enums/enums';
+import { APIPath } from '#libs/enums/enums';
 import { BaseHttpApi } from '#libs/packages/api/api';
 import { type HTTP } from '#libs/packages/http/http';
 import { type Storage } from '#libs/packages/storage/storage';
-
-import { UsersApiPath } from './libs/enums/enums';
-import { type UserGetAllResponseDto } from './libs/types/types';
 
 type Constructor = {
   baseUrl: string;
@@ -15,19 +12,6 @@ type Constructor = {
 class UserApi extends BaseHttpApi {
   public constructor({ baseUrl, http, storage }: Constructor) {
     super({ path: APIPath.USERS, baseUrl, http, storage });
-  }
-
-  public async getAll(): Promise<UserGetAllResponseDto> {
-    const response = await this.load(
-      this.getFullEndpoint(UsersApiPath.ROOT, {}),
-      {
-        method: 'GET',
-        contentType: ContentType.JSON,
-        hasAuth: true,
-      },
-    );
-
-    return await response.json<UserGetAllResponseDto>();
   }
 }
 
