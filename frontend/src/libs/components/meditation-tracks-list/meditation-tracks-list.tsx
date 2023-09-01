7957795fe8dd428aa6f2a119b75b918006110865
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-09-01 08:07:00 +0300

    cp-71: * moved mocked track to a separate folder

--- frontend/src/libs/components/meditation-tracks-list/meditation-tracks-list.tsx
+++ frontend/src/libs/components/meditation-tracks-list/meditation-tracks-list.tsx
@@ -1,38 +1,6 @@
 import { MeditationTrack } from './meditation-track/meditation-track.js';
+import { mockedTracks } from './mock/mocked-tracks.js';
 import styles from './styles.module.scss';
-
-const mockedImageLink = './images/track-background-336x480.jpg';
-
-const mockedTracks = [
-  {
-    id: 1,
-    title: 'Meditation for deep sleep',
-    duration: '10 min',
-    link: '',
-    imageLink: mockedImageLink,
-  },
-  {
-    id: 2,
-    title: 'Breathing meditation',
-    duration: '5 min',
-    link: '',
-    imageLink: mockedImageLink,
-  },
-  {
-    id: 3,
-    title: 'Meditation for relax',
-    duration: '15 min',
-    link: '',
-    imageLink: mockedImageLink,
-  },
-  {
-    id: 4,
-    title: 'Nature meditation',
-    duration: '45 min',
-    link: '',
-    imageLink: mockedImageLink,
-  },
-];
 
 const MeditationTracksList: React.FC = () => {
   return (
---
+++ frontend/src/libs/components/meditation-tracks-list/mock/mocked-tracks.ts
@@ -0,0 +1,34 @@
+const mockedImageLink = './images/track-background-336x480.jpg';
+
+const mockedTracks = [
+  {
+    id: 1,
+    title: 'Meditation for deep sleep',
+    duration: '10 min',
+    link: '',
+    imageLink: mockedImageLink,
+  },
+  {
+    id: 2,
+    title: 'Breathing meditation',
+    duration: '5 min',
+    link: '',
+    imageLink: mockedImageLink,
+  },
+  {
+    id: 3,
+    title: 'Meditation for relax',
+    duration: '15 min',
+    link: '',
+    imageLink: mockedImageLink,
+  },
+  {
+    id: 4,
+    title: 'Nature meditation',
+    duration: '45 min',
+    link: '',
+    imageLink: mockedImageLink,
+  },
+];
+
+export { mockedTracks };
