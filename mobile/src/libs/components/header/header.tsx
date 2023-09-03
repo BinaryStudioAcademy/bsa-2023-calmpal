d80d194ae45012274124f18d66040a163b507277
Dana Todoriak <todoriak@gmail.com>
2023-09-03 11:23:26 +0300

    cp-60: * make 'title' property optional

--- mobile/src/libs/components/header/header.tsx
+++ mobile/src/libs/components/header/header.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import BackArrowImage from '#assets/img/icons/back-arrow.svg';
 import { Text, TouchableOpacity, View } from '#libs/components/components';
 import { AppColor } from '#libs/enums/enums';
-import { useNavigation } from '#libs/hooks/hooks';
+import { useAppRoute, useNavigation } from '#libs/hooks/hooks';
 
 import { Badge } from './components/components';
 import { DEFAULT_BADGE_COUNT } from './libs/constants';
 import { styles } from './styles';
 
 type Properties = {
-  title: string;
+  title?: string;
   isArrowVisible?: boolean;
   badgeCount?: number;
 };
@@ -21,21 +21,22 @@ const Header: React.FC<Properties> = ({
   badgeCount = DEFAULT_BADGE_COUNT,
 }) => {
   const navigation = useNavigation();
+  const { name } = useAppRoute();
 
   const hasValue = Boolean(badgeCount);
 
-  const handleOnPress = (): void => {
+  const handleGoBack = (): void => {
     navigation.goBack();
   };
 
   return (
     <View style={[styles.header, isArrowVisible && styles.headerCenter]}>
       {isArrowVisible && (
-        <TouchableOpacity style={styles.arrow} onPress={handleOnPress}>
+        <TouchableOpacity style={styles.arrow} onPress={handleGoBack}>
           <BackArrowImage color={AppColor.BLUE_200} />
         </TouchableOpacity>
       )}
-      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.title}>{title ?? name}</Text>
       {hasValue && <Badge count={badgeCount} />}
     </View>
   );
