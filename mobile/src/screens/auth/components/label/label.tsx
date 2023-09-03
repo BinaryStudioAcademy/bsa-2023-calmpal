1ae90597d45e85746373cc34f21ed88d3648bd52
Dana Todoriak <todoriak@gmail.com>
2023-09-03 18:06:56 +0300

    cp-145: * LogoImage to Logo

--- mobile/src/screens/auth/components/label/label.tsx
+++ mobile/src/screens/auth/components/label/label.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import LogoImage from '#assets/img/logo.png';
+import Logo from '#assets/img/logo.png';
 import { Image } from '#libs/components/components';
 
 import { styles } from './styles';
 
 const Label: React.FC = () => {
-  return <Image source={LogoImage} style={styles.label} />;
+  return <Image source={Logo} style={styles.label} />;
 };
 
 export { Label };
