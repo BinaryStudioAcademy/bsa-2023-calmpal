67b180d64ea5074a1fc8774a3c15c9f550bee3c8
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-03 16:11:52 +0300

    cp-113: + linear-gradient component

---
+++ mobile/src/libs/components/linear-gradient/linear-gradient.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Gradient from 'react-native-linear-gradient';
+
+import { AppColor } from '#libs/enums/enums';
+
+import { styles } from './styles';
+
+type Properties = {
+  children: React.ReactNode;
+};
+
+const LinearGradient: React.FC<Properties> = ({ children }) => {
+  return (
+    <Gradient
+      colors={[AppColor.WHITE, AppColor.BLUE_100]}
+      style={styles.linearGradient}
+    >
+      {children}
+    </Gradient>
+  );
+};
+
+export { LinearGradient };
---
+++ mobile/src/libs/components/linear-gradient/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+  },
+});
+
+export { styles };
