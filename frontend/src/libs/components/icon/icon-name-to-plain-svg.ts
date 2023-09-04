096dc1450a64ff039a8cceaa3bc779ee80f6fb4c
Vladyslav Dobychin <vladyslavdobychin@gmail.com>
2023-09-04 14:47:05 +0300

    cp-61: * changes file extenstion from tsx to ts

---
+++ frontend/src/libs/components/icon/icon-name-to-plain-svg.ts
@@ -0,0 +1,23 @@
+import { ReactComponent as ArrowIcon } from '#assets/icons/arrow.svg';
+import { ReactComponent as AvatarIcon } from '#assets/icons/avatar.svg';
+import { ReactComponent as ChatIcon } from '#assets/icons/chat-logo.svg';
+import { ReactComponent as NotificationIcon } from '#assets/icons/notification.svg';
+import { ReactComponent as PlusIcon } from '#assets/icons/plus.svg';
+import { ReactComponent as SignOutIcon } from '#assets/icons/sign-out.svg';
+import { ReactComponent as SubscriptionIcon } from '#assets/icons/subscription.svg';
+import { type IconName } from '#libs/types/types.js';
+
+const iconNameToPlainSvgMap: Record<
+  IconName,
+  React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+> = {
+  'arrow': ArrowIcon,
+  'avatar': AvatarIcon,
+  'plus': PlusIcon,
+  'sign-out': SignOutIcon,
+  'notification': NotificationIcon,
+  'subscription': SubscriptionIcon,
+  'chat-logo': ChatIcon,
+};
+
+export { iconNameToPlainSvgMap };
