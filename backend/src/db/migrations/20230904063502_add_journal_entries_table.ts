ceccf43b5e2493a501108c2e9f178f96e0dc5c0a
Анастасія Лисенко <anastasiialysenko0@gmail.com>
2023-09-04 09:46:23 +0300

    cp-157: +  migrations

---
+++ backend/src/db/migrations/20230904063502_add_journal_entries_table.ts
@@ -0,0 +1,33 @@
+import { type Knex } from 'knex';
+
+const TABLE_NAME = 'journal_entries';
+
+const ColumnName = {
+  ID: 'id',
+  TITLE: 'title',
+  TEXT: 'text',
+  CREATED_AT: 'created_at',
+  UPDATED_AT: 'updated_at',
+} as const;
+
+function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.increments(ColumnName.ID).primary();
+    table.string(ColumnName.TITLE).notNullable();
+    table.string(ColumnName.TEXT).notNullable();
+    table
+      .dateTime(ColumnName.CREATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table
+      .dateTime(ColumnName.UPDATED_AT)
+      .notNullable()
+      .defaultTo(knex.fn.now());
+  });
+}
+
+function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists(TABLE_NAME);
+}
+
+export { down, up };
