74530b4105c88a81057b7b020f1c42278160f5aa
Vladyslav Dobychin <vladyslavdobychin@gmail.com>
2023-09-04 11:04:40 +0300

    cp-61: * moved icon-name-to-plain-svg to icon package

--- frontend/src/libs/components/icon/icon.tsx
+++ frontend/src/libs/components/icon/icon.tsx
@@ -1,6 +1,6 @@
+import { iconNameToPlainSvgMap } from '#libs/components/icon/icon-name-to-plain-svg.js';
 import { type IconName } from '#libs/enums/enums.js';
 import { getValidClassNames } from '#libs/helpers/helpers.js';
-import { iconNameToPlainSvgMap } from '#libs/maps/icon-name-to-plain-svg.js';
 
 import styles from './styles.module.scss';
 
