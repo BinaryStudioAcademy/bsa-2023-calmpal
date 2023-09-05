2d546dd3db2808ef86151bd61ddddd5ed0c4407c
myducck <dmitro.kucherenko@gmail.com>
2023-09-05 11:26:00 +0300

    cp-160: + meditation service

---
+++ backend/src/packages/meditation/meditation.service.ts
@@ -0,0 +1,48 @@
+import { type Service } from '#libs/types/types.js';
+
+import {
+  type MeditationEntryRequestDto,
+  type MeditationEntryResponseDto,
+} from './libs/types/types.js';
+import { MeditationEntity } from './meditation.entity.js';
+import { type MeditationRepository } from './meditation.repository.js';
+
+class MeditationService implements Service {
+  private meditationRepository: MeditationRepository;
+
+  public constructor(meditationRepository: MeditationRepository) {
+    this.meditationRepository = meditationRepository;
+  }
+
+  public find(): ReturnType<Service['find']> {
+    return Promise.resolve(null);
+  }
+
+  public async findAll(): ReturnType<Service['findAll']> {
+    return await Promise.resolve({ items: [] });
+  }
+
+  public async create({
+    topicName,
+    audioUrl,
+  }: MeditationEntryRequestDto): Promise<MeditationEntryResponseDto> {
+    const topic = await this.meditationRepository.findTopicByName(topicName);
+
+    const item = await this.meditationRepository.create(
+      MeditationEntity.initializeNew({ topicName, audioUrl }),
+      topic?.id,
+    );
+
+    return item.toObject();
+  }
+
+  public update(): ReturnType<Service['update']> {
+    return Promise.resolve(null);
+  }
+
+  public delete(): ReturnType<Service['delete']> {
+    return Promise.resolve(true);
+  }
+}
+
+export { MeditationService };
