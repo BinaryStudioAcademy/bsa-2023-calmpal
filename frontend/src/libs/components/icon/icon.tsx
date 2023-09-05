b89d43601bdd0570749f3866e1812c32903607c8
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-09-05 09:04:04 +0300

    cp-14: * replaces className property to an optional color property

--- frontend/src/libs/components/icon/icon.tsx
+++ frontend/src/libs/components/icon/icon.tsx
@@ -3,12 +3,13 @@ import { type IconName } from '#libs/types/types.js';
 
 type Properties = {
   name: IconName;
+  color?: string;
 };
 
-const Icon: React.FC<Properties> = ({ name }) => {
+const Icon: React.FC<Properties> = ({ name, color }) => {
   const SelectedIcon = iconNameToIcon[name];
 
-  return <SelectedIcon />;
+  return <SelectedIcon style={{ color }} />;
 };
 
 export { Icon };
