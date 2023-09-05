350974ec4ce7bfc729c3aa0b3c391344554c1105
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-09-05 15:41:10 +0300

    cp-71: * renamed route for meditation list

--- frontend/src/libs/components/sidebar-wrapper/libs/constants.ts
+++ frontend/src/libs/components/sidebar-wrapper/libs/constants.ts
@@ -3,7 +3,7 @@ import { type Route } from '#libs/types/types.js';
 
 const SIDEBAR_ROUTES: Route[] = [
   { path: AppRoute.ROOT, name: 'home', icon: 'home' },
-  { path: AppRoute.MEDITATION, name: 'meditations', icon: 'meditation' },
+  { path: AppRoute.MEDITATION, name: 'meditation', icon: 'meditation' },
   { path: AppRoute.CHATS, name: 'chats', icon: 'chats' },
 ];
 
