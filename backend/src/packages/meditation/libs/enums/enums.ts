2a754aa3640f83ce0041cd2956662898a863ef42
myducck <dmitro.kucherenko@gmail.com>
2023-09-05 11:21:28 +0300

    cp-160: + enums and types

---
+++ backend/src/packages/meditation/libs/enums/enums.ts
@@ -0,0 +1,4 @@
+export { MeditationRelation } from './meditations-relation.enum.js';
+export { MeditationEntriesTableColumn } from './table-columns/meditation-entries-table-column.enum.js';
+export { MeditationTopicsTableColumn } from './table-columns/meditation-topics-table-column.enum.js';
+export { MeditationApiPath } from 'shared/build/index.js';
---
+++ backend/src/packages/meditation/libs/types/types.ts
@@ -0,0 +1,7 @@
+export { type MeditationCommonQueryResponse } from './meditation-common-query-response.js';
+export { type MeditationCreateQueryPayload } from './meditation-create-query-payload.js';
+export { type TopicCommonQueryResponse } from './topic-common-query-response.js';
+export {
+  type MeditationEntryRequestDto,
+  type MeditationEntryResponseDto,
+} from 'shared/build/index.js';
