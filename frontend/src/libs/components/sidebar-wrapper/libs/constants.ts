355737eb5109cc71ae3c2dd1ab7a7db45561c3a1
evgen <evgenkitan@gmail.com>
2023-09-05 12:01:00 +0300

    cp-183: + link to chat page

--- frontend/src/libs/components/sidebar-wrapper/libs/constants.ts
+++ frontend/src/libs/components/sidebar-wrapper/libs/constants.ts
@@ -1,9 +1,11 @@
+import chats from '#assets/img/chat-link.svg';
 import home from '#assets/img/home.svg';
 import { AppRoute } from '#libs/enums/enums.js';
 import { type Route } from '#libs/types/types.js';
 
 const SIDEBAR_ROUTES: Route[] = [
   { path: AppRoute.ROOT, name: 'home', icon: home },
+  { path: AppRoute.CHATS, name: 'chats', icon: chats },
 ];
 
 export { SIDEBAR_ROUTES };
