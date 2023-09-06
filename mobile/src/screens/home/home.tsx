0f34e5f1f5f225c457547dbdc864b4d9d82552d0
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-06 14:25:49 +0300

    cp-196: + authenticated user name from redux

--- mobile/src/screens/home/home.tsx
+++ mobile/src/screens/home/home.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
 import userImagePlaceholder from '#assets/img/user-placeholder.png';
-import { Image, LinearGradient, View } from '#libs/components/components';
+import { Image, LinearGradient, Text, View } from '#libs/components/components';
+import { useAppSelector } from '#libs/hooks/hooks';
+import { type UserAuthResponseDto } from '#packages/users/users';
 
 import { styles } from './styles';
 
 const Home: React.FC = () => {
+  const { userName } = useAppSelector(({ auth }) => {
+    return {
+      userName: (auth.authenticatedUser as UserAuthResponseDto).fullName,
+    };
+  });
+
   return (
     <React.Fragment>
       <LinearGradient>
         <View style={styles.profileContainer}>
           <Image source={userImagePlaceholder} style={styles.userImage} />
+          <Text>{userName}</Text>
         </View>
       </LinearGradient>
     </React.Fragment>
