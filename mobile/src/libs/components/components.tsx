ee85821bbf65f48470094af9e7abb5073ebb893b
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-06 20:14:11 +0300

    cp-113: * exported components

--- mobile/src/libs/components/components.tsx
+++ mobile/src/libs/components/components.tsx
@@ -2,6 +2,7 @@ export { App } from './app/app';
 export { Button } from './button/button';
 export { Card } from './card/card';
 export { Header } from './header/header';
+export { Icon } from './icon/icon';
 export { Input } from './input/input';
 export { InputSearch } from './input-search/input-search';
 export { LinearGradient } from './linear-gradient/linear-gradient';
@@ -9,6 +10,7 @@ export { Link } from './link/link';
 export { Loader } from './loader/loader';
 export { ProtectedRoute } from './protected-route/protected-route';
 export { SignBackground } from './sign-background/sign-background';
+export { Switch } from './switch/switch';
 export { Toast } from './toast/toast';
 export {
   Image,
