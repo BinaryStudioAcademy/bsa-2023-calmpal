7d6aaef1cd5c66714507d1f33c1d05337a57d484
myducck <dmitro.kucherenko@gmail.com>
2023-09-06 16:38:06 +0300

    cp-160: + common modal component

---
+++ frontend/src/libs/components/modal/modal.tsx
@@ -0,0 +1,45 @@
+import { IconColor } from '#libs/enums/enums.js';
+import { getValidClassNames } from '#libs/helpers/get-valid-class-names.js';
+
+import { Icon } from '../components.js';
+import styles from './styles.module.scss';
+
+type Properties = {
+  children: React.ReactNode;
+  isDisplayed: boolean;
+  title: string;
+  onClose?: () => void;
+};
+
+const Modal: React.FC<Properties> = ({
+  children,
+  isDisplayed,
+  title,
+  onClose,
+}) => {
+  return (
+    <div
+      className={getValidClassNames(
+        styles['overlay'],
+        !isDisplayed && 'visually-hidden',
+      )}
+    >
+      <div className={styles['container']}>
+        <div className={styles['modal']}>
+          <div className={styles['header']}>
+            <span className={styles['title']}>{title}</span>
+            <button
+              className={getValidClassNames(styles['icon-container'])}
+              onClick={onClose}
+            >
+              <Icon name="cross" color={IconColor.BLACK} />
+            </button>
+          </div>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Modal };
