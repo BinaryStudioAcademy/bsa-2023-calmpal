7ca71d51d41c3efff1792dfc8b0e78019014fde7
Dana Todoriak <todoriak@gmail.com>
2023-09-06 13:09:33 +0300

    cp-170: - comments

--- mobile/service.js
+++ mobile/service.js
@@ -1,6 +1,3 @@
 import TrackPlayer from 'react-native-track-player';
 
-module.exports = async function () {
-  // TrackPlayer.addEventListener('remote-play', () => TrackPlayer.play());
-  // TrackPlayer.addEventListener('remote-pause', () => TrackPlayer.pause());
-};
+module.exports = async function () {};
