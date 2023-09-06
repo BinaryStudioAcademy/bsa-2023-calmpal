233d7c55c1cafab6cc81635c3b552ca9da208794
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-06 19:52:20 +0300

    cp-113: * adjusted setting component to card

--- mobile/src/libs/components/card/card.tsx
+++ mobile/src/libs/components/card/card.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { type ImageSourcePropType } from 'react-native';
 
 import imagePlaceholder from '#assets/img/card-image-placeholder.png';
-import { Image, Text, TouchableOpacity } from '#libs/components/components';
+import {
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from '#libs/components/components';
 
 import { DEFAULT_NUMBER_OF_LINES } from './libs/constants';
 import { styles } from './styles';
@@ -10,19 +16,28 @@ import { styles } from './styles';
 type Properties = {
   title: string;
   image?: ImageSourcePropType;
+  iconSourceSvg?: JSX.Element;
   onPress: (title: string) => void;
 };
 
 const Card: React.FC<Properties> = ({
   title,
   image = imagePlaceholder,
+  iconSourceSvg,
   onPress,
 }) => {
   const handlePress = (): void => {
     onPress(title);
   };
 
-  return (
+  return iconSourceSvg ? (
+    <Pressable onPress={handlePress}>
+      <View style={styles.container}>
+        <View style={styles.iconContainer}>{iconSourceSvg}</View>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    </Pressable>
+  ) : (
     <TouchableOpacity style={styles.container} onPress={handlePress}>
       <Image source={image} style={styles.image} />
       <Text
