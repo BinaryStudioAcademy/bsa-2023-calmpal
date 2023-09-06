91bbbec80e18186ea02dad52afeb8faad9b19616
myducck <dmitro.kucherenko@gmail.com>
2023-09-06 16:45:47 +0300

    cp-160: + meditation constants

---
+++ frontend/src/pages/meditation/libs/constants.ts
@@ -0,0 +1,8 @@
+import { type MeditationCreateValidation } from '#packages/meditation/libs/types/types.js';
+
+const DEFAULT_MEDITATION_PAYLOAD: MeditationCreateValidation = {
+  title: '',
+  file: null,
+};
+
+export { DEFAULT_MEDITATION_PAYLOAD };
