83d8c24dbb5ce10f7e808f190a80276483944acc
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-06 14:12:49 +0300

    cp-196: + user image and home screen improvements

--- mobile/src/screens/home/home.tsx
+++ mobile/src/screens/home/home.tsx
@@ -1,5 +1,20 @@
-const Home = (): null => {
-  return null;
+import React from 'react';
+
+import userImagePlaceholder from '#assets/img/user-placeholder.png';
+import { Image, LinearGradient, View } from '#libs/components/components';
+
+import { styles } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <React.Fragment>
+      <LinearGradient>
+        <View>
+          <Image source={userImagePlaceholder} style={styles.userImage} />
+        </View>
+      </LinearGradient>
+    </React.Fragment>
+  );
 };
 
 export { Home };
---
+++ mobile/src/screens/home/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+// import { AppColor } from '#libs/enums/enums';
+
+const styles = StyleSheet.create({
+  userImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+});
+
+export { styles };
