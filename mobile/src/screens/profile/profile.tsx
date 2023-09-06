b4b2cff09304071742d90c7d0d90e91338ccaf29
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-06 20:30:26 +0300

    cp-113: + profile component

---
+++ mobile/src/screens/profile/profile.tsx
@@ -0,0 +1,39 @@
+// import { type NavigationProp } from '@react-navigation/native';
+import React from 'react';
+
+import {
+  // Card,
+  Header,
+  // Icon,
+  LinearGradient,
+  View,
+} from '#libs/components/components';
+
+// import { AppColor, RootScreenName } from '#libs/enums/enums';
+// import { useNavigation } from '#libs/hooks/hooks';
+// import { type RootNavigationParameterList } from '#libs/types/types';
+import { styles } from './styles';
+
+const Profile: React.FC = () => {
+  // const navigation =
+  //   useNavigation<NavigationProp<RootNavigationParameterList>>();
+
+  // const handleCategoryPress = (): void => {
+  //   navigation.navigate(RootScreenName.SETTINGS);
+  // };
+
+  return (
+    <LinearGradient>
+      <Header isArrowVisible />
+      <View style={styles.container}>
+        {/* <Card
+          iconSourceSvg={<Icon name="bell" color={AppColor.WHITE} />}
+          title="Notifications and Reminders"
+          onPress={handleCategoryPress}
+        /> */}
+      </View>
+    </LinearGradient>
+  );
+};
+
+export { Profile };
