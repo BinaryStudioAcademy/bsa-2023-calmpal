d2dcb3d341323c233604648531cd361a6bb5fcdb
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-09-06 23:34:59 +0300

    cp-78: - remove getFormattedDate from frontend/helpers

--- frontend/src/libs/enums/enums.ts
+++ frontend/src/libs/enums/enums.ts
@@ -1,7 +1,6 @@
 export { AppRoute } from './app-route.enum.js';
 export { DataStatus } from './data-status.enum.js';
 export { IconColor } from './icon-color.enum.js';
-export { TIME_FORMAT } from './time-format.enum.js';
 export {
   APIPath,
   AppEnvironment,
--- frontend/src/libs/enums/time-format.enum.ts
+++
@@ -1,6 +0,0 @@
-const TIME_FORMAT = {
-  HH_MM_SS: 'HH:mm:ss',
-  MM_SS: 'mm:ss',
-} as const;
-
-export { TIME_FORMAT };
