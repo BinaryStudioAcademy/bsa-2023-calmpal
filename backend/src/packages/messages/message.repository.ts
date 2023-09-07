936c926a0b34f97a5dd048e6086a78e96247b76d
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-09-07 11:32:41 +0300

    cp-190: + message repository with only create method

---
+++ backend/src/packages/messages/message.repository.ts
@@ -0,0 +1,52 @@
+import { type Repository } from '#libs/types/types.js';
+
+import { MessageEntity } from './message.entity.js';
+import { type MessageModel } from './message.model.js';
+
+class MessageRepository implements Repository {
+  private messageModel: typeof MessageModel;
+
+  public constructor(messageModel: typeof MessageModel) {
+    this.messageModel = messageModel;
+  }
+
+  public find(): ReturnType<Repository['find']> {
+    return Promise.resolve(null);
+  }
+
+  public async findAll(): ReturnType<Repository['findAll']> {
+    return await Promise.resolve([]);
+  }
+
+  public async create(entity: MessageEntity): Promise<MessageEntity> {
+    const { name, message, sender_id } = entity.toNewObject();
+
+    const chatMessage = await this.messageModel
+      .query()
+      .insertGraph({
+        name,
+        message,
+        sender_id,
+      })
+      .execute();
+
+    return MessageEntity.initialize({
+      id: chatMessage.id,
+      name: chatMessage.name,
+      message: chatMessage.message,
+      sender_id: chatMessage.sender_id,
+      createdAt: new Date(chatMessage.createdAt),
+      updatedAt: new Date(chatMessage.updatedAt),
+    });
+  }
+
+  public update(): ReturnType<Repository['delete']> {
+    return Promise.resolve(true);
+  }
+
+  public delete(): ReturnType<Repository['delete']> {
+    return Promise.resolve(true);
+  }
+}
+
+export { MessageRepository };
