f3d31d6a998c46ea36dff13fec37b2912f3f153a
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-09-07 11:34:08 +0300

    cp-78: + date.ts for reexport getFormattedDate and getFormattedTime

--- shared/src/libs/helpers/helpers.ts
+++ shared/src/libs/helpers/helpers.ts
@@ -1,5 +1,4 @@
 export { configureString } from './configure-string/configure-string.helper.js';
-export { getFormattedDate } from './date/get-formatted-date/get-formatted-date.helper.js';
-export { getFormattedTime } from './date/get-formatted-time/get-formatted-time.helper.js';
+export { getFormattedDate, getFormattedTime } from './date/date.js';
 export { debounce } from './debounce/debounce.helper.js';
 export { replaceTemplateWithValue } from './replace-template-with-value/replace-template-with-value.helper.js';
