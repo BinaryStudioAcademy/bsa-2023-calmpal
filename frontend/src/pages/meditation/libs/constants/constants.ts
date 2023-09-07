e98ccbbe6862f4c42118546948a3d0672d61e7e2
myducck <dmitro.kucherenko@gmail.com>
2023-09-07 16:00:07 +0300

    cp-160: * meditation constants

--- frontend/src/pages/meditation/libs/constants/constants.ts
+++ frontend/src/pages/meditation/libs/constants/constants.ts
@@ -1,9 +1,18 @@
 import { type MeditationCreateValidation } from '#packages/meditation/libs/types/types.js';
 
+const TRACK_FIRST_INDEX = 0;
+const TRACK_START_TIME = 0;
+const FULL_PERCENTAGE = 100;
+const TRACK_INCREMENT_INDEX = 1;
+const TRACK_SKIP_SECONDS = 30;
+const PROGRESS_BAR = '--player-progress';
+
 const DEFAULT_MEDITATION_PAYLOAD: MeditationCreateValidation = {
   title: '',
   file: null,
 };
+
+const MOCKED_IMAGE = '../../src/assets/img/meditation-image-placeholder.jpg';
 
 const mockedEntries = [
   {
@@ -28,4 +37,38 @@ const mockedEntries = [
   },
 ] as const;
 
-export { DEFAULT_MEDITATION_PAYLOAD, mockedEntries };
+const mockedPlaylist = [
+  {
+    id: 1,
+    title: 'Meditation for deep sleep',
+    purpose: 'Stress relief',
+    src: 'http://traffic.libsyn.com/mindfulorg/winston-breathing-5mins.mp3',
+    img: MOCKED_IMAGE,
+  },
+  {
+    id: 2,
+    title: 'Meditation for relaxing',
+    purpose: 'Relax',
+    src: 'http://traffic.libsyn.com/mindfulorg/SusanKaiserGreenland.mp3',
+    img: MOCKED_IMAGE,
+  },
+  {
+    id: 3,
+    title: 'Meditation for breath practice',
+    purpose: 'Breathing',
+    src: 'http://traffic.libsyn.com/mindfulorg/LovingKindness.mp3',
+    img: MOCKED_IMAGE,
+  },
+];
+
+export {
+  DEFAULT_MEDITATION_PAYLOAD,
+  FULL_PERCENTAGE,
+  mockedEntries,
+  mockedPlaylist,
+  PROGRESS_BAR,
+  TRACK_FIRST_INDEX,
+  TRACK_INCREMENT_INDEX,
+  TRACK_SKIP_SECONDS,
+  TRACK_START_TIME,
+};
