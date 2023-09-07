5e67eded8f6015b2f265c7fb9da5bd5fecade272
Dana Todoriak <todoriak@gmail.com>
2023-09-07 14:17:20 +0300

    cp-138: + navigation item type

--- mobile/src/navigations/root/libs/constants.ts
+++ mobile/src/navigations/root/libs/constants.ts
@@ -1,40 +1,35 @@
 import { RootScreenName } from '#libs/enums/enums';
+import { type NavigationItem } from '#libs/types/types';
 import { Main } from '#navigations/navigations';
 import { Auth } from '#screens/auth/auth';
 import { Survey } from '#screens/survey/survey';
 
-const NAVIGATION_ITEMS = [
+const NAVIGATION_ITEMS: NavigationItem[] = [
   {
     name: RootScreenName.MAIN,
     component: Main,
-    conditionToRender: (
-      authenticatedUser: boolean,
-      isSurveyCompleted: boolean,
-    ): boolean => {
+    conditionToRender: (authenticatedUser, isSurveyCompleted): boolean => {
       return authenticatedUser && isSurveyCompleted;
     },
   },
   {
     name: RootScreenName.SURVEY,
     component: Survey,
-    conditionToRender: (
-      authenticatedUser: boolean,
-      isSurveyCompleted: boolean,
-    ): boolean => {
+    conditionToRender: (authenticatedUser, isSurveyCompleted): boolean => {
       return authenticatedUser && !isSurveyCompleted;
     },
   },
   {
     name: RootScreenName.SIGN_IN,
     component: Auth,
-    conditionToRender: (authenticatedUser: boolean): boolean => {
+    conditionToRender: (authenticatedUser): boolean => {
       return !authenticatedUser;
     },
   },
   {
     name: RootScreenName.SIGN_UP,
     component: Auth,
-    conditionToRender: (authenticatedUser: boolean): boolean => {
+    conditionToRender: (authenticatedUser): boolean => {
       return !authenticatedUser;
     },
   },
