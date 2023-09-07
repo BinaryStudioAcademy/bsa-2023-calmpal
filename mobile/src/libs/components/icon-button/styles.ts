82e713743d0bcb7fab2cbf4355e9020b778e26ee
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-07 16:28:15 +0300

    cp-113: + styles to icon button

--- mobile/src/libs/components/icon-button/styles.ts
+++ mobile/src/libs/components/icon-button/styles.ts
@@ -1,7 +1,24 @@
 import { StyleSheet } from 'react-native';
 
-// import { AppColor } from '#libs/enums/enums';
+import { AppColor } from '#libs/enums/enums';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  iconButtonContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: AppColor.WHITE,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: AppColor.BLUE_300,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 6,
+  },
+});
 
 export { styles };
