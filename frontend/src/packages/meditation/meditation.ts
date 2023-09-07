815728c861280e50c1a713c2b2764dc481fc4181
myducck <dmitro.kucherenko@gmail.com>
2023-09-07 13:53:21 +0300

    cp-160: * refactor meditation imports

--- frontend/src/packages/meditation/meditation.ts
+++ frontend/src/packages/meditation/meditation.ts
@@ -11,3 +11,11 @@ const meditationApi = new MeditationApi({
 });
 
 export { meditationApi };
+export { MeditationApiPath } from './libs/enums/enums.js';
+export {
+  type MeditationCreatePayload,
+  type MeditationCreateValidation,
+  type MeditationEntryRequestDto,
+  type MeditationEntryResponseDto,
+} from './libs/types/types.js';
+export { createMeditationValidationSchema } from './libs/validation-schemas/validation-schemas.js';
