60430164db8749afb2bc064f3f5dace5559576a1
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-08 13:51:24 +0300

    cp-167: + sign out functionality

--- mobile/src/screens/profile/profile.tsx
+++ mobile/src/screens/profile/profile.tsx
@@ -3,17 +3,23 @@ import React from 'react';
 
 import { Card, LinearGradient, View } from '#libs/components/components';
 import { AppColor, ProfileScreenName } from '#libs/enums/enums';
-import { useNavigation } from '#libs/hooks/hooks';
+import { useAppDispatch, useNavigation } from '#libs/hooks/hooks';
 import { type ProfileNavigationParameterList } from '#libs/types/types';
+import { actions as authActions } from '#slices/auth/auth';
 
 import { styles } from './styles';
 
 const Profile: React.FC = () => {
+  const dispatch = useAppDispatch();
   const navigation =
     useNavigation<NavigationProp<ProfileNavigationParameterList>>();
 
   const handleNotificationsPress = (): void => {
     navigation.navigate(ProfileScreenName.SETTINGS);
+  };
+
+  const handleSignOutPress = (): void => {
+    void dispatch(authActions.signOut());
   };
 
   return (
@@ -27,7 +33,7 @@ const Profile: React.FC = () => {
         />
         <Card
           title="Sign Out"
-          onPress={handleNotificationsPress}
+          onPress={handleSignOutPress}
           iconColor={AppColor.WHITE}
           iconName="sign-out"
         />
