c53cee3ad877f6604ca1e0fe3e9a9dc7ebde6b28
Vladyslav Dobychin <vladyslavdobychin@gmail.com>
2023-09-09 15:17:50 +0300

    cp-61: + adjusted mobile styles to match already existing proportions and breaking points of chat side bar

--- frontend/src/libs/components/profile-settings-sidebar/profile-settings-sidebar.tsx
+++ frontend/src/libs/components/profile-settings-sidebar/profile-settings-sidebar.tsx
@@ -36,11 +36,11 @@ const ProfileSettingsSidebar: React.FC = () => {
 
   return (
     <div className={styles['container']}>
-      <div className={styles['header-container']}>
-        <div className={styles['header']}>
-          <div className="visually-hidden">Profile settings</div>
-          <div className={styles['header-text']}>My Profile</div>
-        </div>
+      <div className={styles['header']}>
+        <div className="visually-hidden">Profile settings</div>
+        <div className={styles['header-text']}>My Profile</div>
+      </div>
+      <div className={styles['body']}>
         <div className={styles['user']}>
           <div className="visually-hidden">User details</div>
           <div className={styles['user-details']}>
@@ -50,9 +50,7 @@ const ProfileSettingsSidebar: React.FC = () => {
             <div className={styles['user-name']}>{fullName}</div>
           </div>
         </div>
-      </div>
 
-      <div className={styles['body']}>
         <div className={styles['buttons-container']}>
           <div className="visually-hidden">Profile settings options</div>
           {settingsOptions.map((option) => {
