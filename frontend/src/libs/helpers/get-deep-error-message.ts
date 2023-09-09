1f80b1836478973bcafeb4a1c189bfcdd5239a0b
myducck <dmitro.kucherenko@gmail.com>
2023-09-09 21:11:56 +0300

    cp-160: + get deep error messages

---
+++ frontend/src/libs/helpers/get-deep-error-message.ts
@@ -0,0 +1,31 @@
+import {
+  type FormFieldErrors,
+  type FormFieldValues,
+  type FormPath,
+} from '#libs/types/types.js';
+
+const getDeepErrorMessage = <T extends FormFieldValues>(
+  errors: FormFieldErrors<T>[FormPath<T>],
+): string | null => {
+  if (!errors) {
+    return null;
+  }
+
+  if (errors.message) {
+    return errors.message as string;
+  }
+
+  const message = Object.values(errors)
+    .map((property: { message?: string }) => {
+      return property.message;
+    })
+    .find(Boolean);
+
+  if (message) {
+    return message;
+  }
+
+  return null;
+};
+
+export { getDeepErrorMessage };
--- frontend/src/libs/helpers/helpers.ts
+++ frontend/src/libs/helpers/helpers.ts
@@ -1,3 +1,4 @@
+export { getDeepErrorMessage } from './get-deep-error-message.js';
 export { getValidClassNames } from './get-valid-class-names.js';
 export {
   configureString,
