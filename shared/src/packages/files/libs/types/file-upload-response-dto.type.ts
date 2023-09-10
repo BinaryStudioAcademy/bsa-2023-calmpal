c637cdf2abffcbe4c52db5d9758c79a2106a433a
myducck <dmitro.kucherenko@gmail.com>
2023-09-10 14:01:46 +0300

    cp-160: * file upload response content type

--- shared/src/packages/files/libs/types/file-upload-response-dto.type.ts
+++ shared/src/packages/files/libs/types/file-upload-response-dto.type.ts
@@ -1,5 +1,9 @@
+import { type ContentType } from '#libs/enums/enums.js';
+import { type ValueOf } from '#libs/types/value-of.type.js';
+
 type FileUploadResponseDto = {
   url: string;
+  contentType: ValueOf<typeof ContentType>;
 };
 
 export { type FileUploadResponseDto };
