8c5378476a8f971da338980d5d44689667e98dd0
HIMDanny <danilbliz0201@gmail.com>
2023-09-11 10:23:40 +0300

    cp-189: + empty string validation message

--- shared/src/packages/chats/libs/validation-schemas/create-chat.validation-schema.ts
+++ shared/src/packages/chats/libs/validation-schemas/create-chat.validation-schema.ts
@@ -4,11 +4,10 @@ import { ChatValidationMessage } from '../enums/enums.js';
 import { type ChatRequestDto } from '../types/types.js';
 
 const createChat = joi.object<ChatRequestDto, true>({
-  name: joi
-    .string()
-    .trim()
-    .required()
-    .messages({ 'any.required': ChatValidationMessage.NAME_REQUIRED }),
+  name: joi.string().trim().required().messages({
+    'any.required': ChatValidationMessage.NAME_REQUIRED,
+    'string.empty': ChatValidationMessage.NAME_REQUIRED,
+  }),
 });
 
 export { createChat };
