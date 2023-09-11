a98a9adaad84d674fef67ba7b35da20e972a0fde
HIMDanny <danilbliz0201@gmail.com>
2023-09-11 16:55:07 +0300

    cp-189: + uses to chat response type

--- backend/src/packages/chats/libs/types/types.ts
+++ backend/src/packages/chats/libs/types/types.ts
@@ -1,4 +1,6 @@
+export { type ChatCommonQueryResponse } from './chat-common-query-response.type.js';
 export { type CreateChatPayload } from './create-chat-payload.type.js';
+export { type UserToChatQueryResponse } from './user-to-chat-query-response.type.js';
 export {
   type ChatGetAllItemResponseDto,
   type ChatGetAllResponseDto,
--- shared/src/packages/chats/libs/types/chat-get-all-item-response-dto.type.ts
+++ shared/src/packages/chats/libs/types/chat-get-all-item-response-dto.type.ts
@@ -1,7 +1,9 @@
+import { type UserToChatResponseDto } from './user-to-chat-response-dto.type.js';
+
 type ChatGetAllItemResponseDto = {
   id: number;
   name: string;
-  members: number[];
+  members: UserToChatResponseDto[];
 };
 
 export { type ChatGetAllItemResponseDto };
