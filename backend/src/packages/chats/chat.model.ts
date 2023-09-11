c05b04b290f662334543f5a9fb2f13f782ba894b
HIMDanny <danilbliz0201@gmail.com>
2023-09-11 17:02:50 +0300

    cp-189: + chat relation mappings

--- backend/src/packages/chats/chat.model.ts
+++ backend/src/packages/chats/chat.model.ts
@@ -1,15 +1,34 @@
+import { Model, type RelationMappings } from 'objection';
+
 import {
   AbstractModel,
   DatabaseTableName,
 } from '#libs/packages/database/database.js';
 
+import {
+  ChatsTableColumn,
+  UsersToChatsTableColumn,
+} from './libs/enums/enums.js';
+import { UserToChatModel } from './user-to-chat.model.js';
+
 class ChatModel extends AbstractModel {
   public name!: string;
 
-  public members!: number[];
-
   public static override get tableName(): string {
     return DatabaseTableName.CHATS;
+  }
+
+  public static get relationMappings(): RelationMappings {
+    return {
+      members: {
+        relation: Model.HasManyRelation,
+        modelClass: UserToChatModel,
+        join: {
+          from: `${DatabaseTableName.CHATS}.${ChatsTableColumn.ID}`,
+          to: `${DatabaseTableName.USERS_TO_CHATS}.${UsersToChatsTableColumn.CHAT_ID}`,
+        },
+      },
+    };
   }
 }
 
--- backend/src/packages/chats/libs/enums/enums.ts
+++ backend/src/packages/chats/libs/enums/enums.ts
@@ -1,1 +1,4 @@
+export { ChatsRelation } from './chats-relation.enum.js';
+export { ChatsTableColumn } from './table-columns/chats-table-column.enums.js';
+export { UsersToChatsTableColumn } from './table-columns/users-to-chats-table-column.enum.js';
 export { ChatsApiPath } from 'shared/build/index.js';
