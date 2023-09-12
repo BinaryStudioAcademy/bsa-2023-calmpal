a281a4ff29a711695c3427b428156b6c8b5f4bff
myducck <dmitro.kucherenko@gmail.com>
2023-09-12 13:26:08 +0300

    cp-242: * link class name and its styles

--- frontend/src/libs/components/link/link.tsx
+++ frontend/src/libs/components/link/link.tsx
@@ -1,7 +1,6 @@
 import { NavLink } from 'react-router-dom';
 
 import { type AppRoute } from '#libs/enums/enums.js';
-import { getValidClassNames } from '#libs/helpers/helpers.js';
 import { type ValueOf } from '#libs/types/types.js';
 
 import styles from './styles.module.scss';
@@ -9,14 +8,11 @@ import styles from './styles.module.scss';
 type Properties = {
   to: ValueOf<typeof AppRoute>;
   children: React.ReactNode;
-  className?: string;
 };
 
-const Link: React.FC<Properties> = ({ children, to, className = '' }) => {
-  const classNames = getValidClassNames(className, styles['link']);
-
+const Link: React.FC<Properties> = ({ children, to }) => {
   return (
-    <NavLink className={classNames} to={to}>
+    <NavLink className={styles['link'] as string} to={to}>
       {children}
     </NavLink>
   );
