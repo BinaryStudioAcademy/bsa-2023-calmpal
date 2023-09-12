a01e94bb09dc2d32097c8a9110b22e236bdf14ca
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-09-12 23:06:07 +0300

    cp-190: * replaced adding chat messages table migration to deleting name column migration

---
+++ backend/src/db/migrations/20230912094401_delete_name_column_from_chat_messages_table.ts
@@ -0,0 +1,19 @@
+import { type Knex } from 'knex';
+
+const TABLE_NAME = 'chat_messages';
+
+const COLUMN_NAME_TO_DELETE = 'name';
+
+function up(knex: Knex): Promise<void> {
+  return knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.dropColumn(COLUMN_NAME_TO_DELETE);
+  });
+}
+
+function down(knex: Knex): Promise<void> {
+  return knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.string(COLUMN_NAME_TO_DELETE).unique().notNullable();
+  });
+}
+
+export { down, up };
