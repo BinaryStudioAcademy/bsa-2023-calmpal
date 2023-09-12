3a396868f6751ffd7fef3a203d5859f0efb014bc
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-09-12 12:54:43 +0300

    cp-190: + validation schema for creating chat messages

---
+++ shared/src/packages/chat-messages/libs/validation-schemas/create-chat-message.validation-schema.ts
@@ -0,0 +1,33 @@
+import joi from 'joi';
+
+import {
+  ChatMessageValidationMessage,
+  ChatMessageValidationRule,
+} from '../enums/enums.js';
+import { type ChatMessageCreateRequestDto } from '../types/types.js';
+
+const createMessageValidationSchema = joi.object<
+  ChatMessageCreateRequestDto,
+  true
+>({
+  message: joi
+    .string()
+    .trim()
+    .max(ChatMessageValidationRule.MAXIMUM_MESSAGE_LENGTH)
+    .required()
+    .messages({
+      'any.required': ChatMessageValidationMessage.MESSAGE_REQUIRED,
+      'string.empty': ChatMessageValidationMessage.MESSAGE_REQUIRED,
+      'string.max': ChatMessageValidationMessage.MAXIMUM_MESSAGE_LENGTH,
+    }),
+  senderId: joi.number().integer().strict().required().messages({
+    'any.required': ChatMessageValidationMessage.USER_ID_IS_REQUIRED,
+    'number.base': ChatMessageValidationMessage.USER_ID_MUST_BE_NUMBER,
+  }),
+  chatId: joi.number().integer().strict().required().messages({
+    'any.required': ChatMessageValidationMessage.CHAT_ID_IS_REQUIRED,
+    'number.base': ChatMessageValidationMessage.CHAT_ID_MUST_BE_NUMBER,
+  }),
+});
+
+export { createMessageValidationSchema };
