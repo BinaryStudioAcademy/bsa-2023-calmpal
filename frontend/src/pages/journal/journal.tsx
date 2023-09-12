14c822a850de073fcecb40919f8ae9d5767d3689
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-09-12 11:47:22 +0300

    cp-194: + redirection to journal if dynamic path doesn't exist

--- frontend/src/pages/journal/journal.tsx
+++ frontend/src/pages/journal/journal.tsx
@@ -1,19 +1,38 @@
 import { Note } from '#libs/components/components.js';
+import { AppRoute } from '#libs/enums/app-route.enum.js';
 import { getValidClassNames } from '#libs/helpers/get-valid-class-names.js';
-import { useEffect, useParams, useState } from '#libs/hooks/hooks.js';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useEffect,
+  useParams,
+  useState,
+} from '#libs/hooks/hooks.js';
+import { actions as appActions } from '#slices/app/app.js';
 
 import { JournalSidebar } from './components/journal-sidebar/journal-sidebar.js';
 import styles from './styles.module.scss';
 
 const Journal: React.FC = () => {
   const { id } = useParams();
+  const dispatch = useAppDispatch();
   const [isNoteVisible, setIsNoteVisible] = useState(false);
+
+  const { selectedJournalEntry } = useAppSelector(({ journal }) => {
+    return {
+      selectedJournalEntry: journal.selectedJournalEntry,
+    };
+  });
 
   useEffect(() => {
     if (id) {
-      setIsNoteVisible(true);
+      if (selectedJournalEntry) {
+        setIsNoteVisible(true);
+      } else {
+        dispatch(appActions.navigate(AppRoute.JOURNAL));
+      }
     }
-  }, [id]);
+  }, [dispatch, id, selectedJournalEntry]);
 
   return (
     <div className={styles['wrapper']}>
