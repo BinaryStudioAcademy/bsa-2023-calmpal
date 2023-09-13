762565429d1a412d7204653fa0386e57957d16e3
myducck <dmitro.kucherenko@gmail.com>
2023-09-14 00:03:54 +0300

    cp-160: * meditation topics relation file nae

--- backend/src/packages/meditation/libs/enums/enums.ts
+++ backend/src/packages/meditation/libs/enums/enums.ts
@@ -1,5 +1,5 @@
 export { MeditationEntriesRelation } from './meditations-entries-relation.enum.js';
-export { MeditationTopicsRelation } from './meditations-topic-relation.enum.js';
+export { MeditationTopicsRelation } from './meditations-topics-relation.enum.js';
 export { MeditationEntriesTableColumn } from './table-columns/meditation-entries-table-column.enum.js';
 export { MeditationTopicsTableColumn } from './table-columns/meditation-topics-table-column.enum.js';
 export { MeditationApiPath } from 'shared/build/index.js';
