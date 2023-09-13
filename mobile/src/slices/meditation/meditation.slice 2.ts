4246a54c68b7a96a8071dcc9ddf718e358ae870b
Vladyslav Dobychin <vladyslavdobychin@gmail.com>
2023-09-13 16:45:39 +0300

    cp-161: - removed duplicate files

--- mobile/src/slices/meditation/meditation.slice 2.ts
+++
@@ -1,9 +0,0 @@
-import { createSlice } from '@reduxjs/toolkit';
-
-const { reducer, actions, name } = createSlice({
-  initialState: {},
-  name: 'meditation',
-  reducers: {},
-});
-
-export { actions, name, reducer };
