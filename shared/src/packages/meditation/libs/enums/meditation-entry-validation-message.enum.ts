01cac7c994191c04fab560e7d2e22eb26e1f4cc4
myducck <dmitro.kucherenko@gmail.com>
2023-09-14 12:56:14 +0300

    cp-160: * meditation validation messaged media url

--- shared/src/packages/meditation/libs/enums/meditation-entry-validation-message.enum.ts
+++ shared/src/packages/meditation/libs/enums/meditation-entry-validation-message.enum.ts
@@ -1,5 +1,6 @@
 const MeditationEntryValidationMessage = {
   TOPIC_REQUIRED: 'Topic name is required.',
+  MEDIA_URL_REQUIRED: 'Media file url is required.',
   FILE_REQUIRED: 'File is required.',
   FILE_NAME_REQUIRED: 'File name is required.',
   SIZE_TOO_BIG: 'Size of an uploaded file should be less than 10MB.',
