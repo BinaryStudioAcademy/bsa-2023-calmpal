1b69c57d50219da7f3c57408441e73a1712a4871
myducck <dmitro.kucherenko@gmail.com>
2023-09-14 13:41:17 +0300

    cp-160: - get deep error message helper

--- frontend/src/libs/helpers/helpers.ts
+++ frontend/src/libs/helpers/helpers.ts
@@ -1,5 +1,4 @@
 export { generateUUID } from './generate-uuid.helper.js';
-export { getDeepErrorMessage } from './get-deep-error-message.helper.js';
 export { getValidClassNames } from './get-valid-class-names.helper.js';
 export {
   configureString,
