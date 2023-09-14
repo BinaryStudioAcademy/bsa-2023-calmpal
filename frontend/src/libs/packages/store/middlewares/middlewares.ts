cc1787333e20b7216a1d1e691e2a7b760b4cf7be
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-09-14 16:04:17 +0300

    cp-291: + signOut to the handleError middleware

--- frontend/src/libs/packages/store/middlewares/middlewares.ts
+++ frontend/src/libs/packages/store/middlewares/middlewares.ts
@@ -1,9 +1,11 @@
 import { type AnyAction, type Middleware } from '@reduxjs/toolkit';
 import { isRejected } from '@reduxjs/toolkit';
+import { ExceptionMessage } from 'shared/build/index.js';
 
 import { NotificationType } from '#libs/packages/notification/libs/enums/enums.js';
 import { type AppDispatch } from '#libs/packages/store/libs/types/types.js';
 import { appActions } from '#slices/app/app-notification.js';
+import { signOut } from '#slices/auth/actions.js';
 
 const handleError: Middleware<unknown, unknown, AppDispatch> = ({
   dispatch,
@@ -11,6 +13,10 @@ const handleError: Middleware<unknown, unknown, AppDispatch> = ({
   return (next) => {
     return (action: AnyAction) => {
       if (isRejected(action)) {
+        if (action.error.message === ExceptionMessage.UNAUTHORIZED_USER) {
+          void dispatch(signOut());
+        }
+
         void dispatch(
           appActions.notify({
             message: action.error.message ?? 'Something went wrong',
