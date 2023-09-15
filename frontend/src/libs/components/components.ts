7e4a8902710f7dcec9856970d805aeec416c004e
myducck <dmitro.kucherenko@gmail.com>
2023-09-15 16:59:49 +0300

    cp-160: * error message in components

--- frontend/src/libs/components/components.ts
+++ frontend/src/libs/components/components.ts
@@ -20,5 +20,6 @@ export { Search } from './search/search.js';
 export { Sidebar, SidebarBody, SidebarHeader } from './sidebar/components.js';
 export { Switch } from './switch/switch.js';
 export { Toast } from './toast/toast.js';
+export { ErrorMessage } from '@hookform/error-message';
 export { Provider as StoreProvider } from 'react-redux';
 export { Navigate, Outlet as RouterOutlet } from 'react-router-dom';
