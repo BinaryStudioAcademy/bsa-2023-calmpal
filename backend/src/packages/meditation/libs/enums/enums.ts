6be330d2c0f401873a925d4ab61a5db7f853c807
myducck <dmitro.kucherenko@gmail.com>
2023-09-16 00:18:16 +0300

    cp-160: * meidtation relations file names

--- backend/src/packages/meditation/libs/enums/enums.ts
+++ backend/src/packages/meditation/libs/enums/enums.ts
@@ -1,5 +1,5 @@
-export { MeditationEntriesRelation } from './meditations-entries-relation.enum.js';
-export { MeditationTopicsRelation } from './meditations-topics-relation.enum.js';
+export { MeditationEntriesRelation } from './meditation-entries-relation.enum.js';
+export { MeditationTopicsRelation } from './meditation-topics-relation.enum.js';
 export { MeditationEntriesTableColumn } from './table-columns/meditation-entries-table-column.enum.js';
 export { MeditationTopicsTableColumn } from './table-columns/meditation-topics-table-column.enum.js';
 export { MeditationApiPath } from 'shared/build/index.js';
