331cd9525f654bba3c7107b69bc28d2e01252551
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-15 14:47:19 +0300

    cp-296: + modal styles

--- mobile/src/libs/components/modal/styles.ts
+++ mobile/src/libs/components/modal/styles.ts
@@ -1,7 +1,34 @@
 import { StyleSheet } from 'react-native';
 
-// import { AppColor } from '#libs/enums/enums';
+import { AppColor } from '#libs/enums/enums';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: AppColor.WHITE,
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+    shadowColor: AppColor.BLACK,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 10,
+  },
+});
 
 export { styles };
