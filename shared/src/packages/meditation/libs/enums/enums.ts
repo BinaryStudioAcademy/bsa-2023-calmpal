cd94ebe7f1b3e78995937a481af8407ac748d747
myducck <dmitro.kucherenko@gmail.com>
2023-09-16 14:41:32 +0300

    cp-160: - meditation validation rule

--- shared/src/packages/meditation/libs/enums/enums.ts
+++ shared/src/packages/meditation/libs/enums/enums.ts
@@ -1,3 +1,2 @@
 export { MeditationApiPath } from './meditation-api-path.enum.js';
 export { MeditationEntryValidationMessage } from './meditation-entry-validation-message.enum.js';
-export { MeditationEntryValidationRule } from './meditation-entry-validation-rule.enum.js';
--- shared/src/packages/meditation/libs/enums/meditation-entry-validation-rule.enum.ts
+++
@@ -1,5 +0,0 @@
-const MeditationEntryValidationRule = {
-  MAXIMUM_FILE_SIZE: 10_485_760,
-} as const;
-
-export { MeditationEntryValidationRule };
