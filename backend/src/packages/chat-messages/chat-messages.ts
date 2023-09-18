fae90d3bb29c5609f04fc52f5ec843c2b7973404
HIMDanny <danilbliz0201@gmail.com>
2023-09-18 22:13:21 +0300

    cp-190: - chat messages controller

--- backend/src/packages/chat-messages/chat-messages.ts
+++ backend/src/packages/chat-messages/chat-messages.ts
@@ -1,18 +1,11 @@
-import { logger } from '#libs/packages/logger/logger.js';
-
 import { ChatMessageModel } from './chat-message.model.js';
 import { ChatMessageRepository } from './chat-message.repository.js';
 import { ChatMessageService } from './chat-message.service.js';
-import { ChatMessageController } from './chat-messages.controller.js';
 
 const chatMessageRepository = new ChatMessageRepository(ChatMessageModel);
 const chatMessageService = new ChatMessageService(chatMessageRepository);
-const chatMessagesController = new ChatMessageController(
-  logger,
-  chatMessageService,
-);
+
 export { chatMessageService };
-export { chatMessagesController };
 export { type ChatMessageService } from './chat-message.service.js';
 export {
   type ChatMessageCreateData,
