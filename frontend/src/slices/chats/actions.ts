1a13355712cdefd34796130d0a38b1e42d25b371
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-09-18 20:30:47 +0300

    cp-268: + redirection to /chats after deleting one

--- frontend/src/slices/chats/actions.ts
+++ frontend/src/slices/chats/actions.ts
@@ -1,11 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+import { AppRoute } from '#libs/enums/enums.js';
 import { type AsyncThunkConfig } from '#libs/types/types.js';
 import {
   type ChatCreateRequestDto,
   type ChatGetAllItemResponseDto,
   type ChatGetAllResponseDto,
 } from '#packages/chats/chats.js';
+import { actions as appActions } from '#slices/app/app.js';
 
 import { name as sliceName } from './chats.slice.js';
 
@@ -33,13 +35,15 @@ const deleteChat = createAsyncThunk<
   ChatGetAllItemResponseDto[],
   number,
   AsyncThunkConfig
->(`${sliceName}/delete-chat`, async (id, { extra, getState }) => {
+>(`${sliceName}/delete-chat`, async (id, { extra, getState, dispatch }) => {
   const { chatApi } = extra;
   await chatApi.deleteChat(id);
 
   const {
     chats: { chats },
   } = getState();
+
+  dispatch(appActions.navigate(AppRoute.CHATS));
 
   return chats.filter((chat) => {
     return chat.id !== id;
