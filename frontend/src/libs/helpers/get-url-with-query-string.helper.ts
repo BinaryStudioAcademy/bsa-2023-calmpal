bd5109ba0e67076f03699ad7fcabef35c198d5bb
HIMDanny <danilbliz0201@gmail.com>
2023-09-18 13:06:58 +0300

    cp-300: * rename function argument

--- frontend/src/libs/helpers/get-url-with-query-string.helper.ts
+++ frontend/src/libs/helpers/get-url-with-query-string.helper.ts
@@ -4,12 +4,12 @@ import { type ValueOf } from '#libs/types/types.js';
 type QueryParameters = Record<ValueOf<typeof AppQueryStringKey>, string>;
 
 const getUrlWithQueryString = (
-  url: ValueOf<typeof AppRoute>,
+  path: ValueOf<typeof AppRoute>,
   parameters: QueryParameters,
 ): ValueOf<typeof AppRoute> => {
   const queryParameters = new URLSearchParams(parameters);
 
-  return `${url}?${queryParameters.toString()}` as ValueOf<typeof AppRoute>;
+  return `${path}?${queryParameters.toString()}` as ValueOf<typeof AppRoute>;
 };
 
 export { getUrlWithQueryString };
