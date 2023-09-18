fb71c378ba85ce4bbbda435adefea7928d36215d
myducck <dmitro.kucherenko@gmail.com>
2023-09-18 13:19:07 +0300

    cp-160: * meditation migrations and order

--- backend/src/db/migrations/20230918091330_add_media_data_to_meditation_entries.ts
+++ backend/src/db/migrations/20230918091330_add_media_data_to_meditation_entries.ts
@@ -3,6 +3,7 @@ import { type Knex } from 'knex';
 const TABLE_NAME = 'meditation_entries';
 
 const ColumnName = {
+  NAME: 'name',
   MEDIA_URL: 'media_url',
   CONTENT_TYPE: 'content_type',
 } as const;
@@ -11,6 +12,7 @@ async function up(knex: Knex): Promise<void> {
   await knex(TABLE_NAME).del();
 
   await knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.text(ColumnName.NAME).notNullable();
     table.text(ColumnName.MEDIA_URL).notNullable();
     table.string(ColumnName.CONTENT_TYPE).notNullable();
   });
@@ -18,6 +20,7 @@ async function up(knex: Knex): Promise<void> {
 
 function down(knex: Knex): Promise<void> {
   return knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.dropColumn(ColumnName.NAME);
     table.dropColumn(ColumnName.MEDIA_URL);
     table.dropColumn(ColumnName.CONTENT_TYPE);
   });
