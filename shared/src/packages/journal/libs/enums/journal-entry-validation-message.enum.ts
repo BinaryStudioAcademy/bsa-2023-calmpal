1a14d556050ebcb53138f1f629f4408841646f08
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-09-18 19:07:46 +0300

    cp-268: * import JourneyEntryValidationRule into JournalEntryValidationMessage

--- shared/src/packages/journal/libs/enums/journal-entry-validation-message.enum.ts
+++ shared/src/packages/journal/libs/enums/journal-entry-validation-message.enum.ts
@@ -1,7 +1,8 @@
+import { JourneyEntryValidationRule } from './journal-entry-validation-rule.enum.js';
+
 const JournalEntryValidationMessage = {
-  USER_ID_MUST_BE_NUMBER: 'User id must be a number.',
   TITLE_REQUIRED: 'Title is required.',
-  TITLE_MUST_BE_LESS_THAN_255: 'Title must be less than 255 characters.',
+  TITLE_MUST_BE_LESS_THAN_255: `Title must be less than ${JourneyEntryValidationRule.MAXIMUM_TITLE_LENGTH} characters.`,
   TEXT_REQUIRED: 'Text is required.',
 } as const;
 
