7ddc6c439efef98e1d92cf5489fe4f1f0e3649ca
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-09-18 20:37:04 +0300

    cp-190: + creation method for chat messages api

--- frontend/src/packages/chat-messages/chat-messages-api.ts
+++ frontend/src/packages/chat-messages/chat-messages-api.ts
@@ -1,10 +1,16 @@
+import { ContentType } from 'shared/build/index.js';
+
 import { APIPath } from '#libs/enums/enums.js';
 import { BaseHttpApi } from '#libs/packages/api/api.js';
 import { type HTTP } from '#libs/packages/http/http.js';
 import { type Storage } from '#libs/packages/storage/storage.js';
 
 import { ChatsApiPath } from './libs/enums/enum.js';
-import { type ChatMessageGetAllResponseDto } from './libs/types/types.js';
+import {
+  type ChatMessageCreateRequestDto,
+  type ChatMessageGetAllItemResponseDto,
+  type ChatMessageGetAllResponseDto,
+} from './libs/types/types.js';
 
 type Constructor = {
   baseUrl: string;
@@ -19,11 +25,27 @@ class ChatMessagesApi extends BaseHttpApi {
 
   public async getAllMessagesByChatId(): Promise<ChatMessageGetAllResponseDto> {
     const response = await this.load(
-      this.getFullEndpoint(ChatsApiPath.ROOT, {}),
+      this.getFullEndpoint(ChatsApiPath.CREATE_MESSAGE, {}),
       { method: 'GET', hasAuth: true },
     );
 
     return await response.json<ChatMessageGetAllResponseDto>();
+  }
+
+  public async createChatMessage(
+    payload: ChatMessageCreateRequestDto,
+  ): Promise<ChatMessageGetAllItemResponseDto> {
+    const response = await this.load(
+      this.getFullEndpoint(ChatsApiPath.CREATE_MESSAGE, {}),
+      {
+        method: 'POST',
+        contentType: ContentType.JSON,
+        payload: JSON.stringify(payload),
+        hasAuth: true,
+      },
+    );
+
+    return await response.json<ChatMessageGetAllItemResponseDto>();
   }
 }
 
