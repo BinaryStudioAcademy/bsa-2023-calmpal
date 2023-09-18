781d3def7441123cfaf45cc9640f185517ad6687
HIMDanny <danilbliz0201@gmail.com>
2023-09-18 12:22:39 +0300

    cp-300: + url with query string in sidebar routes

--- frontend/src/libs/components/navigation-menu-wrapper/libs/constants.ts
+++ frontend/src/libs/components/navigation-menu-wrapper/libs/constants.ts
@@ -1,12 +1,25 @@
-import { AppRoute } from '#libs/enums/enums.js';
+import { AppRoute, SidebarModeQueryStringValue } from '#libs/enums/enums.js';
+import { getUrlWithQueryString } from '#libs/helpers/helpers.js';
 import { type Route } from '#libs/types/types.js';
+
+const urlWithQueryString = getUrlWithQueryString({
+  sidebarMode: SidebarModeQueryStringValue.SHOW,
+});
 
 const SIDEBAR_ROUTES: Route[] = [
   { path: AppRoute.ROOT, name: 'home', icon: 'home' },
-  { path: AppRoute.MEDITATION, name: 'meditation', icon: 'meditation' },
-  { path: AppRoute.CHATS, name: 'chats', icon: 'chats' },
-  { path: AppRoute.JOURNAL, name: 'journal', icon: 'journal' },
-  { path: AppRoute.PROFILE, name: 'profile', icon: 'user' },
+  {
+    path: urlWithQueryString(AppRoute.MEDITATION),
+    name: 'meditation',
+    icon: 'meditation',
+  },
+  { path: urlWithQueryString(AppRoute.CHATS), name: 'chats', icon: 'chats' },
+  {
+    path: urlWithQueryString(AppRoute.JOURNAL),
+    name: 'journal',
+    icon: 'journal',
+  },
+  { path: urlWithQueryString(AppRoute.PROFILE), name: 'profile', icon: 'user' },
 ];
 
 export { SIDEBAR_ROUTES };
