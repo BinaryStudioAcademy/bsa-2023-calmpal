5bb4d60a983a43747cd06f104120bc3ee4d38513
myducck <dmitro.kucherenko@gmail.com>
2023-09-18 16:34:50 +0300

    cp-160: - meditation request validation schema

--- shared/src/packages/meditation/libs/validation-schemas/validation-schemas.ts
+++ shared/src/packages/meditation/libs/validation-schemas/validation-schemas.ts
@@ -1,2 +1,1 @@
 export { createMeditationEntryForm } from './create-meditation-entry-form.validation-schema.js';
-export { createMeditationEntryRequest } from './create-meditation-entry-request.validation-schema.js';
