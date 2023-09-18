10ea840ab765f81df9a3e34725709468c4be849b
HIMDanny <danilbliz0201@gmail.com>
2023-09-18 12:21:18 +0300

    cp-300: * sidebar show hook

--- frontend/src/libs/hooks/use-sidebar-show/use-sidebar-show.hook.ts
+++ frontend/src/libs/hooks/use-sidebar-show/use-sidebar-show.hook.ts
@@ -1,3 +1,8 @@
+import {
+  AppQueryStringKey,
+  SidebarModeQueryStringValue,
+} from '#libs/enums/enums.js';
+
 import { useSearchParams as useSearchParameters } from '../hooks.js';
 
 type UseSidebarStateReturn = {
@@ -9,12 +14,17 @@ const useSidebarState = (): UseSidebarStateReturn => {
   const [sidebarMode, setSidebarMode] = useSearchParameters();
 
   const isSidebarShown =
-    sidebarMode.get('sidebarMode') === 'show' ||
-    !sidebarMode.get('sidebarMode');
+    sidebarMode.get(AppQueryStringKey.SIDEBAR_MODE) ===
+    SidebarModeQueryStringValue.SHOW;
 
   const setIsSidebarShown = (value: boolean): void => {
     setSidebarMode((previous) => {
-      return { ...previous, sidebarMode: value ? 'show' : 'hide' };
+      return {
+        ...previous,
+        sidebarMode: value
+          ? SidebarModeQueryStringValue.SHOW
+          : SidebarModeQueryStringValue.HIDE,
+      };
     });
   };
 
