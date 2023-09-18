6d62388028856c4ce7a941a6899846637af350b9
myducck <dmitro.kucherenko@gmail.com>
2023-09-18 16:33:54 +0300

    cp-160: - meditation create form data dto

--- shared/src/packages/meditation/libs/types/meditation-entry-create-form-data-dto.type.ts
+++
@@ -1,8 +0,0 @@
-import { type MultipartFile, type MultipartValue } from '@fastify/multipart';
-
-type MeditationEntryCreateFormDataDto = {
-  name: MultipartValue<string>;
-  file: MultipartFile;
-};
-
-export { type MeditationEntryCreateFormDataDto };
