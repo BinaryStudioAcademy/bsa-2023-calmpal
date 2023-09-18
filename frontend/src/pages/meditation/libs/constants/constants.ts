71359a4aaf0fca2ea6190cc065cdbf4eb98eb45f
myducck <dmitro.kucherenko@gmail.com>
2023-09-18 15:10:17 +0300

    cp-160: * meditation playlist constant

--- frontend/src/pages/meditation/libs/constants/constants.ts
+++ frontend/src/pages/meditation/libs/constants/constants.ts
@@ -42,35 +42,11 @@ const mockedEntries = [
   },
 ] as const;
 
-const mockedPlaylist = [
-  {
-    id: 1,
-    title: 'Meditation for deep sleep',
-    purpose: 'Stress relief',
-    src: 'http://traffic.libsyn.com/mindfulorg/winston-breathing-5mins.mp3',
-    img: MOCKED_IMAGE,
-  },
-  {
-    id: 2,
-    title: 'Meditation for relaxing',
-    purpose: 'Relax',
-    src: 'http://traffic.libsyn.com/mindfulorg/SusanKaiserGreenland.mp3',
-    img: MOCKED_IMAGE,
-  },
-  {
-    id: 3,
-    title: 'Meditation for breath practice',
-    purpose: 'Breathing',
-    src: 'http://traffic.libsyn.com/mindfulorg/LovingKindness.mp3',
-    img: MOCKED_IMAGE,
-  },
-];
-
 export {
   DEFAULT_MEDITATION_PAYLOAD,
   FULL_PERCENTAGE,
+  MOCKED_IMAGE,
   mockedEntries,
-  mockedPlaylist,
   navigationItems,
   PROGRESS_BAR,
   TRACK_FIRST_INDEX,
