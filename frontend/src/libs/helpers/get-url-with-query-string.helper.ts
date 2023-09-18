e13bef5fa9dc01c90ae6a27a813ced18262921ab
HIMDanny <danilbliz0201@gmail.com>
2023-09-18 12:58:15 +0300

    cp-300: * get url with query string function return

--- frontend/src/libs/helpers/get-url-with-query-string.helper.ts
+++ frontend/src/libs/helpers/get-url-with-query-string.helper.ts
@@ -4,13 +4,12 @@ import { type ValueOf } from '#libs/types/types.js';
 type QueryParameters = Record<ValueOf<typeof AppQueryStringKey>, string>;
 
 const getUrlWithQueryString = (
+  url: ValueOf<typeof AppRoute>,
   parameters: QueryParameters,
-): ((url: ValueOf<typeof AppRoute>) => ValueOf<typeof AppRoute>) => {
+): ValueOf<typeof AppRoute> => {
   const queryParameters = new URLSearchParams(parameters);
 
-  return (url: ValueOf<typeof AppRoute>): ValueOf<typeof AppRoute> => {
-    return `${url}?${queryParameters.toString()}` as ValueOf<typeof AppRoute>;
-  };
+  return `${url}?${queryParameters.toString()}` as ValueOf<typeof AppRoute>;
 };
 
 export { getUrlWithQueryString };
