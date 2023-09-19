e28a9444f2b51a7cfe17d40f38bd0c85a0087d69
Oleksandr Chornobryvka <oleksandr.chornobryvka@g,ail.com>
2023-09-19 14:22:25 +0300

    cp-190: - mocked messages constant

--- frontend/src/pages/chat/libs/constants/constants.ts
+++ frontend/src/pages/chat/libs/constants/constants.ts
@@ -1,47 +1,6 @@
-import { type ChatMessageGetAllItemResponseDto } from '#packages/chat-messages/chat-messages.js';
-
 const CHAT_INPUT_DEFAULT_VALUES = {
   message: '',
 } as const;
 
-const MOCK_MESSAGES: ChatMessageGetAllItemResponseDto[] = [
-  {
-    id: 1,
-    chatId: 1,
-    message:
-      'Hi, Doctor. I have been feeling really down lately, and I am not sure why. Can you help me? 😢😭',
-    senderId: 2,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: 2,
-    chatId: 1,
-    message:
-      'Of course! I am here to support you. 🙂 Can you tell me more about how you have been feeling? Any specific symptoms or changes in your daily life?',
-    senderId: 1,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: 3,
-    chatId: 1,
-    message:
-      'I have been experiencing persistent sadness, loss of interest in things I used to enjoy. It is been affecting my work and relationships too!! 💊❌😵',
-    senderId: 2,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    id: 4,
-    chatId: 1,
-    message:
-      'Thanks a lot Amanda. Let me try to analyze that and get back to you...',
-    senderId: 1,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-];
-
-export { CHAT_INPUT_DEFAULT_VALUES, MOCK_MESSAGES };
+export { CHAT_INPUT_DEFAULT_VALUES };
 export { EMPTY_ARRAY_LENGTH } from 'shared/build/index.js';
