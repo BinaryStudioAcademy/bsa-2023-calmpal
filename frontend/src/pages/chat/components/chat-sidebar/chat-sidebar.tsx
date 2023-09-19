f2c1d16c261af1d26928564ac544007ef08673aa
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-09-19 21:56:43 +0300

    cp-268: + delete chat logic to chat sidebar

--- frontend/src/pages/chat/components/chat-sidebar/chat-sidebar.tsx
+++ frontend/src/pages/chat/components/chat-sidebar/chat-sidebar.tsx
@@ -15,11 +15,14 @@ import {
   useCallback,
   useEffect,
   useParams,
+  useRef,
   useSearch,
+  useState,
 } from '#libs/hooks/hooks.js';
 import { type ValueOf } from '#libs/types/types.js';
 import { actions as chatsActions } from '#slices/chats/chats.js';
 
+import { DeleteChatModal } from '../components.js';
 import styles from './styles.module.scss';
 
 type Properties = {
@@ -33,11 +36,18 @@ const ChatSidebar: React.FC<Properties> = ({
 }) => {
   const { id } = useParams<{ id: string }>();
   const dispatch = useAppDispatch();
+  const [deletedChat, setDeleteChat] = useState<null | number>(null);
+  const dialogReference = useRef<HTMLDialogElement>(null);
+
   const { chats } = useAppSelector(({ chats }) => {
     return {
       chats: chats.chats,
     };
   });
+
+  const handleOpen = useCallback(() => {
+    dialogReference.current?.showModal();
+  }, [dialogReference]);
 
   useEffect(() => {
     void dispatch(chatsActions.getAllChats());
@@ -50,46 +60,64 @@ const ChatSidebar: React.FC<Properties> = ({
     // TODO redux logic
   }, [setIsSidebarShown]);
 
-  return (
-    <Sidebar isSidebarShown={isSidebarShown}>
-      <SidebarHeader>
-        <div className={styles['info']}>
-          <span>Chat</span>
-          <span className={styles['chat-number']}>
-            {filteredElements.length}
-          </span>
-        </div>
-        <div className={styles['plus']}>
-          <Link to={AppRoute.CHATS}>
-            <Icon name="plus" color={IconColor.BLUE} width={30} height={30} />
-          </Link>
-        </div>
-      </SidebarHeader>
-      <SidebarBody>
-        <div className={styles['search']}>
-          <Search onValueChange={setFilter} />
-        </div>
-        <div className={styles['chat-list']}>
-          {filteredElements.map((filteredChat) => {
-            const chatLink = AppRoute.CHATS_$ID.replace(
-              ':id',
-              String(filteredChat.id),
-            ) as ValueOf<typeof AppRoute>;
+  const handleDeleteChat = useCallback(
+    (id: number) => {
+      return () => {
+        setDeleteChat(id);
+        handleOpen();
+      };
+    },
+    [handleOpen],
+  );
 
-            return (
-              <Link key={filteredChat.id} to={chatLink}>
-                <Card
-                  title={filteredChat.name}
-                  imageUrl={cardPlaceholder}
-                  onClick={handleSelectChat}
-                  isActive={String(filteredChat.id) === id}
-                />
-              </Link>
-            );
-          })}
-        </div>
-      </SidebarBody>
-    </Sidebar>
+  return (
+    <>
+      <Sidebar isSidebarShown={isSidebarShown}>
+        <SidebarHeader>
+          <div className={styles['info']}>
+            <span>Chat</span>
+            <span className={styles['chat-number']}>
+              {filteredElements.length}
+            </span>
+          </div>
+          <div className={styles['plus']}>
+            <Link to={AppRoute.CHATS}>
+              <Icon name="plus" color={IconColor.BLUE} width={30} height={30} />
+            </Link>
+          </div>
+        </SidebarHeader>
+        <SidebarBody>
+          <div className={styles['search']}>
+            <Search onValueChange={setFilter} />
+          </div>
+          <div className={styles['chat-list']}>
+            {filteredElements.map((filteredChat) => {
+              const chatLink = AppRoute.CHATS_$ID.replace(
+                ':id',
+                String(filteredChat.id),
+              ) as ValueOf<typeof AppRoute>;
+
+              return (
+                <div className={styles['wrapper']} key={filteredChat.id}>
+                  <Link to={chatLink}>
+                    <Card
+                      title={filteredChat.name}
+                      imageUrl={cardPlaceholder}
+                      onClick={handleSelectChat}
+                      isActive={String(filteredChat.id) === id}
+                      iconRight="trash"
+                      onIconClick={handleDeleteChat(filteredChat.id)}
+                      iconColor={IconColor.LIGHT_BLUE}
+                    />
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        </SidebarBody>
+      </Sidebar>
+      <DeleteChatModal ref={dialogReference} id={deletedChat} />
+    </>
   );
 };
 
