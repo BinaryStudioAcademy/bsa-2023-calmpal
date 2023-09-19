ba42155af3f46bf11c3e7b489711a4af4a6c4f98
HIMDanny <danilbliz0201@gmail.com>
2023-09-19 17:22:17 +0300

    cp-190: * delete_name_from_chat_messages_table migration

--- backend/src/db/migrations/20230912094401_delete_name_column_from_chat_messages_table.ts
+++ backend/src/db/migrations/20230912094401_delete_name_column_from_chat_messages_table.ts
@@ -2,7 +2,16 @@ import { type Knex } from 'knex';
 
 const TABLE_NAME = 'chat_messages';
 
+type ChatMessage = {
+  id: number;
+  name: string;
+};
+
+const NAME_INDEX_INCREMENT_NUMBER = 1;
+const NAME_COLUMN_DEFAULT_VALUE = 'Message';
+
 const ColumnName = {
+  ID: 'id',
   NAME: 'name',
 } as const;
 
@@ -12,9 +21,30 @@ function up(knex: Knex): Promise<void> {
   });
 }
 
-function down(knex: Knex): Promise<void> {
-  return knex.schema.alterTable(TABLE_NAME, (table) => {
-    table.string(ColumnName.NAME).unique().notNullable();
+async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.string(ColumnName.NAME);
+  });
+
+  const rows: ChatMessage[] = await knex
+    .select(ColumnName.ID, ColumnName.NAME)
+    .from(TABLE_NAME);
+
+  const updates = rows.map((item, index) => {
+    return knex
+      .table(TABLE_NAME)
+      .where(ColumnName.ID, item.id)
+      .update({
+        name: `${NAME_COLUMN_DEFAULT_VALUE} #${
+          index + NAME_INDEX_INCREMENT_NUMBER
+        }`,
+      });
+  });
+
+  await Promise.all(updates);
+
+  await knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.string(ColumnName.NAME).unique().notNullable().alter();
   });
 }
 
