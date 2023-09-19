451a90423fcb245ffd91845bef65edc3188642ea
myducck <dmitro.kucherenko@gmail.com>
2023-09-19 12:53:55 +0300

    cp-160: + get megabytes helper and constants

--- shared/src/libs/helpers/helpers.ts
+++ shared/src/libs/helpers/helpers.ts
@@ -1,4 +1,5 @@
 export { configureString } from './configure-string/configure-string.helper.js';
 export { getFormattedDate, getFormattedTime } from './date/date.js';
 export { debounce } from './debounce/debounce.helper.js';
+export { getMegabytesFromBytes } from './get-megabytes-rom-bytes/get-megabytes-from-bytes.helper.js';
 export { replaceTemplateWithValue } from './replace-template-with-value/replace-template-with-value.helper.js';
