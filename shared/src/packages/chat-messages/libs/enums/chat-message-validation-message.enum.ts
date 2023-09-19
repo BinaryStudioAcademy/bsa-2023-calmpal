966ce67563637071ca253980ca908c8731b018f7
HIMDanny <danilbliz0201@gmail.com>
2023-09-19 10:14:46 +0300

    cp-190: * change word in validation message

--- shared/src/packages/chat-messages/libs/enums/chat-message-validation-message.enum.ts
+++ shared/src/packages/chat-messages/libs/enums/chat-message-validation-message.enum.ts
@@ -6,7 +6,7 @@ const ChatMessageValidationMessage = {
   CHAT_ID_IS_REQUIRED: 'Chat id is required.',
   USER_ID_MUST_BE_NUMBER: 'User id must be a number.',
   CHAT_ID_MUST_BE_NUMBER: 'Chat id must be a number.',
-  MAXIMUM_MESSAGE_LENGTH: `Message must be less then or equal to ${ChatMessageValidationRule.MAXIMUM_MESSAGE_LENGTH} characters.`,
+  MAXIMUM_MESSAGE_LENGTH: `Message must be less than or equal to ${ChatMessageValidationRule.MAXIMUM_MESSAGE_LENGTH} characters.`,
 } as const;
 
 export { ChatMessageValidationMessage };
