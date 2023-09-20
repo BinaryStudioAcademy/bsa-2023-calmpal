01f1acabc9fc3685c3e8978712557d0bdb83b82c
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-09-20 12:58:51 +0300

    cp-192: * updated meditation entries data

--- backend/src/db/seed-data/meditation-entries.ts
+++ backend/src/db/seed-data/meditation-entries.ts
@@ -2,81 +2,112 @@ const AUDIO_MIME = 'audio/mpeg';
 
 const meditationEntries = [
   {
+    name: 'Atwined',
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Atwined.mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Blue Lagoon',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Blue%20Lagoon.mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Calm Dawn',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Calm%20Dawn.mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Dispersion (Meditation)',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Dispersion%20(Meditation).mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Initiation',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Initiation.mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Joyvision',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Joyvision.mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Lunar Tides (Meditation)',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Lunar%20Tides%20(Meditation).mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Moonbeam Showers (Meditation)',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Moonbeam%20Showers%20(Meditation).mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Moonlit Glade (Meditation)',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Moonlit%20Glade%20(Meditation).mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Night Voyage',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Night%20Voyage.mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Sapphire Glow',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Sapphire%20Glow.mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Solasta',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Solasta.mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Stargazing (Meditation)',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Stargazing%20(Meditation).mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Sweven',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Sweven.mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Weightless (Meditation)',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Weightless%20(Meditation).mp3',
     content_type: AUDIO_MIME,
   },
   {
+    name: 'Your Reverie',
+
     media_url:
       'https://calmpal-storage-development.s3.eu-west-1.amazonaws.com/meditations/Your%20Reverie.mp3',
     content_type: AUDIO_MIME,
