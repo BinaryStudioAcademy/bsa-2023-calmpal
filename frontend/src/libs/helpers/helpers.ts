4b31922f2dc3e1caa52daa34d40fca91fa0684e5
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-09-21 06:58:53 +0300

    cp-194: -   setCursorPosition helper

--- frontend/src/libs/helpers/helpers.ts
+++ frontend/src/libs/helpers/helpers.ts
@@ -1,10 +1,6 @@
 export { getUrlWithQueryString } from './get-url-with-query-string.helper.js';
 export { getValidClassNames } from './get-valid-class-names.helper.js';
 export {
-  changeCursorPosition,
-  setCursorPosition,
-} from './set-cursor-position.helper.js';
-export {
   configureString,
   debounce,
   getFormattedTime,
