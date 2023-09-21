3110ba95ac9e9b5ba8dfcd5846b089958dad27c1
myducck <dmitro.kucherenko@gmail.com>
2023-09-21 17:28:24 +0300

    cp-160: * redundant chat constants

--- frontend/src/pages/chat/libs/constants/constants.ts
+++ frontend/src/pages/chat/libs/constants/constants.ts
@@ -2,7 +2,5 @@ const CHAT_INPUT_DEFAULT_VALUES = {
   message: '',
 } as const;
 
-const DAY_DIFFERENCE_FROM_YESTERDAY = 1;
-
-export { CHAT_INPUT_DEFAULT_VALUES, DAY_DIFFERENCE_FROM_YESTERDAY };
+export { CHAT_INPUT_DEFAULT_VALUES };
 export { EMPTY_ARRAY_LENGTH } from 'shared/build/index.js';
