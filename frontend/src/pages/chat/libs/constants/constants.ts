998253c814239b8ab24c3029a0187e3976ad0d80
myducck <dmitro.kucherenko@gmail.com>
2023-09-21 16:45:24 +0300

    cp-160: * chats constants day difference

--- frontend/src/pages/chat/libs/constants/constants.ts
+++ frontend/src/pages/chat/libs/constants/constants.ts
@@ -2,5 +2,7 @@ const CHAT_INPUT_DEFAULT_VALUES = {
   message: '',
 } as const;
 
-export { CHAT_INPUT_DEFAULT_VALUES };
+const DAY_DIFFERENCE_FROM_YESTERDAY = 1;
+
+export { CHAT_INPUT_DEFAULT_VALUES, DAY_DIFFERENCE_FROM_YESTERDAY };
 export { EMPTY_ARRAY_LENGTH } from 'shared/build/index.js';
