a8dc57e48023465234148a9956d9c34e0ba02f28
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-22 11:20:29 +0300

    cp-282: + meditation api

---
+++ mobile/src/packages/meditation/meditation-api.ts
@@ -0,0 +1,44 @@
+import { APIPath } from '#libs/enums/enums';
+import { BaseHttpApi } from '#libs/packages/api/api';
+import { type HTTP } from '#libs/packages/http/http';
+import { type Storage } from '#libs/packages/storage/storage';
+
+import { MeditationApiPath } from './libs/enums/enums';
+import {
+  type MeditationEntryCreateRequestDto,
+  type MeditationEntryCreateResponseDto,
+} from './libs/types/types';
+
+type Constructor = {
+  baseUrl: string;
+  http: HTTP;
+  storage: Storage;
+};
+
+class MeditationApi extends BaseHttpApi {
+  public constructor({ baseUrl, http, storage }: Constructor) {
+    super({ path: APIPath.MEDITATION, baseUrl, http, storage });
+  }
+
+  public async createMeditationEntry({
+    name,
+    file,
+  }: MeditationEntryCreateRequestDto): Promise<MeditationEntryCreateResponseDto> {
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('file', file);
+
+    const response = await this.load(
+      this.getFullEndpoint(MeditationApiPath.ROOT, {}),
+      {
+        method: 'POST',
+        payload: formData,
+        hasAuth: true,
+      },
+    );
+
+    return await response.json<MeditationEntryCreateResponseDto>();
+  }
+}
+
+export { MeditationApi };
