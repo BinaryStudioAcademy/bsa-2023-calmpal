ece388646be70165b4cdd528a03ad1dae058cdf5
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-23 09:31:20 +0300

    cp-336: + settings fonts

--- mobile/src/screens/notification-settings/components/setting/styles.ts
+++ mobile/src/screens/notification-settings/components/setting/styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 
-import { AppColor } from '#libs/enums/enums';
+import { AppColor, FontFamily } from '#libs/enums/enums';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     fontSize: 16,
     color: AppColor.GRAY_500,
-    fontFamily: 'Montserrat-SemiBold',
+    fontFamily: FontFamily.MONTSERRAT_600,
   },
 });
 
