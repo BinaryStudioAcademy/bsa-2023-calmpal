1ef8679f5b95e83e7dd76f866ded4718c6a68d6c
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-23 13:06:57 +0300

    cp-232: + journal redux actions

--- mobile/src/slices/journal/actions.ts
+++ mobile/src/slices/journal/actions.ts
@@ -1,9 +1,24 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { type AsyncThunkConfig } from '#libs/types/types';
-import { type JournalEntryGetAllResponseDto } from '#packages/journal/journal';
+import {
+  type JournalEntryCreateRequestDto,
+  type JournalEntryGetAllItemResponseDto,
+  type JournalEntryGetAllResponseDto,
+  type JournalEntryUpdatePayloadDto,
+} from '#packages/journal/journal';
 
 import { name as sliceName } from './journal.slice';
+
+const createJournalEntry = createAsyncThunk<
+  JournalEntryGetAllItemResponseDto,
+  JournalEntryCreateRequestDto,
+  AsyncThunkConfig
+>(`${sliceName}/create-journal-entry`, async (payload, { extra }) => {
+  const { journalApi } = extra;
+
+  return await journalApi.createJournalEntry(payload);
+});
 
 const getAllJournalEntries = createAsyncThunk<
   JournalEntryGetAllResponseDto,
@@ -15,4 +30,14 @@ const getAllJournalEntries = createAsyncThunk<
   return await journalApi.getAllJournalEntries();
 });
 
-export { getAllJournalEntries };
+const updateJournalEntry = createAsyncThunk<
+  JournalEntryGetAllItemResponseDto,
+  JournalEntryUpdatePayloadDto,
+  AsyncThunkConfig
+>(`${sliceName}/update-journal-entry`, async (payload, { extra }) => {
+  const { journalApi } = extra;
+
+  return await journalApi.updateJournalEntry(payload);
+});
+
+export { createJournalEntry, getAllJournalEntries, updateJournalEntry };
