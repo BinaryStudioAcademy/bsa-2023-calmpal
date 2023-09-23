f0f0ed7f21f9591a5488f6f651570106b12e1ace
HIMDanny <danilbliz0201@gmail.com>
2023-09-23 10:06:10 +0300

    cp-347: + init subscription service

---
+++ backend/src/packages/subscriptions/subscription.service.ts
@@ -0,0 +1,46 @@
+import { type Billing } from '#libs/packages/billing/billing.js';
+import { type Service } from '#libs/types/types.js';
+
+type Constructor = {
+  billing: Billing;
+};
+
+class SubscriptionService implements Service {
+  private billing: Billing;
+
+  public constructor({ billing }: Constructor) {
+    this.billing = billing;
+  }
+
+  public find(): Promise<unknown> {
+    return Promise.resolve();
+  }
+
+  public findAll(): Promise<{ items: unknown[] }> {
+    return Promise.resolve({ items: [] });
+  }
+
+  public create(): Promise<unknown> {
+    return Promise.resolve();
+  }
+
+  public async createPaymentIntent({
+    price,
+  }: {
+    price: number;
+  }): Promise<{ clientSecret: string | null }> {
+    const { clientSecret } = await this.billing.createPaymentIntent({ price });
+
+    return { clientSecret };
+  }
+
+  public update(): Promise<unknown> {
+    return Promise.resolve();
+  }
+
+  public delete(): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+}
+
+export { SubscriptionService };
