3611a96c9c0cf066c2c282c384bc58f37148dd67
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-23 12:25:07 +0300

    cp-232: + imports form shared for journal

--- mobile/src/packages/journal/libs/types/types.ts
+++ mobile/src/packages/journal/libs/types/types.ts
@@ -1,4 +1,6 @@
 export {
+  type JournalEntryCreateRequestDto,
   type JournalEntryGetAllItemResponseDto,
   type JournalEntryGetAllResponseDto,
+  type JournalEntryUpdatePayloadDto,
 } from 'shared/build/index.js';
