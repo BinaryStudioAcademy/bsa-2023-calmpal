58d825d22bded2aadf8af153aa5a4658cd457de9
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-23 13:03:21 +0300

    cp-232: + styles and components for note

---
+++ mobile/src/screens/journal/components/note/libs/constants.ts
@@ -0,0 +1,11 @@
+const DEFAULT_NOTE_PAYLOAD = {
+  title: 'New note',
+  text: 'Type your text here',
+};
+
+const TEXT_PLACEHOLDER = 'Type your text here';
+
+const SAVE_NOTE_TIMEOUT = 2000;
+
+export { DEFAULT_NOTE_PAYLOAD, SAVE_NOTE_TIMEOUT, TEXT_PLACEHOLDER };
+export { NOTE_SANITIZER_OPTIONS } from 'shared/build/index.js';
