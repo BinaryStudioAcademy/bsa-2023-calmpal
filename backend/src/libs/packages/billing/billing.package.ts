36dfd1b54ba3ff9035b60a362171e91b55932f24
HIMDanny <danilbliz0201@gmail.com>
2023-09-23 10:03:32 +0300

    cp-347: + billing package

---
+++ backend/src/libs/packages/billing/billing.package.ts
@@ -0,0 +1,43 @@
+import Stripe from 'stripe';
+
+import {
+  CURRENCY,
+  SMALLEST_CURRENCY_UNIT_MULTIPLIER,
+} from './constants/constants.js';
+
+type Constructor = {
+  secretKey: string;
+};
+
+class Billing {
+  private secretKey: string;
+
+  private stripe: Stripe;
+
+  public constructor({ secretKey }: Constructor) {
+    this.secretKey = secretKey;
+    this.stripe = new Stripe(this.secretKey, {
+      apiVersion: '2023-08-16',
+    });
+  }
+
+  public async createPaymentIntent({
+    price,
+  }: {
+    price: number;
+  }): Promise<{ clientSecret: string | null }> {
+    const { client_secret: clientSecret } =
+      await this.stripe.paymentIntents.create({
+        amount: this.getPriceInSmallestUnit(price),
+        currency: CURRENCY,
+      });
+
+    return { clientSecret };
+  }
+
+  private getPriceInSmallestUnit(amount: number): number {
+    return amount * SMALLEST_CURRENCY_UNIT_MULTIPLIER;
+  }
+}
+
+export { Billing };
