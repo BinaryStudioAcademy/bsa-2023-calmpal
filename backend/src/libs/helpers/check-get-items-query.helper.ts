926de0b317b4d303fa67dcb448e18ba51f1a4751
evgen <evgenkitan@gmail.com>
2023-09-23 14:38:48 +0300

    cp-314: - check query helper

--- backend/src/libs/helpers/check-get-items-query.helper.ts
+++
@@ -1,4 +0,0 @@
-const checkQuery = (query: string): boolean => {
-  return /^[\sA-Za-z]*$/.test(query);
-};
-export { checkQuery };
--- backend/src/libs/helpers/helpers.ts
+++ backend/src/libs/helpers/helpers.ts
@@ -1,2 +1,1 @@
-export { checkQuery } from './check-get-items-query.helper.js';
 export { replaceTemplateWithValue, sanitizeInput } from 'shared/build/index.js';
