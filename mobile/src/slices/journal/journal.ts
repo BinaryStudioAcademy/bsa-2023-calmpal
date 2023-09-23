312dbbeead29f8e09c2ca487eba95fd108c72628
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-23 13:09:56 +0300

    cp-232: + all actions imports

--- mobile/src/slices/journal/journal.ts
+++ mobile/src/slices/journal/journal.ts
@@ -1,9 +1,15 @@
-import { getAllJournalEntries } from './actions';
+import {
+  createJournalEntry,
+  getAllJournalEntries,
+  updateJournalEntry,
+} from './actions';
 import { actions } from './journal.slice';
 
 const allActions = {
   ...actions,
+  createJournalEntry,
   getAllJournalEntries,
+  updateJournalEntry,
 };
 
 export { allActions as actions };
