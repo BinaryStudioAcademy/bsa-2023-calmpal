401f7c085981d590bdf9de94ace0721649bdfbc7
myducck <dmitro.kucherenko@gmail.com>
2023-09-26 01:03:58 +0300

    cp-276: * relative locale constants, time format

--- shared/src/libs/helpers/date/libs/enums/format-relative-locale.enum.ts
+++ shared/src/libs/helpers/date/libs/enums/format-relative-locale.enum.ts
@@ -1,15 +1,13 @@
-import {
-  DEFAULT_RELATIVE_FORMAT,
-  SINGLE_QUOTE,
-} from '../constants/constants.js';
+import { SINGLE_QUOTE_UNICODE } from '../constants/constants.js';
+import { TimeFormat } from './time-format.enum.js';
 
 const FormatRelativeLocale = {
-  yesterday: `${SINGLE_QUOTE}Yesterday${SINGLE_QUOTE}`,
-  today: `${SINGLE_QUOTE}Today${SINGLE_QUOTE}`,
-  tomorrow: `${SINGLE_QUOTE}Tomorrow${SINGLE_QUOTE}`,
-  lastWeek: DEFAULT_RELATIVE_FORMAT,
-  nextWeek: DEFAULT_RELATIVE_FORMAT,
-  other: DEFAULT_RELATIVE_FORMAT,
+  yesterday: `${SINGLE_QUOTE_UNICODE}Yesterday${SINGLE_QUOTE_UNICODE}`,
+  today: `${SINGLE_QUOTE_UNICODE}Today${SINGLE_QUOTE_UNICODE}`,
+  tomorrow: `${SINGLE_QUOTE_UNICODE}Tomorrow${SINGLE_QUOTE_UNICODE}`,
+  lastWeek: TimeFormat.D_MMMM_YYYY,
+  nextWeek: TimeFormat.D_MMMM_YYYY,
+  other: TimeFormat.D_MMMM_YYYY,
 } as const;
 
 export { FormatRelativeLocale };
--- shared/src/libs/helpers/date/libs/enums/time-format.enum.ts
+++ shared/src/libs/helpers/date/libs/enums/time-format.enum.ts
@@ -1,6 +1,7 @@
 const TimeFormat = {
   HH_MM_SS: 'HH:mm:ss',
   MM_SS: 'mm:ss',
+  D_MMMM_YYYY: 'd MMMM yyyy',
 } as const;
 
 export { TimeFormat };
