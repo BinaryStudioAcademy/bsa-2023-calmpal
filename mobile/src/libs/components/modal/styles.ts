9fd9a5977e53b42111f6a761d9b12fcb8691b66c
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-25 13:59:53 +0300

    cp-269: + modal fonts

--- mobile/src/libs/components/modal/styles.ts
+++ mobile/src/libs/components/modal/styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 
-import { AppColor } from '#libs/enums/enums';
+import { AppColor, FontFamily } from '#libs/enums/enums';
 
 const styles = StyleSheet.create({
   modalContainer: {
@@ -10,10 +10,10 @@ const styles = StyleSheet.create({
   },
   modalTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
     marginBottom: 10,
     alignSelf: 'center',
     color: AppColor.GRAY_500,
+    fontFamily: FontFamily.MONTSERRAT_600,
   },
   buttonsContainer: {
     flexDirection: 'row',
