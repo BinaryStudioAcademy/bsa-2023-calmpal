a70874b858c37c66c9daa45781fb71744e987b8f
HIMDanny <danilbliz0201@gmail.com>
2023-09-25 12:41:53 +0300

    cp-347: + path property to settings options

--- frontend/src/pages/user-profile/components/user-profile-sidebar/libs/constants.ts
+++ frontend/src/pages/user-profile/components/user-profile-sidebar/libs/constants.ts
@@ -1,9 +1,11 @@
+import { AppRoute } from '#libs/enums/enums.js';
 import { type SettingsOption } from '#libs/types/types.js';
 
 const SETTINGS_OPTIONS: SettingsOption[] = [
   {
     key: 'notification',
     title: 'Notifications and Reminders',
+    path: AppRoute.PROFILE,
   },
 ];
 
