c98ae571b7a23764f9d360fb03f40954e7282592
HIMDanny <danilbliz0201@gmail.com>
2023-09-26 01:02:42 +0300

    cp-347: + subscription common query response type

--- backend/src/packages/subscriptions/libs/types/types.ts
+++ backend/src/packages/subscriptions/libs/types/types.ts
@@ -1,3 +1,4 @@
+export { type SubscriptionCommonQueryResponse } from './subscription-common-query-response.type.js';
 export {
   type SubscriptionPaymentIntentCancelRequestDto,
   type SubscriptionPaymentIntentCreateRequestDto,
