e41f136ec31bea6992fc2b15c50d83ec160d775d
HIMDanny <danilbliz0201@gmail.com>
2023-09-25 21:21:32 +0300

    cp-347: + checkout form

--- frontend/src/pages/profile-settings/components/subscription-checkout/components/checkout-form/checkout-form.tsx
+++ frontend/src/pages/profile-settings/components/subscription-checkout/components/checkout-form/checkout-form.tsx
@@ -1,4 +1,77 @@
+import { useCallback } from 'react';
+
+import { Button } from '#libs/components/components.js';
+import { useAppDispatch, useAppSelector } from '#libs/hooks/hooks.js';
+import {
+  useElements,
+  useStripe,
+} from '#pages/profile-settings/libs/hooks/hooks.js';
+import {
+  cancelPaymentIntent,
+  confirmPaymentIntent,
+} from '#slices/subscription/actions.js';
+
+import { PaymentElement } from '../components.js';
+import styles from './styles.module.scss';
+
 const CheckoutForm: React.FC = () => {
-  return <div>CheckoutForm</div>;
+  const dispatch = useAppDispatch();
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const { paymentId, confirmPaymentDataStatus } = useAppSelector(
+    ({ subscription }) => {
+      return {
+        paymentId: subscription.paymentId,
+        confirmPaymentDataStatus: subscription.confirmPaymentDataStatus,
+      };
+    },
+  );
+
+  const isLoading = confirmPaymentDataStatus === 'pending';
+  const isDisabled = isLoading || !stripe || !elements;
+
+  const handleSubmit = useCallback(
+    (event_: React.FormEvent<HTMLFormElement>) => {
+      event_.preventDefault();
+
+      if (!stripe || !elements) {
+        return;
+      }
+
+      void dispatch(confirmPaymentIntent({ elements, stripe }));
+    },
+    [elements, stripe, dispatch],
+  );
+
+  const handleCancelPayment = useCallback(() => {
+    if (!paymentId) {
+      return;
+    }
+
+    void dispatch(cancelPaymentIntent({ id: paymentId }));
+  }, [dispatch, paymentId]);
+
+  return (
+    <form className={styles['form']} onSubmit={handleSubmit}>
+      <PaymentElement />
+      <div className={styles['button-wrapper']}>
+        <Button
+          type="button"
+          label="Cancel"
+          style="primary-blue"
+          onClick={handleCancelPayment}
+          isDisabled={isDisabled}
+        />
+        <Button
+          type="submit"
+          label="Pay now"
+          style="primary-blue"
+          isDisabled={isDisabled}
+          isLoading={isLoading}
+        />
+      </div>
+    </form>
+  );
 };
 export { CheckoutForm };
