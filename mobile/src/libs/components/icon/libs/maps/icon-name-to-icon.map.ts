82b080659c83c0e0686c64c5728e55c5abeb2898
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-25 14:02:32 +0300

    cp-269: * icon names

--- mobile/src/libs/components/icon/libs/maps/icon-name-to-icon.map.ts
+++ mobile/src/libs/components/icon/libs/maps/icon-name-to-icon.map.ts
@@ -1,36 +1,36 @@
-import BackArrowIcon from '#assets/img/icons/back-arrow.svg';
+import BackArrow from '#assets/img/icons/back-arrow.svg';
 import Backward from '#assets/img/icons/backward.svg';
-import BellIcon from '#assets/img/icons/bell.svg';
-import ChatIcon from '#assets/img/icons/chat.svg';
-import DeleteIcon from '#assets/img/icons/delete.svg';
+import Bell from '#assets/img/icons/bell.svg';
+import Chat from '#assets/img/icons/chat.svg';
+import Delete from '#assets/img/icons/delete.svg';
 import Forward from '#assets/img/icons/forward.svg';
-import HomeIcon from '#assets/img/icons/home.svg';
+import Home from '#assets/img/icons/home.svg';
 import Next from '#assets/img/icons/next.svg';
 import Pause from '#assets/img/icons/pause.svg';
 import Play from '#assets/img/icons/play.svg';
-import PlusIcon from '#assets/img/icons/plus.svg';
+import Plus from '#assets/img/icons/plus.svg';
 import Previous from '#assets/img/icons/previous.svg';
-import SignOutIcon from '#assets/img/icons/sign-out.svg';
-import UserIcon from '#assets/img/icons/user.svg';
+import SignOut from '#assets/img/icons/sign-out.svg';
+import User from '#assets/img/icons/user.svg';
 import { type IconName } from '#libs/types/types';
 
 type SVGIcon = React.FC<React.SVGProps<SVGSVGElement>>;
 
 const iconNameToIcon: Record<IconName, SVGIcon> = {
-  'bell': BellIcon as SVGIcon,
-  'plus': PlusIcon as SVGIcon,
-  'chat': ChatIcon as SVGIcon,
-  'delete': DeleteIcon as SVGIcon,
-  'home': HomeIcon as SVGIcon,
-  'back-arrow': BackArrowIcon as SVGIcon,
-  'user': UserIcon as SVGIcon,
+  'bell': Bell as SVGIcon,
+  'plus': Plus as SVGIcon,
+  'chat': Chat as SVGIcon,
+  'delete': Delete as SVGIcon,
+  'home': Home as SVGIcon,
+  'back-arrow': BackArrow as SVGIcon,
+  'user': User as SVGIcon,
   'backward': Backward as SVGIcon,
   'forward': Forward as SVGIcon,
   'next': Next as SVGIcon,
   'pause': Pause as SVGIcon,
   'play': Play as SVGIcon,
   'previous': Previous as SVGIcon,
-  'sign-out': SignOutIcon as SVGIcon,
+  'sign-out': SignOut as SVGIcon,
 };
 
 export { iconNameToIcon };
