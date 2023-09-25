ae0a3e581aff9ecd2be11647b8578e4c59a54899
HIMDanny <danilbliz0201@gmail.com>
2023-09-26 01:08:40 +0300

    cp-347: + subscription repository in subscription service

--- backend/src/packages/subscriptions/subscriptions.ts
+++ backend/src/packages/subscriptions/subscriptions.ts
@@ -2,10 +2,14 @@ import { billing } from '#libs/packages/billing/billing.js';
 import { logger } from '#libs/packages/logger/logger.js';
 
 import { SubscriptionController } from './subscription.controller.js';
+import { SubscriptionModel } from './subscription.model.js';
+import { SubscriptionRepository } from './subscription.repository.js';
 import { SubscriptionService } from './subscription.service.js';
 
+const subscriptionRepository = new SubscriptionRepository(SubscriptionModel);
 const subscriptionService = new SubscriptionService({
   billing,
+  subscriptionRepository,
 });
 
 const subscriptionController = new SubscriptionController(
