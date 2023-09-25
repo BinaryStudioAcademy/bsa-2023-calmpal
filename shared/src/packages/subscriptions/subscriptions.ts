cbaf9b38f2bafa20ff74185731d12caa7dfa110b
HIMDanny <danilbliz0201@gmail.com>
2023-09-25 21:15:22 +0300

    cp-347: * reexport subscription payment cancel request dto type

--- shared/src/packages/subscriptions/subscriptions.ts
+++ shared/src/packages/subscriptions/subscriptions.ts
@@ -1,5 +1,6 @@
 export { SubscriptionApiPath } from './libs/enums/enums.js';
 export {
+  type SubscriptionPaymentIntentCancelRequestDto,
   type SubscriptionPaymentIntentCreateRequestDto,
   type SubscriptionPaymentIntentCreateResponseDto,
 } from './libs/types/types.js';
