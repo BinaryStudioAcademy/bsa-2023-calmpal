116752238959151eb9932daf64b971b5a92b92f4
HIMDanny <danilbliz0201@gmail.com>
2023-09-25 15:07:10 +0300

    cp-347: + subscription management dialog

---
+++ frontend/src/pages/profile-settings/components/subscription/components/subscription-management-dialog/subscription-management-dialog.tsx
@@ -0,0 +1,63 @@
+import { Button, Icon } from '#libs/components/components.js';
+import { IconColor } from '#libs/enums/enums.js';
+
+import { PLAN_BENEFITS } from '../../libs/constants.js';
+import styles from './styles.module.scss';
+
+const dialogStyles = {
+  subscriptionManagement: styles['subscription-management'],
+  heading: styles['heading'],
+  benefits: styles['benefits'],
+  benefit: styles['benefit'],
+  content: styles['content'],
+  sectionTitle: styles['section-title'],
+  subscriptionName: styles['subscription-name'],
+  metaWrapper: styles['meta-wrapper'],
+  meta: styles['meta'],
+};
+
+const SubscriptionManagementDialog: React.FC = () => {
+  return (
+    <div className={dialogStyles.subscriptionManagement}>
+      <h3 className={dialogStyles.heading}>Subscription Management</h3>
+      <div className={dialogStyles.content}>
+        <div>
+          <p className={dialogStyles.sectionTitle}>Current Subscription Plan</p>
+          <p className={dialogStyles.subscriptionName}>Pro Monthly</p>
+          <div className={dialogStyles.metaWrapper}>
+            <div>
+              <p className={dialogStyles.sectionTitle}>Plan renews on</p>
+              <p className={dialogStyles.meta}>30 Sept 2023</p>
+            </div>
+            <div>
+              <p className={dialogStyles.sectionTitle}>Payment</p>
+              <p className={dialogStyles.meta}>$7.5 / month</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p className={dialogStyles.sectionTitle}>Plan benefits</p>
+          <ul className={dialogStyles.benefits}>
+            {PLAN_BENEFITS.map((benefit) => {
+              return (
+                <li key={benefit} className={dialogStyles.benefit}>
+                  <Icon
+                    name="check"
+                    width={12}
+                    height={9}
+                    color={IconColor.BLUE}
+                  />
+                  {benefit}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div>
+          <Button type="button" label="Subscribe" style="primary-blue" />
+        </div>
+      </div>
+    </div>
+  );
+};
+export { SubscriptionManagementDialog };
