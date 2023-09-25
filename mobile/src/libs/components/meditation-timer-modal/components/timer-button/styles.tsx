4bfade8fe24d1c8c7fb2005c8ee46e7716a05462
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-25 22:29:03 +0300

    cp-182: * timer button styles

--- mobile/src/libs/components/meditation-timer-modal/components/timer-button/styles.tsx
+++ mobile/src/libs/components/meditation-timer-modal/components/timer-button/styles.tsx
@@ -3,39 +3,10 @@ import { StyleSheet } from 'react-native';
 import { AppColor, FontFamily } from '#libs/enums/enums';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    backgroundColor: AppColor.GRAY_200_ALPHA_50,
-  },
-  modal: {
-    backgroundColor: AppColor.WHITE,
-    borderColor: AppColor.GRAY_100,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    paddingBottom: 20,
-    paddingHorizontal: 30,
-    paddingTop: 30,
-    elevation: 20,
-    shadowColor: AppColor.BLUE_300,
-  },
-  title: {
-    alignSelf: 'center',
-    color: AppColor.BLACK,
-    fontSize: 16,
-    fontWeight: '600',
-    marginBottom: 20,
-    fontFamily: FontFamily.MONTSERRAT_600,
-  },
-  durations: {
-    flexDirection: 'row',
-    gap: 20,
-    justifyContent: 'center',
-    marginBottom: 10,
-  },
   duration: {
     color: AppColor.GRAY_200_ALPHA_50,
     fontSize: 20,
+    fontFamily: FontFamily.MONTSERRAT_600,
   },
   durationItem: {
     alignItems: 'center',
@@ -50,6 +21,7 @@ const styles = StyleSheet.create({
   durationUnit: {
     color: AppColor.GRAY_200_ALPHA_50,
     fontSize: 12,
+    fontFamily: FontFamily.MONTSERRAT_600,
   },
   activeDuration: {
     backgroundColor: AppColor.BLUE_300,
@@ -57,10 +29,6 @@ const styles = StyleSheet.create({
   },
   activeText: {
     color: AppColor.WHITE,
-  },
-  button: {
-    borderRadius: 40,
-    height: 52,
   },
 });
 
