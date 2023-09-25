f3718a83973fc8c7776ca7520df6fa9c261212b0
HIMDanny <danilbliz0201@gmail.com>
2023-09-26 01:05:42 +0300

    cp-347: + subscription subscribe method

--- backend/src/packages/subscriptions/subscription.service.ts
+++ backend/src/packages/subscriptions/subscription.service.ts
@@ -1,20 +1,30 @@
 import { type Billing } from '#libs/packages/billing/billing.js';
 import { type Service } from '#libs/types/types.js';
+import {
+  type UserAuthResponseDto,
+  userService,
+} from '#packages/users/users.js';
 
 import {
   type SubscriptionPaymentIntentCreateRequestDto,
   type SubscriptionPaymentIntentCreateResponseDto,
 } from './libs/types/types.js';
+import { SubscriptionEntity } from './subscription.entity.js';
+import { type SubscriptionRepository } from './subscription.repository.js';
 
 type Constructor = {
   billing: Billing;
+  subscriptionRepository: SubscriptionRepository;
 };
 
 class SubscriptionService implements Service {
   private billing: Billing;
 
-  public constructor({ billing }: Constructor) {
+  private subscriptionRepository: SubscriptionRepository;
+
+  public constructor({ billing, subscriptionRepository }: Constructor) {
     this.billing = billing;
+    this.subscriptionRepository = subscriptionRepository;
   }
 
   public find(): Promise<unknown> {
@@ -27,6 +37,25 @@ class SubscriptionService implements Service {
 
   public create(): Promise<unknown> {
     return Promise.resolve();
+  }
+
+  public async subscribe({
+    userId,
+  }: {
+    userId: number;
+  }): Promise<UserAuthResponseDto> {
+    const subscriptionEntity = await this.subscriptionRepository.create(
+      SubscriptionEntity.initializeNew({
+        endDate: new Date(),
+      }),
+    );
+
+    const { id } = subscriptionEntity.toObject();
+
+    return await userService.updateSubscription({
+      id: userId,
+      subscriptionId: id,
+    });
   }
 
   public async createPaymentIntent({
