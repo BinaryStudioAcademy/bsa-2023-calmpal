0c674cb9370bcede14a1012a374fec1cdc0ddc6c
HIMDanny <danilbliz0201@gmail.com>
2023-09-25 23:02:29 +0300

    cp-347: * add subscription id column migration

--- backend/src/db/migrations/20230922101634_add_subscription_id_column_to_user_details_table.ts
+++ backend/src/db/migrations/20230922101634_add_subscription_id_column_to_user_details_table.ts
@@ -10,13 +10,19 @@ const ColumnName = {
   SUBSCRIPTION_ID: 'subscription_id',
 } as const;
 
+const RelationRule = {
+  SET_NULL: 'SET NULL',
+} as const;
+
 function up(knex: Knex): Promise<void> {
   return knex.schema.alterTable(TableName.USER_DETAILS, (table) => {
     table
       .integer(ColumnName.SUBSCRIPTION_ID)
       .references(ColumnName.ID)
       .inTable(TableName.SUBSCRIPTIONS)
-      .nullable();
+      .nullable()
+      .onDelete(RelationRule.SET_NULL)
+      .onUpdate(RelationRule.SET_NULL);
   });
 }
 
