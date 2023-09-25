242f9f06f56823d4e0a57092c6707a947920381d
iamAlinaaa <alinaaevns1806@gmail.com>
2023-09-25 22:23:56 +0300

    cp-182: * meditation timer modal styles imporvement

--- mobile/src/libs/components/meditation-timer-modal/styles.ts
+++ mobile/src/libs/components/meditation-timer-modal/styles.ts
@@ -33,31 +33,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginBottom: 10,
   },
-  duration: {
-    color: AppColor.GRAY_200_ALPHA_50,
-    fontSize: 20,
-  },
-  durationItem: {
-    alignItems: 'center',
-    backgroundColor: AppColor.WHITE,
-    borderColor: AppColor.GRAY_200_ALPHA_50,
-    borderRadius: 80,
-    borderWidth: 1,
-    height: 80,
-    justifyContent: 'center',
-    width: 80,
-  },
-  durationUnit: {
-    color: AppColor.GRAY_200_ALPHA_50,
-    fontSize: 12,
-  },
-  activeDuration: {
-    backgroundColor: AppColor.BLUE_300,
-    borderWidth: 0,
-  },
-  activeText: {
-    color: AppColor.WHITE,
-  },
   button: {
     borderRadius: 40,
     height: 52,
