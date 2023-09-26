eca349dd85ce0190c0ad901a3ea746029dd55ee3
myducck <dmitro.kucherenko@gmail.com>
2023-09-26 22:36:36 +0300

    cp-368: * note input constants

--- frontend/src/pages/journal/components/note/components/note-input/libs/constants/constants.ts
+++ frontend/src/pages/journal/components/note/components/note-input/libs/constants/constants.ts
@@ -1,11 +1,18 @@
 const DEFAULT_NOTE_PAYLOAD = {
-  title: 'New note',
-  text: '<p dir="ltr"><span style="white-space:pre-wrap">Type your text here</span></p>',
+  title: '',
+  text: '<p><br /></p>',
 };
 
-const TEXT_PLACEHOLDER = 'Type your text here';
+const TITLE_PLACEHOLDER = 'Enter the title';
+
+const TEXT_PLACEHOLDER = 'Write your notes';
 
 const SAVE_NOTE_TIMEOUT = 2000;
 
-export { DEFAULT_NOTE_PAYLOAD, SAVE_NOTE_TIMEOUT, TEXT_PLACEHOLDER };
+export {
+  DEFAULT_NOTE_PAYLOAD,
+  SAVE_NOTE_TIMEOUT,
+  TEXT_PLACEHOLDER,
+  TITLE_PLACEHOLDER,
+};
 export { NOTE_SANITIZER_OPTIONS } from 'shared/build/index.js';
