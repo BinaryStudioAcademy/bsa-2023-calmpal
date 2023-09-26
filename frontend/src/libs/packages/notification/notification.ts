3b4c87c448f0f6c905476a7493e3af9ab9f99a39
myducck <dmitro.kucherenko@gmail.com>
2023-09-26 22:30:39 +0300

    cp-368: * notification type export

--- frontend/src/libs/packages/notification/notification.ts
+++ frontend/src/libs/packages/notification/notification.ts
@@ -4,4 +4,5 @@ const notification = new Notification();
 
 export { notification };
 export { type NotificationPayload } from '../notification/libs/types/types.js';
+export { NotificationType } from './libs/enums/enums.js';
 export { type Notification } from './notification.package.js';
