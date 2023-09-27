0113097ce38f7d7f2dafd4a53396c0da06f75b45
Dana Todoriak <todoriak@gmail.com>
2023-09-27 08:34:42 +0300

    cp-283: * placeholders

--- mobile/src/libs/components/input-file/input-file.tsx
+++ mobile/src/libs/components/input-file/input-file.tsx
@@ -75,7 +75,7 @@ const InputFile = <T extends FormFieldValues>({
         onPress={handlePickFile as () => void}
       >
         <Icon name="upload" color={AppColor.GRAY_500} />
-        <Text style={styles.primaryText}>click here</Text>
+        <Text style={styles.primaryText}>Click here</Text>
         <Text style={styles.secondaryText}>Only MP3 extension is allowed</Text>
       </Pressable>
       {hasError && <Text style={styles.errorText}>{error as string}</Text>}
