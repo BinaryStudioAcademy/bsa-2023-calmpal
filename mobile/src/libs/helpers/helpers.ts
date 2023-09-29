2b96bbd421f67d014bf08b9c8148f1f147341cbc
Dana Todoriak <todoriak@gmail.com>
2023-09-29 08:45:52 +0300

    cp-170: * getFormattedTime in shared folder

--- mobile/src/libs/helpers/helpers.ts
+++ mobile/src/libs/helpers/helpers.ts
@@ -1,2 +1,5 @@
-export { getFormattedTime } from './get-formatted-time/get-formatted-time.helper';
-export { configureString, debounce } from 'shared/build/index.js';
+export {
+  configureString,
+  debounce,
+  getFormattedTime,
+} from 'shared/build/index.js';
