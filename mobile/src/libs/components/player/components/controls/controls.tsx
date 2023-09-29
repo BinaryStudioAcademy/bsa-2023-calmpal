caaebfa4d8164b9bee6a759bc2964cb626146a5b
Dana Todoriak <todoriak@gmail.com>
2023-09-29 08:51:28 +0300

    cp-170: + setIsPlaying depency in useCalback

--- mobile/src/libs/components/player/components/controls/controls.tsx
+++ mobile/src/libs/components/player/components/controls/controls.tsx
@@ -33,7 +33,7 @@ const Controls: React.FC<Properties> = ({ isPlaying: initialIsPlaying }) => {
   useFocusEffect(
     useCallback(() => {
       setIsPlaying(false);
-    }, []),
+    }, [setIsPlaying]),
   );
 
   return (
