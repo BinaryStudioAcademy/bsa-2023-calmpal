be7cc899a2085b214ec5a5461ca90663fa8a6a22
HIMDanny <danilbliz0201@gmail.com>
2023-09-29 14:20:19 +0300

    cp-481: + render subscription dialog or plan depending on whether the user has a subscription

--- frontend/src/pages/profile-settings/components/subscription/subscription.tsx
+++ frontend/src/pages/profile-settings/components/subscription/subscription.tsx
@@ -1,9 +1,35 @@
-import { SubscriptionManagementDialog } from './components/components.js';
+import { useAppSelector } from '#libs/hooks/hooks.js';
+import { SUBSCRIPTION_PRICE } from '#packages/subscriptions/subscriptions.js';
+
+import {
+  SubscriptionManagementDialog,
+  SubscriptionPlan,
+} from './components/components.js';
+import { PLAN_BENEFITS } from './libs/constants.js';
 
 const Subscription: React.FC = () => {
+  const { subscriptionEndDate } = useAppSelector(({ auth }) => {
+    return {
+      subscriptionEndDate: auth.authenticatedUser?.subscriptionEndDate,
+    };
+  });
+
+  const hasSubscription = Boolean(subscriptionEndDate);
+
   return (
     <section>
-      <SubscriptionManagementDialog />
+      {hasSubscription ? (
+        <SubscriptionManagementDialog
+          benefits={PLAN_BENEFITS}
+          subscriptionPrice={SUBSCRIPTION_PRICE}
+          subscriptionEndDate={subscriptionEndDate as Date}
+        />
+      ) : (
+        <SubscriptionPlan
+          benefits={PLAN_BENEFITS}
+          subscriptionPrice={SUBSCRIPTION_PRICE}
+        />
+      )}
     </section>
   );
 };
