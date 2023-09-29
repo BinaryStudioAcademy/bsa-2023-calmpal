fda0cac14f13223f35999edb5da5f0919f976ba1
HIMDanny <danilbliz0201@gmail.com>
2023-09-29 14:13:33 +0300

    cp-481: * subscription management dialog

--- frontend/src/pages/profile-settings/components/subscription/components/subscription-management-dialog/subscription-management-dialog.tsx
+++ frontend/src/pages/profile-settings/components/subscription/components/subscription-management-dialog/subscription-management-dialog.tsx
@@ -1,37 +1,28 @@
-import { Button, Icon } from '#libs/components/components.js';
-import { AppRoute, IconColor, TimeFormat } from '#libs/enums/enums.js';
+import { Icon } from '#libs/components/components.js';
+import { IconColor, TimeFormat } from '#libs/enums/enums.js';
 import { getFormattedDate } from '#libs/helpers/helpers.js';
-import {
-  useAppDispatch,
-  useAppSelector,
-  useCallback,
-} from '#libs/hooks/hooks.js';
-import { SUBSCRIPTION_PRICE } from '#packages/subscriptions/subscriptions.js';
-import { actions as appActions } from '#slices/app/app.js';
 
-import { PLAN_BENEFITS } from '../../libs/constants.js';
 import styles from './styles.module.scss';
 
 const dialogStyles = {
   sectionTitle: styles['section-title'],
 };
 
-const SubscriptionManagementDialog: React.FC = () => {
-  const dispatch = useAppDispatch();
-  const { subscriptionEndDate } = useAppSelector(({ auth }) => {
-    return {
-      subscriptionEndDate: auth.authenticatedUser?.subscriptionEndDate as Date,
-    };
-  });
+type Properties = {
+  subscriptionPrice: number;
+  benefits: string[];
+  subscriptionEndDate: Date;
+};
 
+const SubscriptionManagementDialog: React.FC<Properties> = ({
+  benefits,
+  subscriptionPrice,
+  subscriptionEndDate,
+}) => {
   const formattedDate = getFormattedDate(
     new Date(subscriptionEndDate),
     TimeFormat.D_MMM_YYYY,
   );
-
-  const handleSubscribe = useCallback(() => {
-    dispatch(appActions.navigate(AppRoute.PROFILE_SUBSCRIPTION_CHECKOUT));
-  }, [dispatch]);
 
   return (
     <div className={styles['subscription-management']}>
@@ -47,14 +38,14 @@ const SubscriptionManagementDialog: React.FC = () => {
             </div>
             <div>
               <p className={dialogStyles.sectionTitle}>Payment</p>
-              <p className={styles['meta']}>${SUBSCRIPTION_PRICE} / month</p>
+              <p className={styles['meta']}>${subscriptionPrice} / month</p>
             </div>
           </div>
         </div>
         <div>
           <p className={dialogStyles.sectionTitle}>Plan benefits</p>
           <ul className={styles['benefits']}>
-            {PLAN_BENEFITS.map((benefit) => {
+            {benefits.map((benefit) => {
               return (
                 <li key={benefit} className={styles['benefit']}>
                   <Icon
@@ -68,15 +59,6 @@ const SubscriptionManagementDialog: React.FC = () => {
               );
             })}
           </ul>
-        </div>
-        <div>
-          <Button
-            type="button"
-            label="Subscribe"
-            style="primary"
-            styleColor="blue"
-            onClick={handleSubscribe}
-          />
         </div>
       </div>
     </div>
