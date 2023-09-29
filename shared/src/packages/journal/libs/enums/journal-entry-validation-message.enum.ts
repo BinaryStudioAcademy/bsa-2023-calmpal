638a2af0b94117ac490de40c7190d6d3a1dbbbff
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-09-29 15:57:10 +0300

    cp-427: * INVALID_TEXT_FORMAT in JournalEntryValidationMessage

--- shared/src/packages/journal/libs/enums/journal-entry-validation-message.enum.ts
+++ shared/src/packages/journal/libs/enums/journal-entry-validation-message.enum.ts
@@ -3,8 +3,7 @@ import { JourneyEntryValidationRule } from './journal-entry-validation-rule.enum
 const JournalEntryValidationMessage = {
   TITLE_REQUIRED: 'Title is required.',
   TITLE_MUST_BE_LESS_THAN: `Title must be less than ${JourneyEntryValidationRule.MAXIMUM_TITLE_LENGTH} characters.`,
-  INVALID_TEXT_FORMAT:
-    'Invalid text format. It should start with "<p" and end with "</p>".',
+  INVALID_TEXT_FORMAT: 'Invalid text format.',
 } as const;
 
 export { JournalEntryValidationMessage };
