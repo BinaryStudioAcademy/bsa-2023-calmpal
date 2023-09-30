34e25db48f2f0f385965aa15b434c78cc1e7423c
myducck <dmitro.kucherenko@gmail.com>
2023-09-30 17:08:44 +0300

    cp-514: * delete account form constants

--- frontend/src/pages/profile-settings/components/delete-account-modal/components/delete-account-form/libs/constants/constants.ts
+++ frontend/src/pages/profile-settings/components/delete-account-modal/components/delete-account-form/libs/constants/constants.ts
@@ -1,4 +1,7 @@
-import { type DeleteAccountFormPayload } from '#packages/users/users.js';
+import {
+  DELETE_ACCOUNT_OTHER_CATEGORY,
+  type DeleteAccountFormPayload,
+} from '#packages/users/users.js';
 
 import { DeleteAccountPayloadKey } from '../enums/enums.js';
 
@@ -11,17 +14,7 @@ const CHECKBOX_OPTIONS = [
   { label: 'I have chosen another app' },
   { label: 'I have reached my goal' },
   { label: 'I can not afford the current pricing' },
-  { label: 'Other' },
+  { label: DELETE_ACCOUNT_OTHER_CATEGORY },
 ] as const;
 
-const OTHER_OPTION_CHECKBOX = 3;
-const OTHER_OPTION_LABEL = CHECKBOX_OPTIONS[OTHER_OPTION_CHECKBOX].label;
-const NO_CHECKBOXES = 0;
-
-export {
-  CHECKBOX_OPTIONS,
-  DEFAULT_DELETE_ACCOUNT_PAYLOAD,
-  NO_CHECKBOXES,
-  OTHER_OPTION_CHECKBOX,
-  OTHER_OPTION_LABEL,
-};
+export { CHECKBOX_OPTIONS, DEFAULT_DELETE_ACCOUNT_PAYLOAD };
